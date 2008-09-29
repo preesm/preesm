01ba3eb481197942d115eee4cd366b3ad3dd9e6c
Jonathan Piat <jpiat@laas.fr>
2008-09-29 14:58:02 +0000

    Modified to match core
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@454 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- plugins/transforms/src/org/ietr/preesm/plugin/exportXml/SDF4JGMLExporter.java
+++ plugins/transforms/src/org/ietr/preesm/plugin/exportXml/SDF4JGMLExporter.java
@@ -3,15 +3,30 @@ package org.ietr.preesm.plugin.exportXml;
 import org.ietr.preesm.core.task.IExporter;
 import org.ietr.preesm.core.task.TextParameters;
 import org.sdf4j.exporter.GMLSDFExporter;
+import org.sdf4j.model.AbstractGraph;
 import org.sdf4j.model.sdf.SDFGraph;
 
 public class SDF4JGMLExporter implements IExporter{
 
-	@Override
-	public void transform(SDFGraph algorithm, TextParameters params) {
-		GMLSDFExporter exporter = new GMLSDFExporter() ;
-		SDFGraph clone = algorithm.clone() ;
-		exporter.export(clone, params.getVariable("path"));
+
+	@SuppressWarnings("unchecked")
+	public void transform(AbstractGraph algorithm, TextParameters params) {
+			GMLSDFExporter exporter = new GMLSDFExporter() ;
+			SDFGraph clone = ((SDFGraph)(algorithm)).clone() ;
+			exporter.export(clone, params.getVariable("path"));
 	}
+
+	@Override
+	public boolean isDAGExporter() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSDFExporter() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
