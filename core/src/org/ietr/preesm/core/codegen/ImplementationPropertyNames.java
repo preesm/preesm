4164dd53e6424034eaa49d50bbeda9a03204bb6e
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2009-01-30 16:20:53 +0000

    Comments on property bean names
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@681 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- core/src/org/ietr/preesm/core/codegen/ImplementationPropertyNames.java
+++ core/src/org/ietr/preesm/core/codegen/ImplementationPropertyNames.java
@@ -3,6 +3,8 @@
  */
 package org.ietr.preesm.core.codegen;
 
+import org.ietr.preesm.core.architecture.simplemodel.Operator;
+
 /**
  * This class contains property bean names used to exchange vertex properties
  * between the mapper and the code generation plugins.
@@ -11,20 +13,69 @@ package org.ietr.preesm.core.codegen;
  */
 public final class ImplementationPropertyNames {
 
+	// Graph properties
+	/**
+	 * See {@link AbcType} for available ABC types.
+	 */
 	public static final String Graph_AbcReferenceType = "AbcReferenceType";
-	public static final String Graph_EdgeSchedReferenceType = "EdgeSchedReferenceType";
-	public static final String Graph_SdfReferenceGraph = "SdfReferenceGraph";
 	
+	/**
+	 * See {@link EdgeSchedType} for available edge scheduling types.
+	 */
+	public static final String Graph_EdgeSchedReferenceType = "EdgeSchedReferenceType";
+
+	/**
+	 * SDFGraph object containing the reference graph for this dag graph.
+	 */
+	public static final String Graph_SdfReferenceGraph = "SdfReferenceGraph";
+
+	// Vertex properties
+	/**
+	 * See {@link VertexType} for available vertex types.
+	 */
 	public static final String Vertex_vertexType = "vertexType";
+	
+	/**
+	 * {@link Operator} on which the vertex is mapped.
+	 */
 	public static final String Vertex_Operator = "Operator";
+	
+	/**
+	 * integer equal to the total order of the vertex.
+	 */
 	public static final String Vertex_schedulingOrder = "schedulingOrder";
 
+	// Vertex of type task properties
+	/**
+	 * integer equal to duration of the task.
+	 */
 	public static final String Task_duration = "duration";
-	
+
+	// Vertex of type send or receive properties
+	/**
+	 * Medium transfering the data.
+	 */
 	public static final String SendReceive_medium = "medium";
-	public static final String SendReceive_dataSize = "dataSize";
-	public static final String SendReceive_Operator_address = "Operator_address";
 	
+	/**
+	 * integer with the data size.
+	 */
+	public static final String SendReceive_dataSize = "dataSize";
+	
+	/**
+	 * String with the operator address.
+	 */
+	public static final String SendReceive_Operator_address = "Operator_address";
+
+	// Vertex of type send properties
+	/**
+	 * Name of the sender vertex.
+	 */
 	public static final String Send_senderGraphName = "senderGraphName";
+	
+	// Vertex of type receive properties
+	/**
+	 * Name of the receiver vertex.
+	 */
 	public static final String Receive_receiverGraphName = "receiverGraphName";
 }
