0d016ffec79ff8e851185e25b057c2b9f954f4f8
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2009-03-04 09:52:55 +0000

    Generating code for pc
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@755 c7aee7ca-e32f-b942-b52e-c1c222c4573e

---
+++ tests/MapperAndCodegen_1/Code/x86/src/TestCom.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : TestCom.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
