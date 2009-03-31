73ec027fc018ac8585ad5dcadf4cee38a5bf103d
Jonathan Piat <jpiat@laas.fr>
2009-03-31 09:14:38 +0000

    new version of SDF4J better support of parameter, and better exception management with relevant message
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@850 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- core/src/org/ietr/preesm/core/task/PreesmException.java
+++ core/src/org/ietr/preesm/core/task/PreesmException.java
@@ -38,18 +38,10 @@ package org.ietr.preesm.core.task;
 
 public class PreesmException extends Exception{
 
-	private String message;
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 620727777815265095L;
 	
 	public PreesmException(String message){
-		this.message = message ;
-	}
-	
-	public String toString(){
-		return message ;
+		super(message);
 	}
 
 }
--- tests/ProdMatVect/Code/PC.c
+++ tests/ProdMatVect/Code/PC.c
@@ -1,9 +1,9 @@
     #include "../src/x86.h"
 
     // Buffer declarations
-    int generate_0[4];
-    int generate_1[4];
-    int prodMatM_0[4];
+    int outputMa_0[9];
+    int outputVe_0[3];
+    int ProdMatV_0[3];
 
     void computationThread_PC(void);
 
@@ -11,45 +11,34 @@
         // Buffer declarations
         long i ;
         long j ;
-        long k ;
 
         for(;;){
-            generateMatrix(generate_0, 4/*size*/);
-            generateMatrix(generate_1, 4/*size*/);
-            {//prodMatMat
-                int *out_matr_1 = &generate_0 [(0 * 8)];
-                for(i = 0; i<2 ; i ++)
-                {//prodMatVect
-                    int *inSub_i__0 = &prodMatM_0 [(i * 2) % 4];
-                    int *outSub_i_0 = &generate_1 [(i * 2) % 4];
-                    int *outSub_i_1 = &out_matr_1 [(i * 4) % 8];
-                    int accOut_a_0[2];
-                    init_accIn(accOut_a_0, 2/*init_size*/);
-                    for(j = 0; j<2 ; j ++)
+            generateMatrix(outputMa_0, 3/*size*/);
+            generateVect(outputVe_0, 3/*size*/);
+            {//ProdMatVect
+                char outLoopP_0[3];
+                char cluster__0[9];
+                init_inLoopPort_0(outLoopP_0, 3/*init_size*/);
+                for(i = 0; i<3 ; i ++)
+                {//cluster_0
+                    char *inSub_i__0 = &cluster__0 [(i * 3) % 9];
+                    int *outSub_i_0 = &outputMa_0 [(i * 3) % 9];
+                    int *outSub_i_1 = &outputVe_0 [(i * 1) % 3];
+                    char vectorOu_0[3];
                     {//prodScalVect
-                        int *outSub_j_0 = &outSub_i_1 [(j * 2) % 4];
-                        int *outSub_j_1 = &outSub_i_0 [(j * 1) % 2];
-                        int scalOut__0[2];
-                        for(k = 0; k<2 ; k ++)
+                        for(j = 0; j<3 ; j ++)
                         {//productScal
-                            int *inSub_k__0 = &scalOut__0 [(k * 1) % 2];
-                            int *outSub_k_0 = &outSub_j_0 [(k * 1) % 2];
-                            int *outSub_k_1 = &accOut_a_0 [(k * 1) % 2];
+                            char *inSub_j__0 = &vectorOu_0 [(j * 1) % 3];
+                            int *outSub_j_0 = &outSub_i_0 [(j * 1) % 3];
+                            char *outSub_j_1 = &outLoopP_0 [(j * 1) % 3];
                             int res_op1[1];
-                            mult(outSub_k_0, outSub_j_1, res_op1);
-                            add(res_op1, outSub_k_1, inSub_k__0);
-                        }
-                        for(k = 0; k<2 ; k ++)
-                        {//copyData
-                            int *inSub_k__0 = &inSub_i__0 [(k * 1) % 2];
-                            int *inSub_k__1 = &accOut_a_0 [(k * 1) % 2];
-                            int *outSub_k_0 = &scalOut__0 [(k * 1) % 2];
-                            copyData(outSub_k_0, inSub_k__1, inSub_k__0);
+                            mult(outSub_j_0, outSub_i_1, res_op1);
+                            add(res_op1, outSub_j_1, inSub_j__0);
                         }
                     }
                 }
             }
-            display(prodMatM_0, 4/*size*/);
+            display(ProdMatV_0, 3/*size*/);
         }
 
     }//computationThread
