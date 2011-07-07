f866d7d64580209646076096f8d3994fba1fc26f
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2011-07-07 16:07:38 +0000

    Moving workflow edition to DFTools.
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1191 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- core/src/org/ietr/preesm/core/scenario/ScenarioParser.java
+++ core/src/org/ietr/preesm/core/scenario/ScenarioParser.java
@@ -44,10 +44,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.ietr.preesm.core.architecture.ArchitectureComponent;
 import org.ietr.preesm.core.architecture.ArchitectureComponentType;
