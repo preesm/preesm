b737fb8ebb095e3260043ae7eb72848f0746cf32
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2011-08-23 13:18:08 +0000

    Added the file wizard for graphML files
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1192 c7aee7ca-e32f-b942-b52e-c1c222c4573e

---
+++ plugins/org.ietr.preesm.ui/src/org/ietr/preesm/ui/NewGraphMLWizard.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2010, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package org.ietr.preesm.ui;
+
+import net.sf.graphiti.GraphitiModelPlugin;
+import net.sf.graphiti.model.Configuration;
+import net.sf.graphiti.model.Graph;
+import net.sf.graphiti.model.ObjectType;
+import net.sf.graphiti.ui.wizards.WizardSaveGraphPage;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+/**
+ * This class provides a wizard to create a new graphml network.
+ * 
+ * @author Matthieu Wipliez
+ * @author mpelcat
+ */
+public class NewGraphMLWizard extends Wizard implements INewWizard {
+
+	private IStructuredSelection selection;
+
+	private IWorkbench workbench;
+
+	/**
+	 * Creates a new wizard.
+	 */
+	public NewGraphMLWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+		setWindowTitle("New Algorithm (GraphML)");
+	}
+
+	@Override
+	public void addPages() {
+		WizardSaveGraphPage page = new WizardSaveGraphPage(selection);
+
+		Configuration configuration = GraphitiModelPlugin.getDefault()
+				.getConfiguration("GraphML");
+		ObjectType type = configuration.getGraphType("SDF Graph");
+
+		page.setGraph(new Graph(configuration, type, true));
+		page.setDescription("Create a new graphml algorithm.");
+		addPage(page);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+		this.workbench = workbench;
+	}
+
+	@Override
+	public boolean performFinish() {
+		final WizardSaveGraphPage page = (WizardSaveGraphPage) getPage("saveGraph");
+		IFile file = page.createNewFile();
+		if (file == null) {
+			return false;
+		}
+
+		// Open editor on new file.
+		IWorkbenchWindow dw = workbench.getActiveWorkbenchWindow();
+		try {
+			if (dw != null) {
+				BasicNewResourceWizard.selectAndReveal(file, dw);
+				IWorkbenchPage activePage = dw.getActivePage();
+				if (activePage != null) {
+					IDE.openEditor(activePage, file, true);
+				}
+			}
+		} catch (PartInitException e) {
+			MessageDialog.openError(dw.getShell(), "Problem opening editor",
+					e.getMessage());
+		}
+
+		return true;
+	}
+
+}
