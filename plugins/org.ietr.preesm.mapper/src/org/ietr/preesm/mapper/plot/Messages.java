b8e841a4aa6e017831098f3dc47afe76fdd1a385
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2012-02-15 17:57:41 +0000

    Corrected a bug on message file location for ui message display. UI messages are retrieved from a messages.properties file.
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1253 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/plot/Messages.java
+++ plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/plot/Messages.java
@@ -47,16 +47,16 @@ import java.util.ResourceBundle;
  * @author mpelcat
  */
 public class Messages {
-	private static final String BUNDLE_NAME = "org.ietr.preesm.plugin.mapper.plot.messages"; //$NON-NLS-1$
+	// messages.properties is researched in the same directory as the Messages class
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
+			.getBundle(Messages.class.getPackage().getName() + "." + BUNDLE_NAME);
 
 	private Messages() {
 	}
 
 	public static String getString(String key) {
-		// TODO Auto-generated method stub
 		try {
 			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
