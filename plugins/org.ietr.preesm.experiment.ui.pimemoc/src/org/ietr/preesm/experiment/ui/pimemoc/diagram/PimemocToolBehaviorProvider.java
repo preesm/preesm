af5cc0c078a335c12f98c0c4682d51bb6f660262
Karol Desnos <karol.desnos@insa-rennes.fr>
2012-12-04 10:11:25 +0000

    (UI) Double click on actors to open their refinement
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1477 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- plugins/org.ietr.preesm.experiment.ui.pimemoc/src/org/ietr/preesm/experiment/ui/pimemoc/diagram/PimemocToolBehaviorProvider.java
+++ plugins/org.ietr.preesm.experiment.ui.pimemoc/src/org/ietr/preesm/experiment/ui/pimemoc/diagram/PimemocToolBehaviorProvider.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
 import org.eclipse.graphiti.examples.common.ui.DiagramTypeWizardPage;
+import org.eclipse.graphiti.features.context.IDoubleClickContext;
+import org.eclipse.graphiti.features.custom.ICustomFeature;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.tb.DefaultToolBehaviorProvider;
 import org.eclipse.graphiti.tb.IToolBehaviorProvider;
+import org.ietr.preesm.experiment.ui.pimemoc.features.OpenRefinementFeature;
 
 /**
  * {@link IToolBehaviorProvider} for the {@link Diagram} with type
@@ -35,6 +38,23 @@ public class PimemocToolBehaviorProvider extends DefaultToolBehaviorProvider {
 		toolTips = new HashMap<GraphicsAlgorithm, String>();
 	}
 
+	@Override
+	public ICustomFeature getDoubleClickFeature(IDoubleClickContext context) {
+		ICustomFeature customFeature = new OpenRefinementFeature(getFeatureProvider());
+		
+		// canExecute() tests especially if the context contains a Actor with a valid refinement
+		if(customFeature.canExecute(context)){
+			return customFeature;
+		}
+		
+		return super.getDoubleClickFeature(context);
+	}
+
+	@Override
+	public String getToolTip(GraphicsAlgorithm ga) {
+		return toolTips.get(ga);
+	}
+	
 	/**
 	 * Set the tooltip message for a given {@link GraphicsAlgorithm}
 	 * 
@@ -45,11 +65,6 @@ public class PimemocToolBehaviorProvider extends DefaultToolBehaviorProvider {
 	 */
 	public void setToolTip(GraphicsAlgorithm ga, String toolTip) {
 		toolTips.put(ga, toolTip);
-	}
-
-	@Override
-	public String getToolTip(GraphicsAlgorithm ga) {
-		return toolTips.get(ga);
 	}
 
 }
