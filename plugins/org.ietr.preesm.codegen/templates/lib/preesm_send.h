2eda1540fef0ac32a1966e9865792af7c5916d6d
Jonathan Piat <jpiat@laas.fr>
2013-02-12 15:54:23 +0000

    new special vertex for send and receive, these vertex may be parameterized with bus speed
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1367 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- plugins/org.ietr.preesm.codegen/templates/lib/preesm_send.h
+++ plugins/org.ietr.preesm.codegen/templates/lib/preesm_send.h
@@ -1,48 +1,45 @@
-#ifndef FORK_H
-#define FORK_H
+#ifndef SEND_H
+#define SEND_H
 
 #include <systemc>
 
 using namespace std;
 
-template<class T, int input_size = 1, int nb_output = 2>
-SC_MODULE (preesm_broadcast) {
+template<class T, int input_size = 1>
+SC_MODULE (preesm_send) {
 
 	sc_core::sc_out<bool> enable_port;
 	sc_core::sc_in<bool> invoke_port;
 
+	sc_core::sc_fifo_out<T> out;
 	sc_core::sc_fifo_in<T> in;
-	sc_core::sc_fifo_out<T> outs[nb_output];
-	T buffer[input_size] ;
 
 	void enable() {
-		bool isEnable;
+		int i;
+		bool isEnable = true;
 		while (1) {
 			isEnable = true;
 			enable_port.write(false);
 			sc_core::wait(10, sc_core::SC_NS);
-			sc_core::wait(100, sc_core::SC_NS, in.data_written_event());
 			isEnable &= (in.num_available() == input_size);
 			enable_port.write(isEnable);
 			sc_core::wait(10, sc_core::SC_NS);
 		}
+
 	}
 
 	void invoke() {
 		int i, j;
 		while (1) {
 			wait(invoke_port.posedge_event());
-			cout << "invoking actor: " << "Broadcast" << endl;
-			for (j = 0; j < input_size; j++) buffer[j] = in.read() ;
-			for (i = 0; i < nb_output; i++) {
-				for (j = 0; j < input_size; j++) {
-					outs[i].write(buffer[j]);
-				}
+			cout << "invoking actor: " << "preesm_send" << endl;
+			for (j = 0; j < input_size; j++) {
+				out.write(in.read());
 			}
 		}
 	}
 
-	SC_CTOR(preesm_broadcast) {
+	SC_CTOR(preesm_send) {
 		SC_THREAD(invoke);
 		SC_THREAD(enable);
 	}
