3e8d8184c933bef86a8cfd4ea75af3e46105c04a
Romina Racca <romina.racca@gmail.com>
2013-03-15 15:11:10 +0000

    Changes in class org.ietr.preesm.experiment.model.transformation.properties.Section.java
    Added properties to each graphic element PiMM and evaluated the expression with Jep.
    
    It stores the expression of each graphic element PiMM in the model.
    
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1533 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- plugins/org.ietr.preesm.experiment.model.transformation/src/org/ietr/preesm/experiment/model/transformation/properties/Section.java
+++ plugins/org.ietr.preesm.experiment.model.transformation/src/org/ietr/preesm/experiment/model/transformation/properties/Section.java
@@ -5,6 +5,8 @@ import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Composite;
@@ -12,7 +14,6 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
-import org.ietr.preesm.experiment.model.pimm.Expression;
 import org.ietr.preesm.experiment.model.pimm.Graph;
 import org.ietr.preesm.experiment.model.pimm.OutputPort;
 import org.ietr.preesm.experiment.model.pimm.Parameter;
@@ -22,7 +23,7 @@ public class Section extends GFPropertySection implements ITabbedPropertyConstan
 
 	private Text txtName;
 	private Text txtExpression;
-	private Expression exp = null;
+	//private Button btn;
 
 	@Override
 	public void createControls(Composite parent, TabbedPropertySheetPage tabbedPropertySheetPage) {
@@ -33,8 +34,9 @@ public class Section extends GFPropertySection implements ITabbedPropertyConstan
 		Composite composite = factory.createFlatFormComposite(parent);
 		FormData data;
 
+		//btn = new Button(composite, SWT.PUSH);
 		txtName = factory.createText(composite, "");
-		
+
 		data = new FormData();
 		data.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
 		data.right = new FormAttachment(100, 0);
@@ -61,35 +63,44 @@ public class Section extends GFPropertySection implements ITabbedPropertyConstan
 		data.right = new FormAttachment(txtExpression, -HSPACE);
 		data.top = new FormAttachment(txtExpression, 50, SWT.CENTER);
 		lblExpression.setLayoutData(data);
+	
+		/*
+		btn.setText("Validar");
+		data = new FormData();
+		data.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
+		data.right = new FormAttachment(10, 0);
+		data.top = new FormAttachment(100, VSPACE);
+		btn.setLayoutData(data);
+		*/
 		
-		
-	/*	
 		txtExpression.addFocusListener(new FocusListener() {
+			
 			@Override
 			public void focusLost(FocusEvent e) {
-				exp = (Expression) txtExpression;
-				System.out.println("Expression focus: "+exp.toString());
-//				Parameter p = null;
-//				p.setExpression(exp);
-				
 				PictogramElement pe = getSelectedPictogramElement();
 				if (pe != null) {
 					Object bo = Graphiti.getLinkService().getBusinessObjectForLinkedPictogramElement(pe);
 					if (bo == null)
 						return;
-					((Parameter) bo).setExpression(exp);
-					//((Parameter) bo).getExpression();
-					//System.out.println("TEXT"+txtExpression.getText());
+					if(bo instanceof Parameter){
+						((Parameter) bo).getExpression().setExpressionString(txtExpression.getText());
+						txtExpression.setText(txtExpression.getText());
+					}
+					if(bo instanceof OutputPort){
+						((OutputPort) bo).getExpression().setExpressionString(txtExpression.getText());
+						txtExpression.setText(txtExpression.getText());
+					}
 					
-					txtExpression.setText(exp.toString());
 				}
 			}
 			
 			@Override
 			public void focusGained(FocusEvent e) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
-		*/
+		
 	}
 
 	@Override
@@ -103,29 +114,29 @@ public class Section extends GFPropertySection implements ITabbedPropertyConstan
 			if (bo instanceof Graph){
 				String name = ((Graph) bo).getName();
 				txtName.setText(name == null ? " " : name);
-				txtExpression.setText(exp == null ? "Exp Null Graph" : "exp");
 			}
-			
+		
 			if (bo instanceof Parameter){
 				String name = ((Parameter) bo).getName();
 				txtName.setText(name == null ? " " : name);
+
+				String expression = ((Parameter) bo).getExpression().getExpressionString();
+				txtExpression.setText(expression == null ? "0" : expression);
 				
-				try{
-					//if(((Parameter) bo).getExpression() == null){System.out.println("Esto es null");}
-					
-					
-					//((Parameter) bo).getExpression().setExpressionString("2+5");
-					String expresion= ((Parameter) bo).getExpression().getExpressionString();
-					txtExpression.setText(exp == null ? "Exp Null Param" : expresion);
-				}catch(NullPointerException e){
-					System.out.println("ERROR"+e.getMessage());
-				}
+				Integer i = ((Parameter) bo).getExpression().evaluate();
+				System.out.println("Parameter: "+i);
 			}
+			
 			
 			if(bo instanceof OutputPort){
 				String name = ((OutputPort) bo).getName();
 				txtName.setText(name == null ? " " : name);
-				txtExpression.setText(exp == null ? "Exp Null Port" : "exp");
+			
+				String expression = ((OutputPort) bo).getExpression().getExpressionString();
+				txtExpression.setText(expression == null ? "0" : expression);
+				
+				Integer i = ((OutputPort) bo).getExpression().evaluate();
+				System.out.println("Port: "+i);
 			}
 		}
 	}
