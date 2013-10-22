fd8549ebf593885b4238f11d7baf97d89ba5e65f
Karol Desnos <karol.desnos@insa-rennes.fr>
2013-10-22 14:24:29 +0000

    (Test Apps) Stereo application: On C6678 !
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1655 c7aee7ca-e32f-b942-b52e-c1c222c4573e

---
+++ tests/Stereo/Code6678/src/c6678.h
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : c6678.h
+ Author      : kdesnos
+ Version     : 1.0
+ Copyright   : CeCILL-C, IETR, INSA Rennes
+ Description : Header inclusion for Preesm generated code.
+ ============================================================================
+ */
+
+#ifndef C6678_H_
+#define C6678_H_
+
+#include "ppm.h"
+#include "displayRGB.h"
+#include "rgb2Gray.h"
+#include "census.h"
+#include "costConstruction.h"
+#include "disparityGen.h"
+#include "offsetGen.h"
+#include "aggregateCost.h"
+#include "computeWeights.h"
+#include "disparitySelect.h"
+#include "medianFilter.h"
+#include "splitMerge.h"
+
+#endif /* C6678_H_ */
