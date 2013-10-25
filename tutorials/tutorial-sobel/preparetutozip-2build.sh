82de4bb99e33ad2d03a211a72956cbab446ca76c
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2013-10-25 11:50:16 +0000

    Automatic tutorial creation for sobel-4build. Many elements are removed for the user to recreate them.
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1679 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- tutorials/tutorial-sobel/preparetutozip-2build.sh
+++ tutorials/tutorial-sobel/preparetutozip-2build.sh
@@ -9,5 +9,24 @@ find . -iname "*.layout" -print0 | xargs -0 -I layoutfiles rm -f layoutfiles
 # rm -Rf Code/bin
 rm -f Code/generated/*
 rm -f DAG/*
+
+# replacing top_display by top_display_2build
+mv Algo/top_display.graphml Algo/top_display.graphml.save
+cp Algo/top_display_2build.graphml Algo/top_display.graphml
+
 cd ..
-zip -r $archivename-2build $archivename -x *preparetutozip.sh* *.svn* *Code/lib/pthread* *Code/lib/SDL* *.yuv* *.dat* *Code/bin*
+
+zip -r $archivename-2build $archivename -x *preparetutozip.sh* \
+*.svn* *Code/lib/pthread* *Code/lib/SDL* *.yuv* *.dat* *Code/bin* \
+*.save* *Algo/top_display_2build.graphml* *.scenario* \
+*4CoreX86.slam* *8CoreX86.slam* *Code6678* *Code/src/sobel.c* \
+*Code/src/splitMerge.c*  *Code/include/sobel.h* *Code/include/splitMerge.h* \
+*Code/IDL/merge.idl* *Code/IDL/sobel.idl* *Code/IDL/split.idl*
+
+cd $archivename
+
+# retrieving the original top_display
+rm Algo/top_display.graphml
+mv Algo/top_display.graphml.save Algo/top_display.graphml
+
+cd ..
