00a9d35d5f78134d8ff8aa94434b3885de410cf5
Maxime Pelcat <Maxime.Pelcat@insa-rennes.fr>
2013-10-25 08:26:11 +0000

    Customized script for zip creation for introduction tutorial
    git-svn-id: http://svn.code.sf.net/p/preesm/code/trunk@1676 c7aee7ca-e32f-b942-b52e-c1c222c4573e

--- tutorials/introduction/preparetutozip.sh
+++ tutorials/introduction/preparetutozip.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # This scripts removes everything that is not wanted in a tutorial zip.
+# It then zips the tutorial
 # Copy it in a tutorial root before running it.
 
 archivename=`pwd | awk -F / '{print $NF}'`
 echo $archivename
 find . -iname "*.layout" -print0 | xargs -0 -I layoutfiles rm -f layoutfiles
-rm -Rf Code/bin
+# rm -Rf Code/bin
 rm -f Code/generated/*
 rm -f DAG/*
-rm -Rf Code/lib/pthread*
-rm -Rf Code/lib/SDL*
 cd ..
-zip -r $archivename $archivename -x *preparetutozip.sh* *.svn*
+zip -r $archivename $archivename -x *preparetutozip.sh* *.svn* *Code/lib/pthread* *Code/lib/SDL* *.yuv* *.dat* *Code/bin*
