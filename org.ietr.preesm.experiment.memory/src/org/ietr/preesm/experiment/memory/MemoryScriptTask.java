329c9557dde903a9ba73afa597fd0402125faa1c
kdesnos <kdesnos@insa-rennes.fr>
2014-01-16 10:13:35 +0000

    (Memory) Add a new workflow task to run actors memory scripts.
    git-svn-id: https://forge.insa-rennes.fr/svn/parcoursrecherche/Preesm@45 3f1b3633-00d9-4c21-a927-a7f1313d4702

---
+++ org.ietr.preesm.experiment.memory/src/org/ietr/preesm/experiment/memory/MemoryScriptTask.java
@@ -0,0 +1,37 @@
+package org.ietr.preesm.experiment.memory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import net.sf.dftools.workflow.WorkflowException;
+import net.sf.dftools.workflow.elements.Workflow;
+import net.sf.dftools.workflow.implement.AbstractTaskImplementation;
+
+public class MemoryScriptTask extends AbstractTaskImplementation {
+
+	@Override
+	public Map<String, Object> execute(Map<String, Object> inputs,
+			Map<String, String> parameters, IProgressMonitor monitor,
+			String nodeName, Workflow workflow) throws WorkflowException {
+		// TODO Auto-generated method stub
+		
+		// Outputs
+		Map<String, Object> outputs = new HashMap<String, Object>();
+		outputs.put("MemEx", inputs.get("MemEx"));
+		return outputs;
+	}
+
+	@Override
+	public Map<String, String> getDefaultParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String monitorMessage() {
+		return "Running Memory Optimization Scripts.";
+	}
+
+}
