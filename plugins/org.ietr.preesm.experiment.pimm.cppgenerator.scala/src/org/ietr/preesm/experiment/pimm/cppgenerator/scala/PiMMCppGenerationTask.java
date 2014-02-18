8919cd86d8301303ffcdadb6375be4fafd6541f9
Clément Guy <clement.guy@insa-rennes.fr>
2014-02-18 17:38:06 +0100

    Creation of the C++ generation task for PiMM models.

---
+++ plugins/org.ietr.preesm.experiment.pimm.cppgenerator.scala/src/org/ietr/preesm/experiment/pimm/cppgenerator/scala/PiMMCppGenerationTask.java
@@ -0,0 +1,32 @@
+package org.ietr.preesm.experiment.pimm.cppgenerator.scala;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.ietr.dftools.workflow.WorkflowException;
+import org.ietr.dftools.workflow.elements.Workflow;
+import org.ietr.dftools.workflow.implement.AbstractTaskImplementation;
+
+public class PiMMCppGenerationTask extends AbstractTaskImplementation {
+
+	@Override
+	public Map<String, Object> execute(Map<String, Object> inputs,
+			Map<String, String> parameters, IProgressMonitor monitor,
+			String nodeName, Workflow workflow) throws WorkflowException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> getDefaultParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String monitorMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
