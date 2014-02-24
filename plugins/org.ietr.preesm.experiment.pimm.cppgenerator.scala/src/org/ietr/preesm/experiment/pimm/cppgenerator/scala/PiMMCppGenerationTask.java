926fa159287f9f8b5edea5142cf48fa057ae1487
Clément Guy <clement.guy@insa-rennes.fr>
2014-02-24 15:10:29 +0100

    Corrected the code for creation of the C++ file, which was not generated in the appropriate folder. Still left 2 TODOs: -Correct when PiScenario are added as automatic inputs -> For now, we cannot access the piscenario directly from a workflow task -Correct when a codegen part is added to PiScenarios -> For now, there is no indication about the folder where to generate code in piscenarios

--- plugins/org.ietr.preesm.experiment.pimm.cppgenerator.scala/src/org/ietr/preesm/experiment/pimm/cppgenerator/scala/PiMMCppGenerationTask.java
+++ plugins/org.ietr.preesm.experiment.pimm.cppgenerator.scala/src/org/ietr/preesm/experiment/pimm/cppgenerator/scala/PiMMCppGenerationTask.java
@@ -35,18 +35,21 @@
  ******************************************************************************/
 package org.ietr.preesm.experiment.pimm.cppgenerator.scala;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.ietr.dftools.workflow.WorkflowException;
 import org.ietr.dftools.workflow.elements.Workflow;
 import org.ietr.dftools.workflow.implement.AbstractTaskImplementation;
-import org.ietr.preesm.core.scenario.PreesmScenario;
 import org.ietr.preesm.experiment.model.pimm.PiGraph;
 import org.ietr.preesm.experiment.pimm.cppgenerator.scala.visitor.CPPCodeGenerationLauncher;
 
@@ -61,36 +64,51 @@ public class PiMMCppGenerationTask extends AbstractTaskImplementation {
 			String nodeName, Workflow workflow) throws WorkflowException {
 
 		// Retrieve inputs
-		//PreesmScenario scenario = (PreesmScenario) inputs.get(scenario_key);
+		// TODO: Correct when PiScenario are added as automatic inputs
+		// PreesmScenario scenario = (PreesmScenario) inputs.get(scenario_key);
 		PiGraph pg = (PiGraph) inputs.get(graph_key);
 
 		CPPCodeGenerationLauncher launcher = new CPPCodeGenerationLauncher();
 
 		String finalResult = launcher.generateCPPCode(pg);
 
-		// Do the print
+		// Get the root of the workspace
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		String workspacePath = workspace.getRoot().getLocation().toString();
-		//String codegenPath = workspacePath + scenario.getCodegenManager().getCodegenDirectory() + "/cpp/" + pg.getName() + ".cpp";
-		String codegenPath = workspacePath + "/tutorialPimm/Code/generated/cpp/" + pg.getName() + ".cpp";
-		
+		IWorkspaceRoot root = workspace.getRoot();
+		// Get the project
+		String projectName = workflow.getProjectName();
+		IProject project = root.getProject(projectName);
+
+		// Get the name of the folder for code generation
+		// TODO: Correct when a codegen part is added to PiScenarios
+		String codegenFolder = "/Code/generated/cpp/";// scenario.getCodegenManager().getCodegenDirectory();
+
+		// Create the folder and its parent if necessary
+		String folderPath = project.getLocation() + codegenFolder;
+		File parent = new File(folderPath);
+		parent.mkdirs();
+
+		// Create the file
+		String filePath = pg.getName() + ".cpp";
+		File file = new File(parent, filePath);
+
+		// Write the file
 		FileWriter out;
 		try {
-			out = new FileWriter(codegenPath);
+			out = new FileWriter(file);
 			out.write(finalResult);
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		//Create an empty output map
-		Map<String, Object> output = new HashMap<String, Object>();
-		return output;
+		// Return an empty output map
+		return Collections.emptyMap();
 	}
 
 	@Override
 	public Map<String, String> getDefaultParameters() {
-		//Create an empty parameters map
+		// Create an empty parameters map
 		Map<String, String> parameters = new HashMap<String, String>();
 		return parameters;
 	}
