efa8bb51c9bbfa38baae5b8d825df078db96bdfe
kdesnos <kdesnos@insa-rennes.fr>
2014-03-26 15:34:37 +0100

    (Tutorial) Sobel on C6678 - Compatible with memory scripts

--- tutorials/tutorial-sobel/Code6678/src/splitMerge.c
+++ tutorials/tutorial-sobel/Code6678/src/splitMerge.c
@@ -14,31 +14,39 @@ Description :
 #include <xdc/runtime/System.h>
 
 void split(int nbSlice, int xSize, int ySize, unsigned char *input, unsigned char *output){
-    int i;
-    int sliceSize =  xSize*ySize/nbSlice;
-
-    // Fill first and last line with 0
-    memset(output,0,xSize);
-    // First Slice
-    memcpy(output+xSize, input, sliceSize);
-    // Copy next line if several slice
-    if (nbSlice > 1){
-        memcpy(output +  xSize + sliceSize , input + sliceSize, xSize);
+	if(output != NULL){
+	    int i;
+	    int sliceSize =  xSize*ySize/nbSlice;
+	
+	    // Fill first and last line with 0
+	    memset(output,0,xSize);
+	    // First Slice
+	    memcpy(output+xSize, input, sliceSize);
+	    // Copy next line if several slice
+	    if (nbSlice > 1){
+	        memcpy(output +  xSize + sliceSize , input + sliceSize, xSize);
+	    }
+	    // Slice other than first and last
+	    for(i=1; i<nbSlice-1; i++){
+	        int destIndex = i*(sliceSize+2*xSize);
+	        memcpy(output + destIndex, input+i*sliceSize-xSize, sliceSize+2*xSize);
+	    }
+	    // Last Slice
+	    i = nbSlice-1;
+	    if(nbSlice > 1){
+	        // we have i = nbSlice -1;
+	        int destIndex = i*(sliceSize+2*xSize);
+	        memcpy(output + destIndex, input+i*sliceSize-xSize, sliceSize+xSize);
+	    }
+	    // Last line
+	    memset(output + (ySize+nbSlice*2-1)*xSize,0,xSize);
+    } else {
+    	// The output has been splitted and is NULL
+    	// Fill first and last line with 0
+	    memset(input-xSize,0,xSize);
+    	// Last line
+	    memset(input + ySize*xSize,0,xSize);
     }
-    // Slice other than first and last
-    for(i=1; i<nbSlice-1; i++){
-        int destIndex = i*(sliceSize+2*xSize);
-        memcpy(output + destIndex, input+i*sliceSize-xSize, sliceSize+2*xSize);
-    }
-    // Last Slice
-    i = nbSlice-1;
-    if(nbSlice > 1){
-        // we have i = nbSlice -1;
-        int destIndex = i*(sliceSize+2*xSize);
-        memcpy(output + destIndex, input+i*sliceSize-xSize, sliceSize+xSize);
-    }
-    // Last line
-    memset(output + (ySize+nbSlice*2-1)*xSize,0,xSize);
 }
 
 
