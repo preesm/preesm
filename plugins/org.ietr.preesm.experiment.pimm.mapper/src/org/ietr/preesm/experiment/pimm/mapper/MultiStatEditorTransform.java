c96e5726da0ef901765c272d3d119b6876c43340
Clément Guy <clement.guy@insa-rennes.fr>
2014-06-02 10:22:40 +0200

    Added a Multi Gantt Plotter task for sets of SDFGraphs (id is org.ietr.preesm.experiment.pimm.mapper.MultiStatEditorTransform)

---
+++ plugins/org.ietr.preesm.experiment.pimm.mapper/src/org/ietr/preesm/experiment/pimm/mapper/MultiStatEditorTransform.java
@@ -0,0 +1,97 @@
+/*********************************************************
+Copyright or � or Copr. IETR/INSA: Matthieu Wipliez, Jonathan Piat,
+Maxime Pelcat, Jean-Fran�ois Nezan, Micka�l Raulet
+
+[mwipliez,jpiat,mpelcat,jnezan,mraulet]@insa-rennes.fr
+
+This software is a computer program whose purpose is to prototype
+parallel applications.
+
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-C license and that you accept its terms.
+ *********************************************************/
+
+package org.ietr.preesm.experiment.pimm.mapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.PlatformUI;
+import org.ietr.dftools.workflow.WorkflowException;
+import org.ietr.dftools.workflow.elements.Workflow;
+import org.ietr.dftools.workflow.implement.AbstractTaskImplementation;
+import org.ietr.preesm.core.scenario.PreesmScenario;
+import org.ietr.preesm.mapper.abc.IAbc;
+import org.ietr.preesm.mapper.ui.stats.EditorRunnable;
+import org.ietr.preesm.mapper.ui.stats.StatEditorInput;
+
+/**
+ * Transform class that can be called in workflow. The transform method displays
+ * the gantt chart of the given mapped dag
+ * 
+ * @author mpelcat
+ */
+public class MultiStatEditorTransform extends AbstractTaskImplementation {
+
+	@Override
+	public Map<String, Object> execute(Map<String, Object> inputs,
+			Map<String, String> parameters, IProgressMonitor monitor,
+			String nodeName, Workflow workflow) throws WorkflowException {
+
+		Set<IAbc> simulators = (Set<IAbc>) inputs.get(KEY_SDF_ABC_SET);
+		PreesmScenario scenario = (PreesmScenario) inputs.get(KEY_SCENARIO);
+
+		for (IAbc simulator : simulators) {
+			if (simulator instanceof IAbc) {
+				IAbc abc = (IAbc) simulator;
+
+				IEditorInput input = new StatEditorInput(abc, scenario,
+						parameters);
+
+				// Run statistic editor
+				PlatformUI.getWorkbench().getDisplay()
+						.asyncExec(new EditorRunnable(input));
+
+			}
+		}
+
+		return new HashMap<String, Object>();
+	}
+
+	@Override
+	public Map<String, String> getDefaultParameters() {
+		return null;
+	}
+
+	@Override
+	public String monitorMessage() {
+		return "Plots the Gantt chart";
+	}
+
+}
