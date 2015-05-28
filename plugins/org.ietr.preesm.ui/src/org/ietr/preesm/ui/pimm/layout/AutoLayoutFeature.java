58e12442d12df3a0d29d9b75247523420eba183b
kdesnos <kdesnos@insa-rennes.fr>
2015-05-28 11:00:06 +0200

    (UI) Create and register class for the automated layout of PiGraphs.

---
+++ plugins/org.ietr.preesm.ui/src/org/ietr/preesm/ui/pimm/layout/AutoLayoutFeature.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright or © or Copr. IETR/INSA: Maxime Pelcat, Jean-François Nezan,
+ * Karol Desnos, Julien Heulot, Clement Guy
+ * 
+ * [mpelcat,jnezan,kdesnos,jheulot,cguy]@insa-rennes.fr
+ * 
+ * This software is a computer program whose purpose is to prototype
+ * parallel applications.
+ * 
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use, 
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info". 
+ * 
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability. 
+ * 
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or 
+ * data to be ensured and,  more generally, to use and operate it in the 
+ * same conditions as regards security. 
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ ******************************************************************************/
+package org.ietr.preesm.ui.pimm.layout;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICustomContext;
+import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+
+/**
+ * {@link AbstractCustomFeature} automating the layout process for PiMM graphs.
+ * 
+ * 
+ * @author kdesnos
+ */
+public class AutoLayoutFeature extends AbstractCustomFeature {
+
+	boolean hasDoneChange = false;
+
+	public AutoLayoutFeature(IFeatureProvider fp) {
+		super(fp);
+	}
+
+	@Override
+	public boolean canExecute(ICustomContext context) {
+		return true;
+	}
+
+	@Override
+	public void execute(ICustomContext context) {
+		System.out.println("Layout the diagram");
+		// Step 1 - Clear all bendpoints
+		
+		// Step 2 - Layout actors in precedence order 
+		// (ignoring cycles / delayed FIFOs in cycles)
+		
+		// Step 3 - Layout non-feedback connections
+		
+		// Step 4 - Layout feedback connections
+	}
+
+	@Override
+	public String getName() {
+		return "Layout Diagram";
+	}
+
+	@Override
+	public boolean hasDoneChanges() {
+		return hasDoneChange;
+	}
+}
