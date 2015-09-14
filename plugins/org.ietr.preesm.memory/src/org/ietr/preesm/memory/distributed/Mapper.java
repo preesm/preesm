5791c220b96be0bb72746dd6aec882565abfa9b4
kdesnos <kdesnos@insa-rennes.fr>
2015-09-14 08:57:55 +0200

    (Memory) Old Mapper task is "functionnal" again.

--- plugins/org.ietr.preesm.memory/src/org/ietr/preesm/memory/distributed/Mapper.java
+++ plugins/org.ietr.preesm.memory/src/org/ietr/preesm/memory/distributed/Mapper.java
@@ -1,3 +1,38 @@
+/*********************************************************
+Copyright or © or Copr. IETR/INSA: Maxime Pelcat, Jean-François Nezan,
+Karol Desnos
+
+[mpelcat,jnezan,kdesnos,jheulot]@insa-rennes.fr
+
+This software is a computer program whose purpose is to prototype
+parallel applications.
+
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-C license and that you accept its terms.
+ *********************************************************/
 package org.ietr.preesm.memory.distributed;
 
 import java.util.HashMap;
@@ -8,6 +43,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.ietr.dftools.algorithm.model.dag.DAGEdge;
 import org.ietr.dftools.algorithm.model.dag.DAGVertex;
 import org.ietr.dftools.architecture.slam.ComponentInstance;
 import org.ietr.dftools.workflow.WorkflowException;
@@ -86,17 +122,22 @@ public class Mapper extends AbstractTaskImplementation {
 			}
 			// scan the vertices of the input MemEx
 			for (MemoryExclusionVertex memExVertex : memEx.vertexSet()) {
-				String memory;
+				String memory = "Shared";;
 				// If dag edge source and target are mapped to the same
 				// component
-				if (memExVertex.getEdge().getSource().getPropertyBean().getValue("Operator")
-						.equals(memExVertex.getEdge().getTarget().getPropertyBean().getValue("Operator"))) {
-					ComponentInstance component;
-					DAGVertex dagVertex = memExVertex.getEdge().getSource();
-					component = (ComponentInstance) dagVertex.getPropertyBean().getValue("Operator");
-					memory = component.getInstanceName();
+				if (memExVertex.getEdge() != null) {
+					// If source and target are mapped to te same core
+					if (memExVertex.getEdge().getSource().getPropertyBean().getValue("Operator")
+							.equals(memExVertex.getEdge().getTarget().getPropertyBean().getValue("Operator"))) {
+						ComponentInstance component;
+						DAGVertex dagVertex = memExVertex.getEdge().getSource();
+						component = (ComponentInstance) dagVertex.getPropertyBean().getValue("Operator");
+						memory = component.getInstanceName();
+					} // Else => Shared memory
 				} else {
-					memory = "Shared";
+					// The MObject is not associated to a DAGEdge
+					// It is either a FIFO_head/body or working memory
+					// For now these mobjects are put in shared memory
 				}
 
 				HashSet<MemoryExclusionVertex> verticesSet = memExesVerticesSet.get(memory);
@@ -125,8 +166,12 @@ public class Mapper extends AbstractTaskImplementation {
 				for (int i = 0; i < 2; i++) {
 					// Retrieve the component on which the DAG Vertex is mapped
 					ComponentInstance component;
-					DAGVertex dagVertex = (i == 0) ? memExVertex.getEdge().getSource()
-							: memExVertex.getEdge().getTarget();
+					DAGEdge edge = memExVertex.getEdge();
+					if(edge == null){
+						throw new RuntimeException("Feedback fifos not yet supported wit this policy.");
+					}
+					DAGVertex dagVertex = (i == 0) ? edge.getSource()
+							: edge.getTarget();
 
 					component = (ComponentInstance) dagVertex.getPropertyBean().getValue("Operator");
 
