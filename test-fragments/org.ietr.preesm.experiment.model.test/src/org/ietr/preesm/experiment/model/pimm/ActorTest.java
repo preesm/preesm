84f54ab57a26004ac3e100f044353cf3d115f8c3
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-04-03 16:25:06 +0200

    [TESTS] Add unit tests

--- test-fragments/org.ietr.preesm.experiment.model.test/src/org/ietr/preesm/experiment/model/pimm/ActorTest.java
+++ test-fragments/org.ietr.preesm.experiment.model.test/src/org/ietr/preesm/experiment/model/pimm/ActorTest.java
@@ -1,16 +1,31 @@
 package org.ietr.preesm.experiment.model.pimm;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ActorTest {
 
 	@Test
-	public void test1() {
+	public void testCreateActor() {
 		Actor actor = null;
 		Assert.assertNull(actor);
 		actor = PiMMFactory.eINSTANCE.createActor();
 		Assert.assertNotNull(actor);
 		Assert.assertNull(actor.getGraph());
+	}
+
+	@Test
+	public void testFail() {
+		Actor actor = null;
+		Assert.assertNull(actor);
+		actor = PiMMFactory.eINSTANCE.createActor();
+		Assert.assertNull(actor);
+	}
+
+	@Test
+	@Ignore
+	public void test1() {
+		//whatever ... it's skipped
 	}
 }
