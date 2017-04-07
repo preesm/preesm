99f63cbf6f8f13d5caece8730becbc36a8ea1670
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-04-07 10:53:36 +0200

    [TESTS] Move test to test fragment

--- plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/evaluator/IBSDFGenerator.java
+++ plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/evaluator/IBSDFGenerator.java
@@ -58,29 +58,29 @@ import org.ietr.preesm.algorithm.importSdf3Xml.Sdf3XmlParser;
  *
  */
 public class IBSDFGenerator {
-	
+
 	// Parameters
 	public int nbactors;
 	public Random rand;
 	// change the path according to where Turbine is placed, and create folders IBgen and IBSDF in this directory
-	public final String path = "/home/blaunay/Bureau/turbine-master/turbine/";
-	
+	public final String path = "/home/anmorvan/git/turbine/Turbine/";
+
 	// Set of graphs
 	public ArrayList<SDFGraph> graphSet;
-	
+
 	public IBSDFGenerator(int n) {
 		graphSet = new ArrayList<SDFGraph>();
 		nbactors = n;
 		rand = new Random();
 	}
-	
-	
+
+
 	/**
 	 * Generates randomly a set of SDF graphs which will be used to construct an IBSDF graph
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
-	private void graphSet_gen() throws IOException, InterruptedException {
+	public void graphSet_gen() throws IOException, InterruptedException {
 		int remaining_actors = nbactors;
 		int randomNum;
 		Process p;
@@ -92,23 +92,23 @@ public class IBSDFGenerator {
 		    if (remaining_actors < randomNum || remaining_actors - randomNum == 1)
 		    	randomNum = remaining_actors;
 		    remaining_actors -= randomNum;
-		    
+
 		    // Generation of the graph
 		    p = Runtime.getRuntime().exec(new String[]{	path+"SDFGenerator.py",
 		    		Integer.toString(randomNum),Integer.toString(graphSet.size())});
 		    p.waitFor();
-			
+
 		    // Import it in PREESM
 		    File file = new File(path+"IBgen/"+String.valueOf(graphSet.size())+".sdf3");
 			InputStream iStream = new FileInputStream(file);
 			Sdf3XmlParser importer = new Sdf3XmlParser();
-			SDFGraph G = importer.parse(iStream); 
+			SDFGraph G = importer.parse(iStream);
 			G.setName(String.valueOf(graphSet.size()));
 			graphSet.add(G);
 		}
 	}
-	
-	
+
+
 	/**
 	 * Picks randomly nb vertices in the graph G
 	 * @param G
@@ -128,8 +128,8 @@ public class IBSDFGenerator {
 		}
 		return verticestoreturn;
 	}
-	
-	
+
+
 	/**
 	 * Inserts the graph g in the vertex v by adding corresponding interfaces
 	 * and ensuring the liveness of the whole graph
@@ -143,20 +143,20 @@ public class IBSDFGenerator {
 		// we need nbsources+nbsinks vertices to connect to the interfaces
 		ArrayList<SDFAbstractVertex> toconnect = randomVertices(g, nbsources+nbsinks);
 		AbstractEdgePropertyType<?> E_in;
-		
+
 		// Add the necessary interfaces and edges connecting them to the graph
 		for (int i=0; i<nbsources; i++) {
 			g.addVertex(v.getSources().get(i));
-			
+
 			// output of the interface
 			SDFSinkInterfaceVertex interface_output = new SDFSinkInterfaceVertex();
 			interface_output.setName(v.getSources().get(i).getName());
 			v.getSources().get(i).addSink(interface_output);
-			
+
 			SDFSourceInterfaceVertex inPort = new SDFSourceInterfaceVertex();
 			inPort.setName("from"+v.getSources().get(i).getName());
 			toconnect.get(i).addSource(inPort);
-			
+
 			SDFEdge newEdge = g.addEdge(v.getSources().get(i), toconnect.get(i));
 			newEdge.setSourceInterface(interface_output);
 			newEdge.setTargetInterface(inPort);
@@ -167,15 +167,15 @@ public class IBSDFGenerator {
 		// Add the sink interfaces
 		for (int i=0; i<nbsinks; i++) {
 			g.addVertex(v.getSinks().get(i));
-			
+
 			SDFSourceInterfaceVertex interface_input = new SDFSourceInterfaceVertex();
 			interface_input.setName(v.getSinks().get(i).getName());
 			v.getSinks().get(i).addSource(interface_input);
-			
+
 			SDFSinkInterfaceVertex outPort = new SDFSinkInterfaceVertex();
 			outPort.setName("to"+v.getSinks().get(i).getName());
 			toconnect.get(i+nbsources).addSink(outPort);
-			
+
 			SDFEdge newEdge = g.addEdge(toconnect.get(i+nbsources),v.getSinks().get(i));
 			newEdge.setSourceInterface(outPort);
 			newEdge.setTargetInterface(interface_input);
@@ -184,8 +184,8 @@ public class IBSDFGenerator {
 			newEdge.setCons(E_in);
 		}
 	}
-	
-	
+
+
 	/**
 	 * Counts the number of actors of a graph g (without the possible interfaces)
 	 */
@@ -196,30 +196,30 @@ public class IBSDFGenerator {
 				cpt++;
 		return cpt;
 	}
-	
-	
+
+
 	/**
 	 * Constructs randomly a hierarchical graph from the SDF graphs in the set.
 	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws SDF4JException
-	 * @throws InvalidExpressionException 
+	 * @throws InvalidExpressionException
 	 */
-	private boolean hierarchize() throws IOException, InterruptedException, SDF4JException, InvalidExpressionException {
+	public boolean hierarchize() throws IOException, InterruptedException, SDF4JException, InvalidExpressionException {
 		int remaining_graphs, current, r;
 		// index of current graph
 		current = 0;
 		remaining_graphs = graphSet.size()-1;
 		ArrayList<SDFAbstractVertex> selectedVertices = new ArrayList<SDFAbstractVertex>();
 		SDFAbstractVertex tmp;
-		
+
 		while (remaining_graphs > 0) {
 			// pick r < remaining_graphs and r < nbacteurs
 			r = rand.nextInt(Math.min(remaining_graphs,nbActors(graphSet.get(current)))+1);
 			// choose r vertices in the current graph
 			while (selectedVertices.size() < r) {
 				tmp = randomVertices(graphSet.get(current), 1).get(0);
-				while (selectedVertices.contains(tmp)) 
+				while (selectedVertices.contains(tmp))
 					tmp = randomVertices(graphSet.get(current), 1).get(0);
 				selectedVertices.add(tmp);
 			}
@@ -228,34 +228,34 @@ public class IBSDFGenerator {
 				insert(graphSet.get(current+i),selectedVertices.get(i-1));
 			}
 			selectedVertices.clear();
-			
+
 			remaining_graphs -= r;
 			// new current graph is the next graph not yet considered
-			current += r; 
+			current += r;
 		}
-		
+
 		// Export the graph
 		GMLSDFExporter exporter = new GMLSDFExporter();
 		Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", "rm -f "+path+"IBSDF/*"});
 		p.waitFor();
 		exporter.export(graphSet.get(0), path+"IBSDF/top.graphml");
-		
+
 		// Check that there is no problem if we normalize and evaluate the liveness
 		IBSDFThroughputEvaluator eval = new IBSDFThroughputEvaluator();
 		NormalizeVisitor normalize = new NormalizeVisitor();
 		graphSet.get(0).accept(normalize);
 		return eval.is_alive((SDFGraph) normalize.getOutput()) != null;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Main method for tests and generate IBSDF graphs
 	 * @param args
 	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws SDF4JException
-	 * @throws InvalidExpressionException 
+	 * @throws InvalidExpressionException
 	 */
 	public static void main(String [] args) throws IOException, InterruptedException, SDF4JException, InvalidExpressionException
 	{
@@ -263,6 +263,6 @@ public class IBSDFGenerator {
 		IBSDFGenerator x = new IBSDFGenerator(50);
 		x.graphSet_gen();
 		x.hierarchize();
-		
+
 	}
 }
---
+++ test-fragments/org.ietr.preesm.evaluator.test/src/org/ietr/preesm/evaluator/test/IBSDFGeneratorTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright or © or Copr. 2015 - 2017 IETR/INSA:
+ *
+ * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017)
+ * blaunay <bapt.launay@gmail.com> (2015)
+ *
+ * This software is a computer program whose purpose is to prototype
+ * parallel applications.
+ *
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ *******************************************************************************/
+package org.ietr.preesm.evaluator.test;
+
+import java.io.IOException;
+
+import org.ietr.dftools.algorithm.model.parameters.InvalidExpressionException;
+import org.ietr.dftools.algorithm.model.visitors.SDF4JException;
+import org.ietr.preesm.evaluator.IBSDFGenerator;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Generator of hierarchical graphs (IBSDF) from alive SDF graphs generated with
+ * <a href="https://github.com/bbodin/turbine/">Turbine</a>, the only parameter
+ * to give is the number of actors. Used independently from the throughput
+ * evaluator to create instances for test
+ *
+ * @author blaunay
+ *
+ */
+public class IBSDFGeneratorTest {
+
+	/**
+	 * Main method for tests and generate IBSDF graphs
+	 *
+	 * @param args
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * @throws SDF4JException
+	 * @throws InvalidExpressionException
+	 */
+	@Test
+	/*
+	 * The tested class requires system specific configuration (turbine
+	 * installed and path configured ...)
+	 */
+	@Ignore
+	public void testGenerator() throws IOException, InterruptedException, SDF4JException, InvalidExpressionException {
+		IBSDFGenerator x = new IBSDFGenerator(50);
+		x.graphSet_gen();
+		x.hierarchize();
+
+	}
+}
