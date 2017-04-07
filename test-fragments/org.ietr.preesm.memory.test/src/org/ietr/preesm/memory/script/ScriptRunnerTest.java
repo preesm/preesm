b9412c30149dc991e061dca169e70b91ecbd0638
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-04-07 11:39:08 +0200

    [TESTS] Add script runner test

---
+++ test-fragments/org.ietr.preesm.memory.test/src/org/ietr/preesm/memory/script/ScriptRunnerTest.java
@@ -0,0 +1,20 @@
+package org.ietr.preesm.memory.script;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScriptRunnerTest {
+	public static final String SCRIPT_FOLDER_PATH = "resources/BSscripts/";
+
+	/**
+	 * Make sure the tests will be able to find the test files
+	 */
+	@Test
+	public void testFindScripts() {
+		File dir = new File(SCRIPT_FOLDER_PATH);
+		String[] list = dir.list();
+		Assert.assertEquals(15, list.length);
+	}
+}
