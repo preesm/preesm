1af0c6402eb501d73654e9c27ab724352389b9cc
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-05-03 17:59:32 +0200

    [REFACTOR] split method

--- plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/expression/ExpressionEvaluator.java
+++ plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/expression/ExpressionEvaluator.java
@@ -21,56 +21,62 @@ public class ExpressionEvaluator {
    *
    */
   public static final String evaluate(final Expression expression) {
-
     final String allExpression = expression.getString();
-    final JEP jep = new JEP();
 
+    Parameterizable parameterizableObj = lookUpParameters(expression);
+
+    try {
+      final JEP jep = new JEP();
+      addInputParameterValues(parameterizableObj, jep);
+      final long round = parse(allExpression, jep);
+
+      return Long.toString(round);
+    } catch (final ParseException e) {
+      throw new RuntimeException("Parsing Error, check expression syntax" + " : " + allExpression, e);
+    } catch (final NumberFormatException e) {
+      throw new RuntimeException("Evaluation Error, check parameter dependencies" + " : " + allExpression, e);
+    }
+  }
+
+  private static long parse(final String allExpression, final JEP jep) throws ParseException {
+    final Node parse = jep.parse(allExpression);
+    final Object result = jep.evaluate(parse);
+    if (!(result instanceof Double)) {
+      throw new UnsupportedOperationException("Unsupported result type " + result.getClass().getSimpleName());
+    }
+    Double dResult = (Double) result;
+    final long round = Math.round(dResult);
+    return round;
+  }
+
+  private static Parameterizable lookUpParameters(final Expression expression) {
     Parameterizable parameterizableObj;
     if (expression.eContainer() instanceof Parameterizable) {
       parameterizableObj = (Parameterizable) expression.eContainer();
     } else if (expression.eContainer().eContainer() instanceof Parameterizable) {
       parameterizableObj = (Parameterizable) expression.eContainer().eContainer();
     } else {
-      return "Neither a child of Parameterizable nor a child of a child of Parameterizable";
+      throw new RuntimeException("Neither a child of Parameterizable nor a child of a child of Parameterizable");
     }
+    return parameterizableObj;
+  }
 
-    try {
-      for (final ConfigInputPort port : parameterizableObj.getConfigInputPorts()) {
-        if ((port.getIncomingDependency() != null) && (port.getIncomingDependency().getSetter() instanceof Parameter)) {
-          final Parameter p = (Parameter) port.getIncomingDependency().getSetter();
+  private static void addInputParameterValues(Parameterizable parameterizableObj, final JEP jep) {
+    for (final ConfigInputPort port : parameterizableObj.getConfigInputPorts()) {
+      if ((port.getIncomingDependency() != null) && (port.getIncomingDependency().getSetter() instanceof Parameter)) {
+        final Parameter p = (Parameter) port.getIncomingDependency().getSetter();
 
-          String parameterName;
-          if ((parameterizableObj instanceof Parameter) || (parameterizableObj instanceof Delay) || (parameterizableObj instanceof InterfaceActor)) {
-            parameterName = p.getName();
-          } else {
-            parameterName = port.getName();
-          }
-
-          final String evaluatedParam = p.getExpression().evaluate();
-
-          jep.addVariable(parameterName, Double.parseDouble(evaluatedParam));
+        String parameterName;
+        if ((parameterizableObj instanceof Parameter) || (parameterizableObj instanceof Delay) || (parameterizableObj instanceof InterfaceActor)) {
+          parameterName = p.getName();
+        } else {
+          parameterName = port.getName();
         }
+
+        final String evaluatedParam = p.getExpression().evaluate();
+
+        jep.addVariable(parameterName, Double.parseDouble(evaluatedParam));
       }
-
-      final Node parse = jep.parse(allExpression);
-
-      final Object result = jep.evaluate(parse);
-      String evaluation;
-
-      /*
-       * Display an Integer as it: 1 instead of 1.0 (Useful to parse them then)
-       */
-      if ((result instanceof Double) && (((Double) result % 1) == 0)) {
-        evaluation = Integer.toString((int) (double) result);
-      } else {
-        evaluation = result.toString();
-      }
-      return evaluation;
-
-    } catch (final ParseException e) {
-      return "Parsing Error, check expression syntax" + " : " + allExpression;
-    } catch (final NumberFormatException e) {
-      return "Evaluation Error, check parameter dependencies" + " : " + allExpression;
     }
   }
 }
