487eb8c62697b91cfbdc4937151dc7c63c28c0cf
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-06-02 15:01:56 +0200

    (UI) Update Preesm Menus
    - Remove workflow DebugUI launch menu (Run As ...)
    - Add popup menu entry for running workflows in Preesm menu
    - Add icons in Preesm popup menu

---
+++ plugins/org.ietr.preesm.ui/src/org/ietr/preesm/ui/pimm/checker/popup/actions/WorkflowRunnerPopup.java
@@ -0,0 +1,36 @@
+package org.ietr.preesm.ui.pimm.checker.popup.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.ietr.dftools.ui.workflow.launch.WorkflowLaunchShortcut;
+import org.ietr.preesm.ui.Activator;
+
+/**
+ *
+ */
+public class WorkflowRunnerPopup extends AbstractHandler {
+
+  final Shell shell = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell();
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+
+    final TreeSelection activeMenuSelection = (TreeSelection) HandlerUtil.getActiveMenuSelection(event);
+    final IFile workflowFile = (IFile) activeMenuSelection.getFirstElement();
+
+    ILaunchConfiguration configuration = WorkflowLaunchShortcut.createLaunchConfiguration(workflowFile);
+    if (configuration != null) {
+      DebugUITools.launch(configuration, "run");
+    }
+
+    return null;
+  }
+
+}
