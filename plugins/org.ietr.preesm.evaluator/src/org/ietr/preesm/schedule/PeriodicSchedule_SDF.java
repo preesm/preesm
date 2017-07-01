b9a1b6a9087ba6ab3c578bbc94b7c9d8bac8e658
HDeroui <HDeroui@insa-rennes.fr>
2017-07-01 18:13:19 +0200

    SDF Periodic schedule - compute actors period

--- plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/schedule/PeriodicSchedule_SDF.java
+++ plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/schedule/PeriodicSchedule_SDF.java
@@ -3,24 +3,35 @@ package org.ietr.preesm.schedule;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import org.apache.commons.lang.math.Fraction;
+import org.ietr.dftools.algorithm.model.sdf.SDFAbstractVertex;
+import org.ietr.dftools.algorithm.model.sdf.SDFEdge;
 import org.ietr.dftools.algorithm.model.sdf.SDFGraph;
 import org.ietr.preesm.mathematicalModels.PeriodicScheduleModel_Gurobi;
+import org.ietr.preesm.throughput.helpers.GraphStructureHelper;
 import org.ietr.preesm.throughput.transformers.SDFTransformer;
 
 /**
- * this class computes the periodic schedule for an SDF graph :
+ * this class computes the periodic schedule for an SDF graph. It computes the execution period W and the start date of the first execution for each actor. It
+ * also define the maximum throughput of a periodic schedule of the graph.
  * 
- * compute the period w for each actor and determine the maximum throughput of the graph.
+ * TODO add the method that return the start date of an execution t of an actor a
+ * 
+ * TODO use only one method to compute the normalized period of the graph which return a fraction instead of a double.
  * 
  * @author HDeroui
  *
  */
 public class PeriodicSchedule_SDF {
 
-  // methods for MCR problem
+  /**
+   * @author HDeroui
+   *
+   *         methods for MCR problem
+   * 
+   */
   public static enum Method {
     Algorithm, LinearProgram_Gurobi, LinearProgram_GLPK
-  };
+  }
 
   /**
    * tests if a periodic schedule exists for an SDF graph using the same sufficient condition of Alix Munier for the liveness of SDF graphs.
@@ -34,70 +45,64 @@ public class PeriodicSchedule_SDF {
     return true;
   }
 
-  public double schedule(SDFGraph graph, Method method) {
-    double throughput = -1;
+  /**
+   * Schedules an SDF graph with a periodic schedule. It computes the execution period W and the start date S0 of the first execution for each actor of the
+   * graph.
+   * 
+   * @param graph
+   *          SDF graph
+   * @param method
+   *          for the MCR problem
+   * @param selfLoopEdge
+   *          if true the method will add a self loop edge for each actor to add an execution dependency between the instances of each actor
+   * @return throughput
+   */
+
+  public double schedule(SDFGraph graph, Method method, boolean selfLoopEdge) {
+    Fraction throughput = Fraction.getFraction(-1);
 
     // Step 1: normalize the graph
     SDFTransformer.normalize(graph);
 
     // if a periodic schedule exists for the graph
     if (isPeriodic(graph)) {
-      // Step 2: compute the normalized period K
-      // this.computePeriods(g, method);
-      this.computePeriodswithoutLoop(graph, method);
+      // add a self loop edge for each actor if selfLoopEdge = true
+      ArrayList<SDFEdge> selfLoopEdgesList = null;
+      if (selfLoopEdge) {
+        selfLoopEdgesList = new ArrayList<>(graph.vertexSet().size());
+        for (SDFAbstractVertex actor : graph.vertexSet()) {
+          SDFEdge edge = GraphStructureHelper.addEdge(graph, actor.getName(), null, actor.getName(), null,
+              (Integer) actor.getPropertyBean().getValue("normalizedRate"), (Integer) actor.getPropertyBean().getValue("normalizedRate"),
+              (Integer) actor.getPropertyBean().getValue("normalizedRate"), null);
+          selfLoopEdgesList.add(edge);
+        }
+      }
 
-      // Step 3: compute actors period and define the maximum throughput of the graph
-      this.computeMaxThroughput(graph);
+      // Step 2: compute the normalized period K
+      this.computeNormalizedPeriod(graph, method);
+
+      // remove the self loop edges added before
+      if (selfLoopEdge) {
+        for (SDFEdge edge : selfLoopEdgesList) {
+          graph.removeEdge(edge);
+        }
+      }
+
+      // Step 3: compute actors period and define the maximum throughput of the computed periodic schedule
+      throughput = this.computeActorsPeriod(graph);
 
       // Step 4: compute the start date of the first execution of each actor
-      this.computeStartingTimes(graph);
+      this.computeActorsFirstExecutionStartDate(graph);
 
     } else {
       System.out.println("a periodic schedule does not exist for this graph");
       // return -1 as an error
     }
 
-    return throughput;
+    return throughput.doubleValue();
   }
 
-  /**
-   * compute all actors period through a mathematical model (or an algorithm)
-   */
-  public double computePeriods(SDFGraph g, Method method) {
-    switch (method) {
-      case Algorithm: {
-        // use one of the known algorithm to compute the optimal K
-        return -1;
-      }
-      case LinearProgram_Gurobi: {
-        System.out.println("computing Kopt !!");
-        // ***** add self loop to each actor *****
-        ArrayList<String> addedEdgesList = new ArrayList<>(g.actors.size());
-        for (Actor a : g.actors.values()) {
-          g.createEdge(a.id + "_Loop", a.id, null, a.id, null, a.normalizationValue, a.normalizationValue, a.normalizationValue, null);
-          addedEdgesList.add(a.id + "_Loop");
-        }
-
-        // use the linear programming to compute the optimal K
-        PeriodicScheduleModel_Gurobi model = new PeriodicScheduleModel_Gurobi();
-        double period = model.getNormalizedPeriod(g);
-
-        // remove the added edges
-        for (String e : addedEdgesList)
-          g.removeEdge(e);
-
-        // set he normalized period
-        g.normalizedPeriod = period;
-        System.out.println("Kopt found !! " + period);
-        return period;
-
-      }
-      default:
-        return -1;
-    }
-  }
-
-  public double computePeriodswithoutLoop(SDFGraph g, Method method) {
+  public double computeNormalizedPeriod(SDFGraph g, Method method) {
     switch (method) {
       case Algorithm: {
         // use one of the known algorithm to compute the optimal K
@@ -138,50 +143,38 @@ public class PeriodicSchedule_SDF {
     }
   }
 
-  public Fraction computePeriodFractionWithoutLoop(SDFGraph g, Method method) {
-    switch (method) {
-      case Algorithm: {
-        // use one of the known algorithm to compute the optimal K
-        return null;
-      }
-      case LinearProgram_Gurobi: {
-        System.out.println("computing Kopt !!");
-        // use the linear programming to compute the optimal K
-        KPeriodFraction_Gurobi model = new KPeriodFraction_Gurobi();
-
-        // PeriodicScheduleModel model = new PeriodicScheduleModel();
-        Fraction k = model.getNormalizedPeriod(g);
-
-        // set he normalized period
-        g.normalizedPeriod = model.period;
-        System.out.println("Kopt found !! " + g.normalizedPeriod);
-
-        return k;
-
-      }
-      default:
-        System.out.println("Only \"LinearProgram_Gurobi\" is aloud !");
-        return null;
-    }
-  }
-
   /**
-   * compute the maximum throughput of the graph</br>
-   * max Th = min (1/k*Z)
+   * computes the execution period of each actor and define the maximum throughput of the periodic schedule as MaxTh = min (1/k*Z)
+   * 
+   * @param graph
+   *          SDF graph
+   * @return the maximum throughput of the periodic schedule
    */
-  public double computeMaxThroughput(SDFGraph g) {
-    // get the maximum Z
-    Double Zmax = .0;
-    for (Actor a : g.actors.values())
-      if (a.normalizationValue > Zmax)
-        Zmax = a.normalizationValue;
-    // compute the maximum throughput : Th = 1/Kmin*Zmax
-    System.out.println("ZMax = " + Zmax);
-    return 1 / (g.normalizedPeriod * Zmax);
+  public Fraction computeActorsPeriod(SDFGraph graph) {
+    // get the normalized period of the graph
+    Fraction k = (Fraction) graph.getPropertyBean().getValue("normalizedPeriod");
+
+    // define the maximum execution period
+    Fraction maxW = Fraction.getFraction(0);
+
+    // define the execution period of each actor of the graph as w=k*z
+    for (SDFAbstractVertex actor : graph.vertexSet()) {
+      Fraction w = k.multiplyBy(Fraction.getFraction((int) actor.getPropertyBean().getValue("normalizedRate")));
+      actor.setPropertyValue("executionPeriod", w.doubleValue());
+
+      // test if the current execution period is greater than maxW
+      if (w.doubleValue() > maxW.doubleValue()) {
+        maxW = w.reduce();
+      }
+    }
+
+    // return the maximum throughput as 1/maxW
+    Fraction maxThroughput = Fraction.getFraction(maxW.getDenominator(), maxW.getNumerator());
+    return maxThroughput;
   }
 
   public double computeDurationOfIteration(SDFGraph g) {
-    Hashtable<String, Double> S = this.computeStartingTimes(g);
+    Hashtable<String, Double> S = this.computeActorsFirstExecutionStartDate(g);
     // Hashtable<String, Double> vertexFinishTime = new Hashtable<>(g.actors.size());
     double K = g.normalizedPeriod;
 
@@ -220,7 +213,7 @@ public class PeriodicSchedule_SDF {
    * @param g
    * @return
    */
-  public Hashtable<String, Double> computeStartingTimes(SDFGraph g) {
+  public Hashtable<String, Double> computeActorsFirstExecutionStartDate(SDFGraph g) {
 
     // compute starting times
     /*
