70f4c390d5ee7c6fb38d708eceaeaf943b105235
Deroui Hamza <hamza.deroui@insa-rennes.fr>
2017-07-27 11:21:14 +0200

    IBSDF consistency - RV computation

--- plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/deadlock/IBSDFConsistency.java
+++ plugins/org.ietr.preesm.evaluator/src/org/ietr/preesm/deadlock/IBSDFConsistency.java
@@ -1,9 +1,34 @@
 package org.ietr.preesm.deadlock;
 
+import org.ietr.dftools.algorithm.model.sdf.SDFAbstractVertex;
+import org.ietr.dftools.algorithm.model.sdf.SDFGraph;
+
 /**
  * @author hderoui
  *
  */
-public class IBSDFConsistency {
+public abstract class IBSDFConsistency {
 
+  /**
+   * @param graph
+   *          IBSDF graph
+   * @return true if consistent
+   */
+  public static boolean computeRV(SDFGraph graph) {
+    // step 1: compute the RV of the top graph
+    if (!SDFConsistency.computeRV(graph)) {
+      System.err.println("IBSDF RV computation : " + graph.getName() + " is deadlock !!");
+      return false;
+    } else {
+      // step 2: compute the RV of each subgraph
+      for (SDFAbstractVertex actor : graph.vertexSet()) {
+        if (actor.getGraphDescription() != null) {
+          if (!IBSDFConsistency.computeRV((SDFGraph) actor.getGraphDescription())) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+  }
 }
