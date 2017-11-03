baa05a9f0c8a6d4a2cc8d0c3bc89c3bf28f38a4e
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-11-03 11:09:17 +0100

    (PiMM) refactor subgraph connector visitor

--- plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/pimm/util/SubgraphConnectorVisitor.java
+++ plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/pimm/util/SubgraphConnectorVisitor.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.ietr.preesm.experiment.model.pimm.AbstractActor;
 import org.ietr.preesm.experiment.model.pimm.AbstractVertex;
 import org.ietr.preesm.experiment.model.pimm.Actor;
@@ -72,7 +73,6 @@ import org.ietr.preesm.experiment.model.pimm.Port;
 import org.ietr.preesm.experiment.model.pimm.Refinement;
 import org.ietr.preesm.experiment.model.pimm.RoundBufferActor;
 
-// TODO: Auto-generated Javadoc
 /**
  * Parse and connect hierarchical sub-{@link PiGraph} to a top level {@link PiGraph}.
  *
@@ -110,10 +110,12 @@ public class SubgraphConnectorVisitor extends PiMMDefaultVisitor {
     pg.accept(this);
     // Replace Actors with refinement by PiGraphs in pg and all its
     // subgraphs
-    for (final PiGraph key : this.graphReplacements.keySet()) {
-      for (final SubgraphConnectorVisitor.ActorByGraphReplacement r : this.graphReplacements.get(key)) {
-        key.getVertices().remove(r.toBeRemoved);
-        key.getVertices().add(r.toBeAdded);
+
+    for (final Entry<PiGraph, List<ActorByGraphReplacement>> e : this.graphReplacements.entrySet()) {
+      for (final ActorByGraphReplacement r : e.getValue()) {
+        e.getKey().getVertices().remove(r.toBeRemoved);
+        e.getKey().getVertices().add(r.toBeAdded);
+
       }
     }
   }
@@ -151,7 +153,7 @@ public class SubgraphConnectorVisitor extends PiMMDefaultVisitor {
       // Connect all Fifos and Dependencies incoming into a and outgoing
       // from a in order to make them incoming into innerGraph and
       // outgoing from innerGraph instead
-      reconnectPiGraph(a, innerGraph);
+      SubgraphReconnector.reconnectPiGraph(a, innerGraph);
 
       this.currentActor = innerGraph;
       innerGraph.accept(this);
@@ -161,93 +163,6 @@ public class SubgraphConnectorVisitor extends PiMMDefaultVisitor {
         this.graphReplacements.put(this.currentGraph, new ArrayList<ActorByGraphReplacement>());
       }
       this.graphReplacements.get(this.currentGraph).add(replacement);
-    }
-  }
-
-  /**
-   * Reconnect pi graph.
-   *
-   * @param a
-   *          the a
-   * @param pg
-   *          the pg
-   */
-  /*
-   * Connect all the ports of the PiGraph to the Fifos and Dependencies connected to the ports of the Actor
-   */
-  private void reconnectPiGraph(final Actor a, final PiGraph pg) {
-    boolean found = false;
-    for (final DataInputPort dip1 : a.getDataInputPorts()) {
-      found = false;
-      for (final DataInputPort dip2 : pg.getDataInputPorts()) {
-        if (dip1.getName().equals(dip2.getName())) {
-          final Fifo fifo = dip1.getIncomingFifo();
-          dip2.setIncomingFifo(fifo);
-          fifo.setTargetPort(dip2);
-
-          dip2.setExpression(dip1.getExpression());
-          dip2.setAnnotation(dip1.getAnnotation());
-
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        throw new RuntimeException("PiGraph" + pg.getName() + "does not have a corresponding DataInputPort for " + dip1.getName() + " of Actor " + a.getName());
-      }
-    }
-    for (final DataOutputPort dop1 : a.getDataOutputPorts()) {
-      found = false;
-      for (final DataOutputPort dop2 : pg.getDataOutputPorts()) {
-        if (dop1.getName().equals(dop2.getName())) {
-          final Fifo fifo = dop1.getOutgoingFifo();
-          dop2.setOutgoingFifo(fifo);
-          fifo.setSourcePort(dop2);
-
-          dop2.setExpression(dop1.getExpression());
-          dop2.setAnnotation(dop1.getAnnotation());
-
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        throw new RuntimeException(
-            "PiGraph" + pg.getName() + "does not have a corresponding DataOutputPort for " + dop1.getName() + " of Actor " + a.getName());
-      }
-    }
-    for (final ConfigInputPort cip1 : a.getConfigInputPorts()) {
-      found = false;
-      for (final ConfigInputPort cip2 : pg.getConfigInputPorts()) {
-        if (cip1.getName().equals(cip2.getName())) {
-          final Dependency dep = cip1.getIncomingDependency();
-          cip2.setIncomingDependency(dep);
-          dep.setGetter(cip2);
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        throw new RuntimeException(
-            "PiGraph" + pg.getName() + " does not have a corresponding ConfigInputPort for " + cip1.getName() + " of Actor " + a.getName());
-      }
-    }
-    for (final ConfigOutputPort cop1 : a.getConfigOutputPorts()) {
-      found = false;
-      for (final ConfigOutputPort cop2 : pg.getConfigOutputPorts()) {
-        if (cop1.getName().equals(cop2.getName())) {
-          for (final Dependency dep : cop1.getOutgoingDependencies()) {
-            cop2.getOutgoingDependencies().add(dep);
-            dep.setSetter(cop2);
-          }
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        throw new RuntimeException(
-            "PiGraph" + pg.getName() + "does not have a corresponding ConfigOutputPort for " + cop1.getName() + " of Actor " + a.getName());
-      }
     }
   }
 
@@ -595,10 +510,10 @@ public class SubgraphConnectorVisitor extends PiMMDefaultVisitor {
   public class ActorByGraphReplacement {
 
     /** The to be removed. */
-    public Actor toBeRemoved;
+    public final Actor toBeRemoved;
 
     /** The to be added. */
-    public PiGraph toBeAdded;
+    public final PiGraph toBeAdded;
 
     /**
      * Instantiates a new actor by graph replacement.
