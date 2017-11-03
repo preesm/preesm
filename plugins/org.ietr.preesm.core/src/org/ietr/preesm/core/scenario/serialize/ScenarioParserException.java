89a6b7ea8875d4446a4350ab4641284830ba1785
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-11-03 10:25:17 +0100

    (UI) Add special exception related to scenario parser

---
+++ plugins/org.ietr.preesm.core/src/org/ietr/preesm/core/scenario/serialize/ScenarioParserException.java
@@ -0,0 +1,19 @@
+package org.ietr.preesm.core.scenario.serialize;
+
+/**
+ *
+ * @author anmorvan
+ *
+ */
+public class ScenarioParserException extends RuntimeException {
+
+  public ScenarioParserException(String string, Exception e) {
+    super(string, e);
+  }
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+}
