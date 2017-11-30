82fd69307aa7ed74e0e1a7f17ab9d50435eb8472
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-11-30 10:55:12 +0100

    [TESTS] Add spider test

---
+++ tests/org.ietr.preesm.test.it/resources/org.ietr.preesm.spider.sobel/Code/include/nbSliceSetter.h
@@ -0,0 +1,19 @@
+/*
+	============================================================================
+	Name        : nbSliceSetter.h
+	Author      : jheulot
+	Version     :
+	Copyright   : CECILL-C
+	Description : Set nbSlice Parameter
+	============================================================================
+*/
+
+#ifndef NB_SLICE_SETTER
+#define NB_SLICE_SETTER
+
+#include <spider.h>
+
+void initNbSliceSetter(Param height);
+void nbSliceSetter(Param height, Param* nbSlice);
+
+#endif//NB_SLICE_SETTER
