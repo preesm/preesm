7c208e71a507e65ce78ebd923c061bd28ea2e1f9
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-12-21 15:48:26 +0100

    (UI) Create dedicated UI plugin for SDF

--- plugins/org.ietr.preesm.ui.sdf/src/org/ietr/preesm/ui/wizard/NewGraphMLWizard.java
+++ plugins/org.ietr.preesm.ui.sdf/src/org/ietr/preesm/ui/wizard/NewGraphMLWizard.java
@@ -35,7 +35,7 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-package org.ietr.preesm.ui;
+package org.ietr.preesm.ui.wizard;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.dialogs.MessageDialog;
