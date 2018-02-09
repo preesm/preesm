12f44dfb0b948756067e22c9d7634179d8135908
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-09 15:59:55 +0100

    (PAPI) Add main parser class, test and test input

---
+++ test-fragments/org.ietr.preesm.core.test/src/org/ietr/preesm/core/test/PapiConfigParserTest.java
@@ -0,0 +1,16 @@
+package org.ietr.preesm.core.test;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author anmorvan
+ *
+ */
+public class PapiConfigParserTest {
+
+  @Test
+  public void testParser() {
+
+  }
+}
