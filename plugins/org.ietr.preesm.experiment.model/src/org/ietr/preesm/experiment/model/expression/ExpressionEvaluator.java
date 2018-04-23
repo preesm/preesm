bd68484ec9a6d6ec1457aba469e96d2dafb9073a
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-04-23 16:10:46 +0200

    [RELENG] Fix headers

--- plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/expression/ExpressionEvaluator.java
+++ plugins/org.ietr.preesm.experiment.model/src/org/ietr/preesm/experiment/model/expression/ExpressionEvaluator.java
@@ -3,6 +3,7 @@
  *
  * Alexandre Honorat <ahonorat@insa-rennes.fr> (2018)
  * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017 - 2018)
+ * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2018)
  *
  * This software is a computer program whose purpose is to help prototyping
  * parallel applications using dataflow formalism.
