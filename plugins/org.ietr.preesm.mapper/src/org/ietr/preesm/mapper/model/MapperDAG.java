7b9b022051d4f6de3bf2d9cbcfafbca2031d1d83
Florian <farresti@insa-rennes.fr>
2018-05-02 15:09:25 +0200

    (MapperDAG) cloning the edge aggregate properly

--- plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/model/MapperDAG.java
+++ plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/model/MapperDAG.java
@@ -42,10 +42,12 @@ package org.ietr.preesm.mapper.model;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import org.ietr.dftools.algorithm.model.AbstractEdge;
 import org.ietr.dftools.algorithm.model.AbstractGraph;
 import org.ietr.dftools.algorithm.model.dag.DAGEdge;
 import org.ietr.dftools.algorithm.model.dag.DAGVertex;
 import org.ietr.dftools.algorithm.model.dag.DirectedAcyclicGraph;
+import org.ietr.dftools.algorithm.model.dag.EdgeAggregate;
 import org.ietr.dftools.algorithm.model.sdf.SDFAbstractVertex;
 import org.ietr.dftools.algorithm.model.sdf.SDFGraph;
 import org.ietr.preesm.mapper.model.property.DAGMappings;
@@ -200,6 +202,15 @@ public class MapperDAG extends DirectedAcyclicGraph {
       newEdge.setInit(origEdge.getInit().clone());
       newEdge.setTiming(origEdge.getTiming().clone());
       newEdge.copyProperties(origEdge);
+      // Updating the aggregate list with proper reference
+      newEdge.setAggregate(new EdgeAggregate());
+      for (final AbstractEdge<?, ?> e : origEdge.getAggregate()) {
+        final DAGEdge edge = (DAGEdge) e;
+        final DAGEdge newAggEdge = new DAGEdge();
+        newAggEdge.copyProperties(edge);
+        newAggEdge.setContainingEdge(newEdge);
+        newEdge.getAggregate().add(newAggEdge);
+      }
     }
     newDAG.copyProperties(this);
 
