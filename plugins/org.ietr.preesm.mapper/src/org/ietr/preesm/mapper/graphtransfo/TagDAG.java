c86a8c49409a110955080f9628661e1024b04eaa
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-05-17 08:43:13 +0200

    [RELENG] Fix headers

--- plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/graphtransfo/TagDAG.java
+++ plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/graphtransfo/TagDAG.java
@@ -1,7 +1,7 @@
 /**
  * Copyright or © or Copr. IETR/INSA - Rennes (2008 - 2018) :
  *
- * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017)
+ * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017 - 2018)
  * Clément Guy <clement.guy@insa-rennes.fr> (2014)
  * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2018)
  * Jonathan Piat <jpiat@laas.fr> (2008 - 2011)
