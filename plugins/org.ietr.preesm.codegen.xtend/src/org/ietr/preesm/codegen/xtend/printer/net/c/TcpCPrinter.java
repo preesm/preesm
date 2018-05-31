d877095a4388b2a3c5b5b92475a31d67f6606bae
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-05-31 09:18:45 +0200

    (CodeGen) First version of TcpCodegen - simple fork

---
+++ plugins/org.ietr.preesm.codegen.xtend/src/org/ietr/preesm/codegen/xtend/printer/net/c/TcpCPrinter.java
@@ -0,0 +1,12 @@
+package org.ietr.preesm.codegen.xtend.printer.net.c;
+
+import org.ietr.preesm.codegen.xtend.printer.c.CPrinter;
+
+/**
+ *
+ * @author anmorvan
+ *
+ */
+public class TcpCPrinter extends CPrinter {
+
+}
