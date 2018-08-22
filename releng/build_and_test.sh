55ac75ea196561c02bceabe67b8ce0654dd92616
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-08-22 16:02:46 +0200

    [RELENG] Update build script

--- releng/build_and_test.sh
+++ releng/build_and_test.sh
@@ -43,9 +43,9 @@ time (
   (cd $DIR && mvn -U -e -C -B -V -P doUpdateSite -Dtycho.mode=maven dependency:go-offline) || exit 22
   #CHECKSTYLE (offline)
   echo ""
-  echo "Checkstyle (offline)"
+  echo "Checkstyle"
   echo ""
-  (cd $DIR && mvn --offline -e -C -B -V -Dtycho.mode=maven checkstyle:check) || exit 3
+  (cd $DIR && mvn -e -C -B -V -Dtycho.mode=maven checkstyle:check) || exit 3
   #fetch P2 deps
   echo ""
   echo "Fetch P2 Deps"
@@ -53,25 +53,33 @@ time (
   (cd $DIR && mvn -U -e -C -B -V -P doUpdateSite help:help) || exit 4
   #clean (offline)
   echo ""
-  echo "Clean (offline)"
+  echo "Clean"
   echo ""
-  (cd $DIR && mvn --offline -e -C -B -V -P doUpdateSite -Dtycho.mode=maven clean) || exit 5
+  (cd $DIR && mvn -e -C -B -V -P doUpdateSite -Dtycho.mode=maven clean) || exit 5
   #build code and package (offline, no tests)
   echo ""
-  echo "Build & Package (offline)"
+  echo "Build & Package"
   echo ""
   (cd $DIR && mvn -e -C -B -V package -fae -Dmaven.test.skip=true) || exit 6
   # build and run tests (offline)
-  echo ""
-  echo "Test all & Run Sonar (offline)"
-  echo ""
-  (cd $DIR && mvn -e -C -B -V verify -fae) || exit 7
+  if [ -z ${TRAVIS+x} ]; then
+    echo ""
+    echo "Test all & Run Sonar"
+    echo ""
+    (cd $DIR && mvn -e -C -B -V verify sonar:sonar -fae) || exit 7
+  else
+    echo ""
+    echo "Test all"
+    echo ""
+    (cd $DIR && mvn -e -C -B -V verify -fae) || exit 7
+  fi
   #package update site (offline, no tests)
   echo ""
-  echo "Package update site (offline)"
+  echo "Package update site"
   echo ""
   (cd $DIR && mvn -e -C -B -V -P doUpdateSite package -fae -Dmaven.test.skip=true) || exit 8
 )
+
 
 if [ -x /usr/bin/Xvfb ]; then
   kill -2 $XVFBPID
