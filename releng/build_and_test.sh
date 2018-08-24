a839e6fcd5b57ab24936a52ea8ef2d664fd0531f
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-08-24 09:34:00 +0200

    [RELENG] Update travis scripts

--- releng/build_and_test.sh
+++ releng/build_and_test.sh
@@ -2,8 +2,27 @@
 
 DIR=$(cd `dirname $0` && echo `git rev-parse --show-toplevel`)
 
-#fast version:
-#(cd $DIR && mvn -U -e -C -B -V -P doUpdateSite clean verify sonar:sonar -fae)
+FETCH=NO
+FAST=NO
+CHECK=NO
+if [ ! -z ${1+x} ]; then
+  if [ "$1" == "--fetch" ]; then
+    FETCH=YES
+  fi
+  if [ "$1" == "--check" ]; then
+    CHECK=YES
+  fi
+  if [ "$1" == "--fast" ]; then
+    FAST=YES
+  fi
+fi
+
+# enable Sonar on Travis
+if [ ! -z ${TRAVIS+x} ]; then
+  SONAR="sonar:sonar"
+else
+  SONAR=
+fi
 
 function find_free_display_number {
   USEDXDISPLAYS=`find /tmp -maxdepth 1 -type f -name ".X*-lock" | rev | cut -d"/" -f 1 | colrm 1 5 | rev | colrm 1 2`
@@ -27,6 +46,35 @@ if [ -x /usr/bin/Xvfb ]; then
   export DISPLAY=:${XDN}.0
   /usr/bin/Xvfb :${XDN} -ac -screen 0 1280x1024x16&
   XVFBPID=$!
+fi
+
+
+#fetch version:
+if [ "$FETCH" == "YES" ]; then
+  echo "Fetch dependencies ..."
+  time (
+    (cd $DIR && ./releng/fetch-rcptt-runner.sh)
+    (cd $DIR && mvn -U -e -C -B -P doUpdateSite -Dtycho.mode=maven dependency:go-offline)
+    (cd $DIR && mvn -U -e -C -B -P doUpdateSite help:help)
+  )
+  exit 0
+fi
+
+#check version:
+if [ "$CHECK" == "YES" ]; then
+  echo "Check code ..."
+  time (cd $DIR && mvn  -e -C -B -P doUpdateSite -Dtycho.mode=maven checkstyle:check)
+  exit 0
+fi
+
+#fast version:
+if [ "$FAST" == "YES" ]; then
+  echo "Fast build ..."
+  time (
+    (cd $DIR && ./releng/fetch-rcptt-runner.sh)
+    (cd $DIR && mvn -e -C -B clean verify ${SONAR} -fae)
+  )
+  exit 0
 fi
 
 time (
@@ -62,17 +110,10 @@ time (
   echo ""
   (cd $DIR && mvn -e -C -B -V package -fae -Dmaven.test.skip=true) || exit 6
   # build and run tests (offline)
-  if [ ! -z ${TRAVIS+x} ]; then
-    echo ""
-    echo "Test all & Run Sonar"
-    echo ""
-    (cd $DIR && mvn -e -C -B -V verify sonar:sonar -fae) || exit 71
-  else
-    echo ""
-    echo "Test all"
-    echo ""
-    (cd $DIR && mvn -e -C -B -V verify -fae) || exit 72
-  fi
+  echo ""
+  echo "Test all & Run Sonar"
+  echo ""
+  (cd $DIR && mvn -e -C -B -V verify ${SONAR} -fae) || exit 7
   #package update site (offline, no tests)
   echo ""
   echo "Package update site"
