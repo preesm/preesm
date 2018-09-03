f4de7b7f88f1acdebb6dbe689de0304bc76a84a9
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-09-03 14:08:11 +0200

    (Mapper) Refactor; Only consider vertices with property

--- plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/optimizer/SyncGraph.java
+++ plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/optimizer/SyncGraph.java
@@ -5,8 +5,12 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 import org.ietr.dftools.algorithm.model.dag.DAGVertex;
 import org.ietr.dftools.architecture.slam.ComponentInstance;
+import org.ietr.dftools.architecture.slam.attributes.Parameter;
+import org.ietr.preesm.core.architecture.route.MessageRouteStep;
+import org.ietr.preesm.mapper.AbstractMappingFromDAG;
 import org.ietr.preesm.mapper.PreesmMapperException;
 import org.ietr.preesm.mapper.model.special.SendVertex;
 import org.ietr.preesm.mapper.model.special.TransferVertex;
@@ -17,7 +21,7 @@ import org.jgrapht.io.DOTExporter;
 /**
  *
  */
-public class SyncGraph extends org.jgrapht.graph.DirectedAcyclicGraph<ConsecutiveTransfersGroup, String> {
+public class SyncGraph extends org.jgrapht.graph.SimpleDirectedWeightedGraph<ConsecutiveTransfersGroup, String> {
 
   private static final long serialVersionUID = -5902732205999536143L;
 
@@ -25,13 +29,49 @@ public class SyncGraph extends org.jgrapht.graph.DirectedAcyclicGraph<Consecutiv
     super(String.class);
   }
 
+  private static boolean isSynchronizationTransfer(final TransferVertex currentVertex) {
+    boolean isSyncWait = true;
+    // Check if all comm steps are zero copy.
+    final List<ComponentInstance> communicationSteps = ((MessageRouteStep) currentVertex.getRouteStep()).getNodes();
+    for (final ComponentInstance component : communicationSteps) {
+      boolean isZeroCopy = false;
+      for (final Parameter p : component.getParameters()) {
+        isZeroCopy |= p.getKey().equals(RedundantSynchronizationCleaner.ZERO_COPY)
+            && p.getValue().equals(AbstractMappingFromDAG.VALUE_TRUE);
+      }
+      isSyncWait &= isZeroCopy;
+    }
+    return isSyncWait;
+  }
+
   /**
    *
    */
-  public static final SyncGraph buildUnecessaryEdges(final ConsecutiveTransfersMap groupMap) {
-
+  public static final SyncGraph buildEdges(final ConsecutiveTransfersMap groupMap) {
     final SyncGraph graph = new SyncGraph();
-    // 1- add vertices (one per group, regardless the component)
+    buildSeqSyncEdges(groupMap, graph);
+    buildDagSyncEdges(groupMap, graph);
+    return graph;
+  }
+
+  private static void buildDagSyncEdges(final ConsecutiveTransfersMap groupMap, final SyncGraph graph) {
+    for (final Entry<ComponentInstance, ConsecutiveTransfersList> e : groupMap.entrySet()) {
+      final ConsecutiveTransfersList groupList = e.getValue();
+      for (final ConsecutiveTransfersGroup sourceGroup : groupList) {
+        for (final TransferVertex vertex : sourceGroup) {
+          if (vertex instanceof SendVertex && isSynchronizationTransfer(vertex)) {
+            final DAGVertex receiver = vertex.outgoingEdges().iterator().next().getTarget();
+            final ConsecutiveTransfersGroup targetGroup = groupMap.findGroup(receiver);
+            final String comName = "com_" + SyncGraph.getName(sourceGroup) + "_" + SyncGraph.getName(targetGroup);
+            graph.addEdge(sourceGroup, targetGroup, comName);
+            graph.setEdgeWeight(comName, +1);
+          }
+        }
+      }
+    }
+  }
+
+  private static void buildSeqSyncEdges(final ConsecutiveTransfersMap groupMap, final SyncGraph graph) {
     for (final Entry<ComponentInstance, ConsecutiveTransfersList> e : groupMap.entrySet()) {
       final ConsecutiveTransfersList groupList = e.getValue();
       final ComponentInstance component = e.getKey();
@@ -39,32 +79,37 @@ public class SyncGraph extends org.jgrapht.graph.DirectedAcyclicGraph<Consecutiv
         final ConsecutiveTransfersGroup group = groupList.get(i);
         graph.addVertex(group);
         for (int j = 0; j < i; j++) {
-          graph.addEdge(groupList.get(j), group, "seq" + component.getInstanceName() + "_" + j + "_" + i);
+          final String comName = "seq" + component.getInstanceName() + "_" + j + "_" + i;
+          graph.addEdge(groupList.get(j), group, comName);
+          graph.setEdgeWeight(comName, -1);
         }
       }
     }
-    final List<String> seqSyncEdges = new ArrayList<>(graph.edgeSet());
+  }
 
-    // 3- add syncs between all elements that actually share sync in the DAG
-    for (final Entry<ComponentInstance, ConsecutiveTransfersList> e : groupMap.entrySet()) {
-      final ConsecutiveTransfersList groupList = e.getValue();
-      for (final ConsecutiveTransfersGroup sourceGroup : groupList) {
-        for (final TransferVertex vertex : sourceGroup) {
-          if (vertex instanceof SendVertex) {
-            final DAGVertex receiver = vertex.outgoingEdges().iterator().next().getTarget();
-            final ConsecutiveTransfersGroup targetGroup = groupMap.findGroup(receiver);
-            final String comName = "com_" + SyncGraph.getName(sourceGroup) + "_" + SyncGraph.getName(targetGroup);
-            graph.addEdge(sourceGroup, targetGroup, comName);
-          }
-        }
+  /**
+   *
+   */
+  public final List<String> getSeqSyncEdges() {
+    final Set<String> edgeSet = this.edgeSet();
+    final List<String> seqSyncEdges = new ArrayList<>();
+    edgeSet.forEach(s -> {
+      if (this.getEdgeWeight(s) < 0) {
+        seqSyncEdges.add(s);
       }
-    }
+    });
+    return seqSyncEdges;
+  }
 
-    final SyncGraph reducedGraph = (SyncGraph) graph.clone();
+  /**
+   *
+   */
+  public final SyncGraph computeRedundantEdges() {
+    final SyncGraph reducedGraph = (SyncGraph) this.clone();
     TransitiveReduction.INSTANCE.reduce(reducedGraph);
+    final SyncGraph graph = (SyncGraph) this.clone();
     graph.removeAllEdges(reducedGraph.edgeSet());
-    graph.removeAllEdges(seqSyncEdges);
-
+    graph.removeAllEdges(getSeqSyncEdges());
     return graph;
   }
 
@@ -78,7 +123,7 @@ public class SyncGraph extends org.jgrapht.graph.DirectedAcyclicGraph<Consecutiv
   /**
    *
    */
-  class VertexNameProvider implements ComponentNameProvider<ConsecutiveTransfersGroup> {
+  private static class VertexNameProvider implements ComponentNameProvider<ConsecutiveTransfersGroup> {
     @Override
     public String getName(final ConsecutiveTransfersGroup group) {
       return SyncGraph.getName(group);
@@ -88,16 +133,11 @@ public class SyncGraph extends org.jgrapht.graph.DirectedAcyclicGraph<Consecutiv
   /**
    *
    */
-  class EdgeNameProvider implements ComponentNameProvider<String> {
+  private static class EdgeNameProvider implements ComponentNameProvider<String> {
     @Override
     public String getName(final String str) {
       return str;
     }
-  }
-
-  @Override
-  public String toString() {
-    return super.toString();
   }
 
   /**
