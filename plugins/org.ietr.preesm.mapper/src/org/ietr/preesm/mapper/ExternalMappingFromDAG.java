b5a2b89c8f42f93a6af9823084e6a64d8035a03e
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-09-26 11:03:10 +0200

    (Mapper) Add sample demo mapping

---
+++ plugins/org.ietr.preesm.mapper/src/org/ietr/preesm/mapper/ExternalMappingFromDAG.java
@@ -0,0 +1,59 @@
+package org.ietr.preesm.mapper;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.ietr.dftools.algorithm.iterators.TopologicalDAGIterator;
+import org.ietr.dftools.architecture.slam.ComponentInstance;
+import org.ietr.dftools.architecture.slam.Design;
+import org.ietr.preesm.core.scenario.PreesmScenario;
+import org.ietr.preesm.mapper.abc.impl.latency.LatencyAbc;
+import org.ietr.preesm.mapper.abc.taskscheduling.AbstractTaskSched;
+import org.ietr.preesm.mapper.algo.list.InitialLists;
+import org.ietr.preesm.mapper.model.MapperDAG;
+import org.ietr.preesm.mapper.model.MapperDAGVertex;
+import org.ietr.preesm.mapper.params.AbcParameters;
+
+/**
+ *
+ * @author anmorvan
+ *
+ */
+public class ExternalMappingFromDAG extends AbstractMappingFromDAG {
+
+  public static final String SCHEDULE_FILE = "SCHEDULE_FILE";
+
+  @Override
+  public Map<String, String> getDefaultParameters() {
+    final Map<String, String> params = new LinkedHashMap<>();
+    params.put(SCHEDULE_FILE, "/schedule.json");
+    return params;
+  }
+
+  @Override
+  protected LatencyAbc schedule(final Map<String, Object> outputs, final Map<String, String> parameters,
+      final InitialLists initial, final PreesmScenario scenario, final AbcParameters abcParams, final MapperDAG dag,
+      final Design architecture, final AbstractTaskSched taskSched) {
+
+    final LatencyAbc abc = LatencyAbc.getInstance(abcParams, dag, architecture, scenario);
+    final String mainOperatorName = scenario.getSimulationManager().getMainOperatorName();
+    final ComponentInstance mainOperator = architecture.getComponentInstance(mainOperatorName);
+    final List<ComponentInstance> componentInstances = architecture.getComponentInstances();
+
+    final TopologicalDAGIterator topologicalDAGIterator = new TopologicalDAGIterator(dag);
+    while (topologicalDAGIterator.hasNext()) {
+      final MapperDAGVertex next = (MapperDAGVertex) topologicalDAGIterator.next();
+      if (abc.isMapable(next, mainOperator, false)) {
+        abc.map(next, mainOperator, true, false);
+      } else {
+        final ComponentInstance compatibleComponent = componentInstances.stream()
+            .filter(c -> abc.isMapable(next, c, false)).findFirst().orElseThrow(() -> new RuntimeException(""));
+        abc.map(next, compatibleComponent, true, false);
+      }
+
+    }
+
+    return abc;
+  }
+
+}
