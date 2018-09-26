6cdd75273b6d062620ee6e133e58091464ebe2c2
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-09-26 14:30:57 +0200

    (Refactor) Component comparator properly compares tailing numbers

--- plugins/org.ietr.preesm.core/src/org/ietr/preesm/core/architecture/util/DesignTools.java
+++ plugins/org.ietr.preesm.core/src/org/ietr/preesm/core/architecture/util/DesignTools.java
@@ -75,8 +75,24 @@ public class DesignTools {
      * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
      */
     @Override
-    public int compare(final ComponentInstance o1, final ComponentInstance o2) {
-      return o1.getInstanceName().compareTo(o2.getInstanceName());
+    public int compare(ComponentInstance cb1, ComponentInstance cb2) {
+      final String o1 = cb1.getInstanceName();
+      final String o2 = cb2.getInstanceName();
+
+      final String o1StringPart = o1.replaceAll("\\d", "");
+      final String o2StringPart = o2.replaceAll("\\d", "");
+
+      if (o1StringPart.equalsIgnoreCase(o2StringPart)) {
+        return extractInt(o1) - extractInt(o2);
+      } else {
+        return o1.compareTo(o2);
+      }
+    }
+
+    int extractInt(String s) {
+      String num = s.replaceAll("\\D", "");
+      // return 0 if no digits found
+      return num.isEmpty() ? 0 : Integer.parseInt(num);
     }
 
   }
