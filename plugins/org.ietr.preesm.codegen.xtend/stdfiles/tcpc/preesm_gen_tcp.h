a9e80c9ecf28af566950daeea61d8e5c196fa8ab
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-09-26 16:22:35 +0200

    [RELENG] Update .mailmap

--- plugins/org.ietr.preesm.codegen.xtend/stdfiles/tcpc/preesm_gen_tcp.h
+++ plugins/org.ietr.preesm.codegen.xtend/stdfiles/tcpc/preesm_gen_tcp.h
@@ -32,7 +32,6 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-
 #ifndef _PREESM_GEN_TCP_H
 #define _PREESM_GEN_TCP_H
 
