2f4adabb896ec0a01187a85b2516bd44009ef30c
Alexandre Honorat <alexandre.honorat@insa-rennes.fr>
2018-11-05 17:25:04 +0100

    Adding selection rate parameter to the periods checker.

--- plugins/org.ietr.preesm.pimm.algorithm/src/org/ietr/preesm/pimm/algorithm/checker/periods/PeriodsPreschedulingChecker.java
+++ plugins/org.ietr.preesm.pimm.algorithm/src/org/ietr/preesm/pimm/algorithm/checker/periods/PeriodsPreschedulingChecker.java
@@ -1,5 +1,6 @@
 package org.ietr.preesm.pimm.algorithm.checker.periods;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -9,6 +10,9 @@ import org.ietr.dftools.workflow.elements.Workflow;
 import org.ietr.dftools.workflow.implement.AbstractTaskImplementation;
 import org.ietr.dftools.workflow.implement.AbstractWorkflowNodeImplementation;
 import org.ietr.preesm.core.scenario.PreesmScenario;
+import org.ietr.preesm.experiment.model.pimm.AbstractActor;
+import org.ietr.preesm.experiment.model.pimm.Actor;
+import org.ietr.preesm.experiment.model.pimm.PeriodicElement;
 import org.ietr.preesm.experiment.model.pimm.PiGraph;
 
 /**
@@ -18,6 +22,19 @@ import org.ietr.preesm.experiment.model.pimm.PiGraph;
  *
  */
 public class PeriodsPreschedulingChecker extends AbstractTaskImplementation {
+
+  /**
+   * Identify the parameter to get the rate of periodic actors to analyze.
+   */
+  public static final String SELECTION_RATE = "Selection rate (%)";
+
+  /**
+   * By default all actors are analyzed.
+   */
+  public static final String DEFAULT_SELECTION_RATE = "100";
+
+  private static final String GENERIC_RATE_ERROR = "Periodic actors selection rate "
+      + "must be an integer between 1 and 100 (%), instead of: ";
 
   @Override
   public Map<String, Object> execute(Map<String, Object> inputs, Map<String, String> parameters,
@@ -31,6 +48,38 @@ public class PeriodsPreschedulingChecker extends AbstractTaskImplementation {
       throw new WorkflowException("This task must be called with a flatten PiMM graph, abandon.");
     }
 
+    final String rateStr = parameters.get(SELECTION_RATE);
+    try {
+      int rate = Integer.parseInt(rateStr);
+      if (rate < 1 || rate > 100) {
+        throw new WorkflowException(GENERIC_RATE_ERROR + rate + ".");
+      }
+    } catch (NumberFormatException e) {
+      throw new WorkflowException(GENERIC_RATE_ERROR + rateStr + ".", e);
+    }
+
+    final Map<Actor, Long> periodicActors = new HashMap<>();
+    for (final AbstractActor absActor : graph.getActors()) {
+      if (absActor instanceof Actor && absActor instanceof PeriodicElement) {
+        final Actor actor = (Actor) absActor;
+        if (!actor.isHierarchical() && !actor.isConfigurationActor()) {
+          final long period = actor.getPeriod().evaluate();
+          if (period > 0) {
+            periodicActors.put(actor, period);
+          }
+        }
+      }
+    }
+
+    // 1. find all actor w/o incoming edges and all other with incoming edges
+    // 2. perform heuristic to select periodic nodes
+    // 3. for each selected periodic node for nblf:
+    // _a compute subgraph
+    // _b compute nblf
+    // 4. for each selected periodic node for nbff:
+    // _a compute subgraph
+    // _b compute nbff
+
     final Map<String, Object> output = new LinkedHashMap<>();
     output.put(AbstractWorkflowNodeImplementation.KEY_PI_GRAPH, graph);
     return output;
@@ -38,7 +87,9 @@ public class PeriodsPreschedulingChecker extends AbstractTaskImplementation {
 
   @Override
   public Map<String, String> getDefaultParameters() {
-    return new LinkedHashMap<>();
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put(SELECTION_RATE, DEFAULT_SELECTION_RATE);
+    return parameters;
   }
 
   @Override
