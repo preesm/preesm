3e1d5de245796a8273335b5afae8e1f970c5ec3b
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2019-01-21 13:37:52 +0100

    [RELENG] Add property for rcptt runner

--- releng/fetch-rcptt-runner.sh
+++ releng/fetch-rcptt-runner.sh
@@ -12,11 +12,13 @@
 DIR=$(cd `dirname $0` && echo `git rev-parse --show-toplevel`)
 
 MIRROR=$(cd ${DIR} && cat pom.xml | grep -o "<eclipse.mirror>\(.*\)</eclipse.mirror>" | sed 's#<eclipse.mirror>\(.*\)</eclipse.mirror>#\1#g')
-RCPTTVER=$(cd ${DIR} && cat pom.xml | grep -o "<rcptt-version>\(.*\)</rcptt-version>" | sed 's#<rcptt-version>\(.*\)</rcptt-version>#\1#g')
+RCPTTVER=$(cd ${DIR} && cat pom.xml | grep -o "<rcptt-runner-version>\(.*\)</rcptt-runner-version>" | sed 's#<rcptt-runner-version>\(.*\)</rcptt-runner-version>#\1#g')
 
 # test if rcptt runner is already installed (in default M2 repo)
 if [ ! -e "${HOME}/.m2/repository/org/eclipse/rcptt/runner/rcptt.runner/${RCPTTVER}/rcptt.runner-${RCPTTVER}.zip" ]; then
-  wget ${MIRROR}/rcptt/release/${RCPTTVER}/runner/rcptt.runner-${RCPTTVER}.zip -O rcptt.runner-${RCPTTVER}.zip
+
+  URLVER=$(echo ${RCPTTVER} | sed -e 's#-#/#g')
+  wget ${MIRROR}/rcptt/milestone/${URLVER}/runner/rcptt.runner-${RCPTTVER}.zip -O rcptt.runner-${RCPTTVER}.zip
   mvn -Dtycho.mode=maven install:install-file -Dfile=rcptt.runner-${RCPTTVER}.zip -DgroupId=org.eclipse.rcptt.runner -DartifactId=rcptt.runner -Dversion=${RCPTTVER} -Dpackaging=zip
   rm rcptt.runner-${RCPTTVER}.zip
 fi
