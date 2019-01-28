c8481aec3ff8194c093acc2c48a37991888a8aad
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2019-01-28 13:17:15 +0100

    [RELENG] Fix headers

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/memory/script/Buffer.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/memory/script/Buffer.java
@@ -3,7 +3,7 @@
  *
  * Alexandre Honorat <ahonorat@insa-rennes.fr> (2018)
  * Alexandre Honorat <alexandre.honorat@insa-rennes.fr> (2019)
- * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2018)
+ * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2018 - 2019)
  * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2018)
  *
  * This software is a computer program whose purpose is to help prototyping
