bc964de99891fbe30d8958d929eb711e5f01c983
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-04-03 17:11:03 +0200

    (Commons) Add Java reflection utility classes

---
+++ tests/org.preesm.tests.framework/src/org/preesm/commons/test/ReflectionUtilTest.java
@@ -0,0 +1,23 @@
+package org.preesm.commons.test;
+
+import java.util.Collection;
+import org.junit.Test;
+import org.preesm.commons.ReflectionUitl;
+import org.preesm.workflow.implement.AbstractTaskImplementation;
+
+/**
+ *
+ * @author anmorvan
+ *
+ */
+public class ReflectionUtilTest {
+
+  @Test
+  public void testRefelctionClasses() {
+    final Collection<Class<?>> lookupChildClassesOf = ReflectionUitl.lookupChildClassesOf("org.preesm.workflow.tasks",
+        AbstractTaskImplementation.class);
+    for (final Class<?> t : lookupChildClassesOf) {
+      System.out.println(t);
+    }
+  }
+}
