29df5d9f3ccd5552a81f9b445788bda22a6f1cea
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-04-04 15:16:59 +0200

    (Commons) Rename class

--- tests/org.preesm.tests.framework/src/org/preesm/commons/test/ReflectionUtilTest.java
+++ tests/org.preesm.tests.framework/src/org/preesm/commons/test/ReflectionUtilTest.java
@@ -2,7 +2,7 @@ package org.preesm.commons.test;
 
 import java.util.Collection;
 import org.junit.Test;
-import org.preesm.commons.ReflectionUitl;
+import org.preesm.commons.ReflectionUtil;
 import org.preesm.workflow.implement.AbstractTaskImplementation;
 
 /**
@@ -14,10 +14,11 @@ public class ReflectionUtilTest {
 
   @Test
   public void testRefelctionClasses() {
-    final Collection<Class<?>> lookupChildClassesOf = ReflectionUitl.lookupChildClassesOf("org.preesm.workflow.tasks",
+    final Collection<Class<?>> lookupChildClassesOf = ReflectionUtil.lookupChildClassesOf("org.preesm.workflow.tasks",
         AbstractTaskImplementation.class);
     for (final Class<?> t : lookupChildClassesOf) {
       System.out.println(t);
     }
   }
+
 }
