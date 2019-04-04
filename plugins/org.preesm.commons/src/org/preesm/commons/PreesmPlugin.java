6d9225cea5475668dc72335ea794104102d82ac9
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-04-04 15:19:24 +0200

    (Commons) Add preesm plugin class

---
+++ plugins/org.preesm.commons/src/org/preesm/commons/PreesmPlugin.java
@@ -0,0 +1,12 @@
+package org.preesm.commons;
+
+import org.eclipse.emf.common.EMFPlugin.EclipsePlugin;
+import org.osgi.framework.BundleActivator;
+
+/**
+ *
+ *
+ */
+public class PreesmPlugin extends EclipsePlugin implements BundleActivator {
+
+}
