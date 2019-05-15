2b529aad3a8669cf934615caee46640c3334541d
dmadronal <daniel.madronal@upm.es>
2019-05-15 19:07:02 +0200

    Added omp library (now it is supported in bare)

--- plugins/org.preesm.codegen.xtend/stdfiles/mppa2Explicit/communication.c
+++ plugins/org.preesm.codegen.xtend/stdfiles/mppa2Explicit/communication.c
@@ -173,14 +173,10 @@ void receiveDistributedEnd(int cluster)
 	}
 }
 
-#ifdef __nodeos__
 #include <omp.h>
-#endif
 
 void communicationInit() {
-	#ifdef __nodeos__
-	omp_set_num_threads(NB_OMP_CORE);
-	#endif
+	omp_set_num_threads(PREESM_NB_CORES);
 	#if 0
 	switch (__k1_get_cluster_id()){
 		case 0:	local_buffer_size = Core0_size; break;
