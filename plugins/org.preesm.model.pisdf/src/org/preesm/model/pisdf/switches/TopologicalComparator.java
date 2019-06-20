b0b31efaf91aa56c4fdc9c3b5b546468668031ed
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-06-20 17:43:14 +0200

    fixup

--- plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/switches/TopologicalComparator.java
+++ plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/switches/TopologicalComparator.java
@@ -12,7 +12,9 @@ public class TopologicalComparator implements Comparator<AbstractActor> {
 
   @Override
   public int compare(final AbstractActor o1, final AbstractActor o2) {
-    if (isPredecessor(o1, o2)) {
+    if (o1.equals(o2)) {
+      return 0;
+    } else if (isPredecessor(o1, o2)) {
       return 1;
     } else if (isPredecessor(o2, o1)) {
       return -1;
