79d53dd3d5ad9fde0622e84adba62b97a1f9d119
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-06-24 19:43:06 +0200

    (Algos) Add toString for schedule results

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/scheduler/SchedulerResult.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/scheduler/SchedulerResult.java
@@ -1,7 +1,11 @@
 package org.preesm.algorithm.scheduler;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.preesm.model.algorithm.mapping.Mapping;
+import org.preesm.model.algorithm.schedule.HierarchicalSchedule;
 import org.preesm.model.algorithm.schedule.Schedule;
+import org.preesm.model.pisdf.AbstractActor;
 
 /**
  *
@@ -14,4 +18,28 @@ public class SchedulerResult {
     this.mapping = mapping;
     this.schedule = schedule;
   }
+
+  @Override
+  public String toString() {
+    return "\n\n" + buildString(schedule, mapping, "").toString();
+  }
+
+  private static StringBuilder buildString(final Schedule sched, final Mapping mapp, final String indent) {
+    final StringBuilder res = new StringBuilder("");
+    res.append(indent + sched.getClass().getSimpleName() + " {\n");
+    if (sched instanceof HierarchicalSchedule) {
+      for (final Schedule child : sched.getChildren()) {
+        res.append(buildString(child, mapp, indent + "  ").toString());
+      }
+    } else {
+      for (final AbstractActor actor : sched.getActors()) {
+        final List<String> collect = mapp.getMapping(actor).stream().map(m -> m.getInstanceName())
+            .collect(Collectors.toList());
+        res.append(indent + "  " + collect + " " + actor.getName() + "\n");
+      }
+    }
+    res.append(indent + "}\n");
+    return res;
+  }
+
 }
