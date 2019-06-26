cd604380fd0ae1cf2400167ff7b880d96a69b4cb
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-06-26 13:56:42 +0200

    (Algos) Update schedule model

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/scheduler/SimpleScheduler.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/scheduler/SimpleScheduler.java
@@ -3,11 +3,13 @@ package org.preesm.algorithm.scheduler;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import org.eclipse.emf.common.util.ECollections;
+import org.preesm.commons.logger.PreesmLogger;
 import org.preesm.model.algorithm.mapping.Mapping;
 import org.preesm.model.algorithm.mapping.MappingFactory;
 import org.preesm.model.algorithm.schedule.ActorSchedule;
-import org.preesm.model.algorithm.schedule.ParallelSchedule;
+import org.preesm.model.algorithm.schedule.HierarchicalSchedule;
 import org.preesm.model.algorithm.schedule.ScheduleFactory;
 import org.preesm.model.pisdf.AbstractActor;
 import org.preesm.model.pisdf.PiGraph;
@@ -27,7 +29,7 @@ public class SimpleScheduler extends AbstractScheduler {
   @Override
   protected SchedulerResult exec(final PiGraph piGraph, final Design slamDesign, final Scenario scenario) {
 
-    final ParallelSchedule topParallelSchedule = ScheduleFactory.eINSTANCE.createParallelSchedule();
+    final HierarchicalSchedule topParallelSchedule = ScheduleFactory.eINSTANCE.createParallelHiearchicalSchedule();
     final Mapping createMapping = MappingFactory.eINSTANCE.createMapping();
     final Map<ComponentInstance, ActorSchedule> cmpSchedules = new LinkedHashMap<>();
 
@@ -39,12 +41,15 @@ public class SimpleScheduler extends AbstractScheduler {
           .sorted(new ComponentInstanceComparator()).findFirst().orElse(scenario.getSimulationInfo().getMainOperator());
       createMapping.getMappings().put(orderedActor, ECollections.singletonEList(targetCmpIntance));
       if (!cmpSchedules.containsKey(targetCmpIntance)) {
-        final ActorSchedule createActorSchedule = ScheduleFactory.eINSTANCE.createActorSchedule();
+        final ActorSchedule createActorSchedule = ScheduleFactory.eINSTANCE.createSequentialActorSchedule();
         cmpSchedules.put(targetCmpIntance, createActorSchedule);
         topParallelSchedule.getScheduleTree().add(createActorSchedule);
       }
       cmpSchedules.get(targetCmpIntance).getActors().add(orderedActor);
     }
+
+    final int span = topParallelSchedule.getSpan();
+    PreesmLogger.getLogger().log(Level.INFO, "span = " + span);
 
     return new SchedulerResult(createMapping, topParallelSchedule);
   }
