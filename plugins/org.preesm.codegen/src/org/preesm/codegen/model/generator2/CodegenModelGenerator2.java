fdf6a524389d384511b6658f29ad43854e6fe0df
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-07-25 16:49:31 +0200

    (Codegen) add new task; generate empty core code

--- plugins/org.preesm.codegen/src/org/preesm/codegen/model/generator2/CodegenModelGenerator2.java
+++ plugins/org.preesm.codegen/src/org/preesm/codegen/model/generator2/CodegenModelGenerator2.java
@@ -1,13 +1,17 @@
 package org.preesm.codegen.model.generator2;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import org.eclipse.emf.common.util.EList;
 import org.preesm.algorithm.mapping.model.Mapping;
 import org.preesm.algorithm.memalloc.model.Allocation;
 import org.preesm.algorithm.schedule.model.Schedule;
 import org.preesm.codegen.model.Block;
+import org.preesm.codegen.model.CoreBlock;
+import org.preesm.codegen.model.util.CodegenModelUserFactory;
 import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.scenario.Scenario;
+import org.preesm.model.slam.ComponentInstance;
 import org.preesm.model.slam.Design;
 
 /**
@@ -35,6 +39,8 @@ public class CodegenModelGenerator2 {
   private final Allocation memAlloc;
   private final boolean    papify;
 
+  private final List<Block> coreBlocks;
+
   private CodegenModelGenerator2(final Design archi, final PiGraph algo, final Scenario scenario,
       final Schedule schedule, final Mapping mapping, final Allocation memAlloc, boolean papify) {
     this.archi = archi;
@@ -44,10 +50,20 @@ public class CodegenModelGenerator2 {
     this.mapping = mapping;
     this.memAlloc = memAlloc;
     this.papify = papify;
+
+    this.coreBlocks = new ArrayList<>();
   }
 
   private List<Block> generate() {
-    return Collections.emptyList();
+
+    final EList<ComponentInstance> cmps = this.archi.getOperatorComponentInstances();
+    for (ComponentInstance cmp : cmps) {
+      final CoreBlock createCoreBlock = CodegenModelUserFactory.createCoreBlock(cmp);
+      this.coreBlocks.add(createCoreBlock);
+    }
+
+    return this.coreBlocks;
+
   }
 
 }
