af4348b62bc1c732a8e7a2b886e8b205198069df
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-08-20 14:55:54 +0200

    (Commons) Fix collection util method

--- plugins/org.preesm.commons/src/org/preesm/commons/CollectionUtil.java
+++ plugins/org.preesm.commons/src/org/preesm/commons/CollectionUtil.java
@@ -16,7 +16,7 @@ public class CollectionUtil {
    * Retrieves all the map values of the selected keys in a list. If selectedKeys is ordered, the resulting list will be
    * ordered accordingly. If a key is absent, the result will omit the value.
    */
-  public static final <V, T> List<V> mapGetAll(final Map<T, V> map, final Collection<T> selectedKeys) {
+  public static final <V, T> List<V> mapGetAll(final Map<T, V> map, final Collection<? extends T> selectedKeys) {
     final List<V> result = new LinkedList<>();
     for (final T key : selectedKeys) {
       if (map.containsKey(key)) {
