ba7e44c25cc4690c2adb6168b60cc5a149f31118
dmadronal <daniel.madronal@upm.es>
2019-08-28 19:43:21 +0200

    Undo changes in FAST and PFAST, I cannot test them even without energyAwareness enabled

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/mapper/FASTMappingFromPiMM.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/mapper/FASTMappingFromPiMM.java
@@ -41,10 +41,8 @@
 package org.preesm.algorithm.mapper;
 
 import java.util.Map;
-import java.util.logging.Level;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.preesm.algorithm.mapper.abc.impl.latency.LatencyAbc;
-import org.preesm.algorithm.mapper.energyawareness.EnergyAwarenessProvider;
 import org.preesm.algorithm.mapper.model.MapperDAG;
 import org.preesm.algorithm.model.dag.DirectedAcyclicGraph;
 import org.preesm.algorithm.pisdf.pimm2srdag.StaticPiMM2MapperDAGVisitor;
@@ -52,7 +50,6 @@ import org.preesm.commons.doc.annotations.Parameter;
 import org.preesm.commons.doc.annotations.Port;
 import org.preesm.commons.doc.annotations.PreesmTask;
 import org.preesm.commons.doc.annotations.Value;
-import org.preesm.commons.logger.PreesmLogger;
 import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.scenario.Scenario;
 import org.preesm.model.slam.Design;
@@ -76,19 +73,7 @@ import org.preesm.workflow.implement.AbstractWorkflowNodeImplementation;
         @Parameter(name = "balanceLoads", values = { @Value(name = "false") }),
         @Parameter(name = "displaySolutions", values = { @Value(name = "false") }),
         @Parameter(name = "fastTime", values = { @Value(name = "100") }),
-        @Parameter(name = "fastLocalSearchTime", values = { @Value(name = "10") }),
-        @Parameter(name = "EnergyAwareness",
-            values = { @Value(name = "True", effect = "Turns on energy aware mapping/scheduling") }),
-        @Parameter(name = "EnergyAwarenessFirstConfig",
-            values = { @Value(name = "First", effect = "Takes as starting point the first valid combination of PEs"),
-                @Value(name = "Middle", effect = "Takes as starting point half of the available PEs"),
-                @Value(name = "Max", effect = "Takes as starting point all the available PEs"),
-                @Value(name = "Random", effect = "Takes as starting point a random number of PEs") }),
-        @Parameter(name = "EnergyAwarenessSearchType", values = {
-            @Value(name = "Thorough",
-                effect = "Analyzes PE combinations one by one until the performance objective is reached"),
-            @Value(name = "Halves", effect = "Divides in halves the remaining available PEs and goes up/down depending"
-                + " if the FPS reached are below/above the objective") })
+        @Parameter(name = "fastLocalSearchTime", values = { @Value(name = "10") })
 
     })
 public class FASTMappingFromPiMM extends FASTMappingFromDAG {
@@ -101,47 +86,9 @@ public class FASTMappingFromPiMM extends FASTMappingFromDAG {
     final Design architecture = (Design) inputs.get(AbstractWorkflowNodeImplementation.KEY_ARCHITECTURE);
     final Scenario scenario = (Scenario) inputs.get(AbstractWorkflowNodeImplementation.KEY_SCENARIO);
 
-    String messageLogger = "";
-    Map<String, Object> mapping = null;
+    final MapperDAG dag = StaticPiMM2MapperDAGVisitor.convert(algorithm, architecture, scenario);
+    inputs.put(AbstractWorkflowNodeImplementation.KEY_SDF_DAG, dag);
 
-    if (parameters.containsKey("EnergyAwareness") && parameters.get("EnergyAwareness").equalsIgnoreCase("true")) {
-      messageLogger = "Energy-awareness enabled. This option will increase the mapping/scheduling "
-          + "task so it may take a while";
-      PreesmLogger.getLogger().log(Level.INFO, messageLogger);
-      String firstConfig = "middle";
-      String searchingMode = "halves";
-      if (parameters.containsKey("EnergyAwarenessFirstConfig")) {
-        firstConfig = parameters.get("EnergyAwarenessFirstConfig");
-      }
-      if (parameters.containsKey("EnergyAwarenessSearchType")) {
-        searchingMode = parameters.get("EnergyAwarenessSearchType");
-      }
-      EnergyAwarenessProvider provider = new EnergyAwarenessProvider(scenario, firstConfig, searchingMode);
-      /** iterate **/
-      while (true) {
-        /** Get configuration **/
-        provider.computeNextConfig();
-        /** Update scenario mapping for the current configuration **/
-        provider.updateScenario();
-        /** Check if the configuration is valid **/
-        if (provider.hasFinished()) {
-          break;
-        }
-        /** Try the mapping */
-        final MapperDAG dag = StaticPiMM2MapperDAGVisitor.convert(algorithm, architecture,
-            provider.getScenarioMapping());
-        inputs.put(AbstractWorkflowNodeImplementation.KEY_SDF_DAG, dag);
-        inputs.put(AbstractWorkflowNodeImplementation.KEY_SCENARIO, provider.getScenarioMapping());
-        mapping = super.execute(inputs, parameters, monitor, nodeName, workflow);
-        /** Evaluate mapping **/
-        provider.evaluateMapping(mapping);
-      }
-      mapping = provider.getFinalMapping();
-    } else {
-      final MapperDAG dag = StaticPiMM2MapperDAGVisitor.convert(algorithm, architecture, scenario);
-      inputs.put(AbstractWorkflowNodeImplementation.KEY_SDF_DAG, dag);
-      mapping = super.execute(inputs, parameters, monitor, nodeName, workflow);
-    }
-    return mapping;
+    return super.execute(inputs, parameters, monitor, nodeName, workflow);
   }
 }
