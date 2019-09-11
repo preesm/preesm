5c1e67305cbcf85bcbc8782714e0ed4abd201cb6
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-09-11 17:01:14 +0200

    (Slam) Implement cost evaluation for xcore routes

--- plugins/org.preesm.model.slam/src/org/preesm/model/slam/route/RouteCostEvaluator.java
+++ plugins/org.preesm.model.slam/src/org/preesm/model/slam/route/RouteCostEvaluator.java
@@ -3,6 +3,11 @@ package org.preesm.model.slam.route;
 import org.preesm.model.slam.ComNode;
 import org.preesm.model.slam.Component;
 import org.preesm.model.slam.ComponentInstance;
+import org.preesm.model.slam.SlamMemoryRouteStep;
+import org.preesm.model.slam.SlamMessageRouteStep;
+import org.preesm.model.slam.SlamRoute;
+import org.preesm.model.slam.SlamRouteStep;
+import org.preesm.model.slam.util.SlamSwitch;
 
 /**
  *
@@ -19,12 +24,18 @@ public class RouteCostEvaluator {
    * @return the long
    */
   public static final long evaluateTransferCost(final Route route, final long transferSize) {
-    long cost = 0;
-    // Iterating the route and incrementing transfer cost
-    for (final AbstractRouteStep step : route.getRouteSteps()) {
-      cost += getTransferCost(step, transferSize);
-    }
-    return cost;
+    return new RouteStepCostEvaluator(transferSize).doSwitch(route);
+  }
+
+  /**
+   * Evaluates the cost of a data transfer with size transferSize along the route.
+   *
+   * @param transferSize
+   *          the transfer size
+   * @return the long
+   */
+  public static final long evaluateTransferCost(final SlamRoute route, final long transferSize) {
+    return new SlamRouteStepCostEvaluator(transferSize).doSwitch(route);
   }
 
   /**
@@ -36,6 +47,49 @@ public class RouteCostEvaluator {
    */
   public static final long getTransferCost(final AbstractRouteStep routeStep, final long transfersSize) {
     return new RouteStepCostEvaluator(transfersSize).doSwitch(routeStep);
+  }
+
+  /**
+   * Evaluates the cost of a data transfer with size transferSize. This cost can include overheads, involvements...
+   *
+   * @param transfersSize
+   *          the transfers size
+   * @return the transfer cost
+   */
+  public static final long getTransferCost(final SlamRouteStep routeStep, final long transfersSize) {
+    return new SlamRouteStepCostEvaluator(transfersSize).doSwitch(routeStep);
+  }
+
+  /**
+   * Returns the longest time a contention node needs to transfer the data before the RAM in the route steps.
+   *
+   * @param transfersSize
+   *          the transfers size
+   * @return the sender side worst transfer time
+   */
+  public long getSenderSideWorstTransferTime(final SlamMemoryRouteStep memRouteStep, final long transfersSize) {
+    long time = 0;
+
+    for (final ComponentInstance node : memRouteStep.getSenderSideContentionNodes()) {
+      time = Math.max(time, (long) (transfersSize / ((ComNode) node.getComponent()).getSpeed()));
+    }
+    return time;
+  }
+
+  /**
+   * Returns the longest time a contention node needs to transfer the data after the RAM in the route steps.
+   *
+   * @param transfersSize
+   *          the transfers size
+   * @return the receiver side worst transfer time
+   */
+  public long getReceiverSideWorstTransferTime(final SlamMemoryRouteStep memRouteStep, final long transfersSize) {
+    long time = 0;
+
+    for (final ComponentInstance node : memRouteStep.getReceiverSideContentionNodes()) {
+      time = Math.max(time, (long) (transfersSize / ((ComNode) node.getComponent()).getSpeed()));
+    }
+    return time;
   }
 
   /**
@@ -56,8 +110,19 @@ public class RouteCostEvaluator {
     public long doSwitch(Object o) {
       if (o instanceof MessageRouteStep) {
         return caseMessageRouteStep((MessageRouteStep) o);
+      } else if (o instanceof Route) {
+        return caseRoute((Route) o);
       }
       return -1L;
+    }
+
+    public long caseRoute(final Route route) {
+      long cost = 0;
+      // Iterating the route and incrementing transfer cost
+      for (final AbstractRouteStep step : route.getRouteSteps()) {
+        cost += getTransferCost(step, transferSize);
+      }
+      return cost;
     }
 
     public long caseMessageRouteStep(final MessageRouteStep msg) {
@@ -76,6 +141,49 @@ public class RouteCostEvaluator {
       }
       return time;
     }
+  }
+
+  /**
+   *
+   * @author anmorvan
+   *
+   */
+  private static class SlamRouteStepCostEvaluator extends SlamSwitch<Long> {
+
+    private long transferSize;
+
+    SlamRouteStepCostEvaluator(final long transferSize) {
+      this.transferSize = transferSize;
+    }
+
+    @Override
+    public Long caseSlamRoute(SlamRoute route) {
+      long cost = 0;
+      // Iterating the route and incrementing transfer cost
+      for (final SlamRouteStep step : route.getRouteSteps()) {
+        cost += doSwitch(step);
+      }
+      return cost;
+    }
+
+    @Override
+    public Long caseSlamMessageRouteStep(SlamMessageRouteStep msg) {
+      long time = 0;
+      for (final ComponentInstance node : msg.getNodes()) {
+        final Component def = node.getComponent();
+        if (def instanceof ComNode) {
+          final ComNode comNode = (ComNode) def;
+          time = Math.max(time, (long) (transferSize / comNode.getSpeed()));
+        }
+      }
+
+      // No zero transfer time is alloweds
+      if (time <= 0) {
+        time = 1;
+      }
+      return time;
+    }
+
   }
 
 }
