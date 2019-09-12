f99e080a161a8904541d7f70393b736bb89f8a85
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-09-12 16:43:11 +0200

    (Slam) Refactor routing table

--- plugins/org.preesm.model.slam/src/org/preesm/model/slam/route/SlamRoutingTable.java
+++ plugins/org.preesm.model.slam/src/org/preesm/model/slam/route/SlamRoutingTable.java
@@ -1,10 +1,10 @@
 /**
- * Copyright or © or Copr. IETR/INSA - Rennes (2008 - 2019) :
+ * Copyright or © or Copr. IETR/INSA - Rennes (2009 - 2019) :
  *
  * Antoine Morvan [antoine.morvan@insa-rennes.fr] (2017 - 2019)
- * Clément Guy [clement.guy@insa-rennes.fr] (2014 - 2015)
- * Matthieu Wipliez [matthieu.wipliez@insa-rennes.fr] (2008)
- * Maxime Pelcat [maxime.pelcat@insa-rennes.fr] (2008 - 2014)
+ * Clément Guy [clement.guy@insa-rennes.fr] (2014)
+ * Jonathan Piat [jpiat@laas.fr] (2011)
+ * Maxime Pelcat [maxime.pelcat@insa-rennes.fr] (2009 - 2016)
  *
  * This software is a computer program whose purpose is to help prototyping
  * parallel applications using dataflow formalism.
@@ -38,10 +38,16 @@
 package org.preesm.model.slam.route;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.logging.Level;
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.preesm.commons.exceptions.PreesmRuntimeException;
 import org.preesm.commons.logger.PreesmLogger;
 import org.preesm.model.slam.ComponentInstance;
@@ -53,34 +59,158 @@ import org.preesm.model.slam.impl.ComNodeImpl;
 import org.preesm.model.slam.utils.SlamUserFactory;
 
 /**
- * This class can evaluate a given transfer and choose the best route between two operators.
+ * Table representing the different routes available to go from one operator to another.
  *
  * @author mpelcat
  */
-public class RouteCalculator {
-
-  /** The archi. */
-  private final Design archi;
-
-  /** The table. */
-  private RoutingTable table = null;
+public class SlamRoutingTable {
 
   /**
-   * Constructor from a given architecture.
-   *
-   * @param archi
-   *          the archi
+   * A couple of operators to which the routes are linked.
    */
-  public RouteCalculator(final Design archi) {
+  private class OperatorCouple extends MutablePair<ComponentInstance, ComponentInstance> {
+    private static final long serialVersionUID = -451571160460519876L;
 
+    OperatorCouple(final ComponentInstance op1, final ComponentInstance op2) {
+      super(op1, op2);
+    }
+
+    @Override
+    public String toString() {
+      return "(" + getOp1() + "," + getOp2() + ")";
+    }
+
+    ComponentInstance getOp1() {
+      return getLeft();
+    }
+
+    ComponentInstance getOp2() {
+      return getRight();
+    }
+  }
+
+  /**
+   * A route transfer comparator that never returns 0.
+   */
+  private class RouteComparator implements Comparator<SlamRoute> {
+
+    @Override
+    public int compare(final SlamRoute o1, final SlamRoute o2) {
+      final double difference = RouteCostEvaluator.evaluateTransferCost(o1, 1)
+          - RouteCostEvaluator.evaluateTransferCost(o2, 1);
+      if (difference >= 0) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+
+  }
+
+  /**
+   * A list of routes ordered in inverse order of transfer cost.
+   */
+  private class RouteList extends ConcurrentSkipListSet<SlamRoute> {
+
+    private static final long serialVersionUID = -851695207011182681L;
+
+    RouteList() {
+      super(new RouteComparator());
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder sb = new StringBuilder("|");
+      for (final SlamRoute r : this) {
+        sb.append(r.toString() + "|");
+      }
+      return sb.toString();
+    }
+  }
+
+  /** List of available routes. */
+  private final Map<OperatorCouple, RouteList> table;
+  private final Design                         archi;
+
+  /**
+   * Instantiates a new routing table.
+   *
+   */
+  public SlamRoutingTable(final Design archi) {
+    super();
     this.archi = archi;
-    this.table = new RoutingTable();
+    this.table = new LinkedHashMap<>();
 
     // Creating the route steps between directly connected operators
     createRouteSteps();
     // Concatenation of route steps to generate optimal routes using
     // the Floyd Warshall algorithm
     createRoutes();
+  }
+
+  /**
+   * Gets a route with a given index.
+   *
+   * @param op1
+   *          the op 1
+   * @param op2
+   *          the op 2
+   * @return the best route
+   */
+  SlamRoute getBestRoute(final ComponentInstance op1, final ComponentInstance op2) {
+    final OperatorCouple obj = new OperatorCouple(op1, op2);
+    if (this.table.containsKey(obj)) {
+      return this.table.get(obj).first();
+    }
+    return null;
+  }
+
+  /**
+   * Removes all the routes corresponding to the operator couple.
+   *
+   * @param op1
+   *          the op 1
+   * @param op2
+   *          the op 2
+   */
+  void removeRoutes(final ComponentInstance op1, final ComponentInstance op2) {
+    final OperatorCouple route = new OperatorCouple(op1, op2);
+    if (this.table.containsKey(route)) {
+      this.table.get(route).clear();
+    }
+  }
+
+  /**
+   * Adds a new route.
+   *
+   * @param op1
+   *          the op 1
+   * @param op2
+   *          the op 2
+   * @param route
+   *          the route
+   */
+  void addRoute(final ComponentInstance op1, final ComponentInstance op2, final SlamRoute route) {
+    final OperatorCouple opCouple = new OperatorCouple(op1, op2);
+    if (!this.table.containsKey(opCouple)) {
+      this.table.put(opCouple, new RouteList());
+    }
+    this.table.get(opCouple).add(route);
+  }
+
+  /**
+   * Displays the table.
+   *
+   * @return the string
+   */
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    for (final Entry<OperatorCouple, RouteList> e : this.table.entrySet()) {
+      final OperatorCouple couple = e.getKey();
+      sb.append(couple + " -> " + e.getValue() + "\n");
+    }
+    return sb.toString();
   }
 
   /**
@@ -153,7 +283,7 @@ public class RouteCalculator {
           && !otherEnd.getInstanceName().equals(source.getInstanceName())) {
         final SlamRoute newRoute = SlamUserFactory.eINSTANCE.createSlamRoute(this.archi, source, alreadyVisitedNodes,
             otherEnd);
-        this.table.addRoute(source, otherEnd, newRoute);
+        this.addRoute(source, otherEnd, newRoute);
       }
     }
   }
@@ -164,7 +294,7 @@ public class RouteCalculator {
   private void createRoutes() {
     PreesmLogger.getLogger().log(Level.INFO, "Initializing routing table.");
 
-    floydWarshall(this.table, this.archi.getOperatorComponentInstances());
+    floydWarshall(this, this.archi.getOperatorComponentInstances());
   }
 
   /**
@@ -175,7 +305,7 @@ public class RouteCalculator {
    * @param operators
    *          the operators
    */
-  private void floydWarshall(final RoutingTable table, final List<ComponentInstance> operators) {
+  private void floydWarshall(final SlamRoutingTable table, final List<ComponentInstance> operators) {
 
     for (final ComponentInstance k : operators) {
 
@@ -221,7 +351,7 @@ public class RouteCalculator {
    * @return the route
    */
   public SlamRoute getRoute(final ComponentInstance op1, final ComponentInstance op2) {
-    final SlamRoute r = this.table.getBestRoute(op1, op2);
+    final SlamRoute r = this.getBestRoute(op1, op2);
 
     if (r == null) {
       final String msg = "Did not find a route between " + op1 + " and " + op2 + ".";
