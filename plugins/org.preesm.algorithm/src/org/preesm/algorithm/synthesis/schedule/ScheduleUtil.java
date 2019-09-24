c7749e1a68a742105fe062ebe3f612618c39c150
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-09-24 15:44:54 +0200

    (Algo) remove schedule comparator

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/ScheduleUtil.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/ScheduleUtil.java
@@ -1,6 +1,5 @@
 package org.preesm.algorithm.synthesis.schedule;
 
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -11,13 +10,10 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.Pair;
 import org.preesm.algorithm.schedule.model.ActorSchedule;
 import org.preesm.algorithm.schedule.model.HierarchicalSchedule;
-import org.preesm.algorithm.schedule.model.ParallelSchedule;
 import org.preesm.algorithm.schedule.model.Schedule;
-import org.preesm.algorithm.schedule.model.SequentialSchedule;
 import org.preesm.algorithm.schedule.model.util.ScheduleSwitch;
 import org.preesm.commons.exceptions.PreesmRuntimeException;
 import org.preesm.model.pisdf.AbstractActor;
-import org.preesm.model.pisdf.util.topology.PiSDFTopologyHelper;
 
 /**
  *
@@ -28,62 +24,6 @@ public class ScheduleUtil {
 
   private ScheduleUtil() {
     // forbid instantiation
-  }
-
-  /**
-   * Instanciate a Comparator of AbstractActor that respects both topological order and schedule order;
-   */
-  public static final Comparator<AbstractActor> getScheduleComparator(final Schedule schedule) {
-    return new Comparator<AbstractActor>() {
-
-      private final Map<AbstractActor, ActorSchedule> actorToSchedule = ScheduleUtil.actorToScheduleMap(schedule);
-
-      @Override
-      public int compare(final AbstractActor leftActor, final AbstractActor rightActor) {
-        if (leftActor.equals(rightActor)) {
-          return 0;
-        }
-        if (actorToSchedule.containsKey(leftActor) && actorToSchedule.containsKey(rightActor)) {
-          final int res;
-          final ActorSchedule leftActorSchedule = actorToSchedule.get(leftActor);
-          final ActorSchedule rightActorSchedule = actorToSchedule.get(rightActor);
-          if (leftActorSchedule == rightActorSchedule) {
-            res = leftActorSchedule.getActorList().indexOf(leftActor)
-                - rightActorSchedule.getActorList().indexOf(rightActor);
-          } else {
-            // find common schedule ancestor (there is one)
-            // check order on that ancestor
-            final Pair<Schedule,
-                Schedule> lca = ScheduleUtil.findLowestCommonAncestorChildren(leftActorSchedule, rightActorSchedule);
-            final Schedule left = lca.getLeft();
-            final Schedule right = lca.getRight();
-
-            final HierarchicalSchedule parent = left.getParent();
-            if (parent instanceof SequentialSchedule) {
-              res = parent.getScheduleTree().indexOf(left) - parent.getScheduleTree().indexOf(right);
-            } else if (parent instanceof ParallelSchedule) {
-              // check if a path exists
-              final boolean isPred = PiSDFTopologyHelper.isPredecessor(leftActor, rightActor);
-              if (isPred) {
-                res = 1;
-              } else {
-                final boolean isSucc = PiSDFTopologyHelper.isSuccessor(leftActor, rightActor);
-                if (isSucc) {
-                  res = -1;
-                } else {
-                  res = parent.getScheduleTree().indexOf(left) - parent.getScheduleTree().indexOf(right);
-                }
-              }
-            } else {
-              throw new UnsupportedOperationException();
-            }
-          }
-          return res;
-        } else {
-          throw new PreesmRuntimeException();
-        }
-      }
-    };
   }
 
   /**
