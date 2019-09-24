a0b4ac4b560e3c49918263a2b936830de5c6f6fb
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-09-24 15:44:13 +0200

    (Algo) Update schedule iterator

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/iterator/ScheduleAndTopologyIterator.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/iterator/ScheduleAndTopologyIterator.java
@@ -2,12 +2,10 @@ package org.preesm.algorithm.synthesis.schedule.iterator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.Map;
 import org.preesm.algorithm.schedule.model.ActorSchedule;
-import org.preesm.algorithm.schedule.model.HierarchicalSchedule;
 import org.preesm.algorithm.schedule.model.Schedule;
-import org.preesm.algorithm.schedule.model.util.ScheduleSwitch;
+import org.preesm.algorithm.synthesis.schedule.ScheduleOrderedVisitor;
 import org.preesm.algorithm.synthesis.schedule.ScheduleUtil;
 import org.preesm.model.pisdf.AbstractActor;
 
@@ -26,35 +24,18 @@ public class ScheduleAndTopologyIterator extends ScheduleIterator {
 
   @Override
   public List<AbstractActor> createOrder(final Schedule schedule) {
-    final SortedSet<AbstractActor> res = new TreeSet<>(ScheduleUtil.getScheduleComparator(schedule));
-    new ActorScheduleSwitch(res).doSwitch(schedule);
-    return new ArrayList<>(res);
-  }
 
-  /**
-   *
-   * @author anmorvan
-   *
-   */
-  private class ActorScheduleSwitch extends ScheduleSwitch<Boolean> {
+    final Map<AbstractActor, ActorSchedule> actorToScheduleMap = ScheduleUtil.actorToScheduleMap(schedule);
 
-    final SortedSet<AbstractActor> orderedSet;
+    final List<AbstractActor> res = new ArrayList<>();
 
-    public ActorScheduleSwitch(SortedSet<AbstractActor> treeSet) {
-      this.orderedSet = treeSet;
-    }
+    new ScheduleOrderedVisitor(actorToScheduleMap) {
+      @Override
+      public void visit(AbstractActor actor) {
+        res.add(actor);
+      }
 
-    @Override
-    public Boolean caseHierarchicalSchedule(final HierarchicalSchedule object) {
-      object.getChildren().forEach(this::doSwitch);
-      return true;
-    }
-
-    @Override
-    public Boolean caseActorSchedule(final ActorSchedule object) {
-      final List<AbstractActor> actorList = object.getActorList();
-      this.orderedSet.addAll(actorList);
-      return true;
-    }
+    }.doSwitch(schedule);
+    return res;
   }
 }
