bfcd6470c1025a87c791057788c4756540851f29
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-10-16 10:32:52 +0200

    (Codegen) Properly set actor in codegen model generator

--- plugins/org.preesm.codegen/src/org/preesm/codegen/model/generator/CodegenModelGenerator.java
+++ plugins/org.preesm.codegen/src/org/preesm/codegen/model/generator/CodegenModelGenerator.java
@@ -1709,8 +1709,7 @@ public class CodegenModelGenerator extends AbstractCodegenModelGenerator {
     final ActorFunctionCall func = CodegenModelUserFactory.eINSTANCE.createActorFunctionCall();
     func.setName(prototype.getFunctionName());
     func.setActorName(dagVertex.getName());
-    AbstractActor oriPiActor = PreesmCopyTracker.getOriginalSource(dagVertex.getReferencePiVertex());
-    func.setActor(oriPiActor);
+    func.setActor(dagVertex.getReferencePiVertex());
     // Retrieve the Arguments that must correspond to the incoming data
     // fifos
     final Entry<List<Variable>, List<PortDirection>> callVars = generateCallVariables(dagVertex, prototype, isInit);
