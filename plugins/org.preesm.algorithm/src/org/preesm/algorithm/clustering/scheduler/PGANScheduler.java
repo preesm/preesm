25ead26e4b8bcdac210731e3c8cd2d42954d4629
dylangageot <gageot.dylan@gmail.com>
2020-01-31 14:18:08 +0100

    (PGAN) Compute BRV inside PGAN scheduler

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scheduler/PGANScheduler.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scheduler/PGANScheduler.java
@@ -1,10 +1,14 @@
 package org.preesm.algorithm.clustering.scheduler;
 
+import java.util.Map;
 import org.preesm.algorithm.schedule.model.Schedule;
 import org.preesm.commons.exceptions.PreesmRuntimeException;
+import org.preesm.model.pisdf.AbstractVertex;
 import org.preesm.model.pisdf.Delay;
 import org.preesm.model.pisdf.Fifo;
 import org.preesm.model.pisdf.PiGraph;
+import org.preesm.model.pisdf.brv.BRVMethod;
+import org.preesm.model.pisdf.brv.PiBRV;
 import org.preesm.model.pisdf.factory.PiMMUserFactory;
 
 /**
@@ -22,6 +26,11 @@ public class PGANScheduler {
    * Copy of input graph.
    */
   private final PiGraph copiedGraph;
+
+  /**
+   * Repetition vector of actual processed graph.
+   */
+  private Map<AbstractVertex, Long> repetitionVector;
 
   /**
    * Builds a PGAN scheduler.
@@ -46,6 +55,7 @@ public class PGANScheduler {
   public Schedule schedule() {
 
     // TODO: Compute BRV from a subgraph
+    this.repetitionVector = PiBRV.compute(this.inputGraph, BRVMethod.LCM);
 
     // Clusterize while creating schedule
 
@@ -53,7 +63,7 @@ public class PGANScheduler {
   }
 
   private final void isClusterizable() {
-    // Check for uncompatible delay (with getter/setter
+    // Check for uncompatible delay (with getter/setter)
     for (Fifo fifo : inputGraph.getFifosWithDelay()) {
       Delay delay = fifo.getDelay();
 
