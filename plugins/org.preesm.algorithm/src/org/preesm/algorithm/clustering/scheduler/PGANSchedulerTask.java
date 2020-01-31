2f53495e27baec08421ab4698963e2d5583bfd0b
dylangageot <gageot.dylan@gmail.com>
2020-01-31 16:49:49 +0100

    (PGAN) Schedule every subgraph (not cluster for the moment)

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scheduler/PGANSchedulerTask.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scheduler/PGANSchedulerTask.java
@@ -6,7 +6,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.preesm.algorithm.schedule.model.Schedule;
 import org.preesm.commons.doc.annotations.Port;
 import org.preesm.commons.doc.annotations.PreesmTask;
+import org.preesm.model.pisdf.AbstractActor;
 import org.preesm.model.pisdf.PiGraph;
+import org.preesm.model.pisdf.statictools.PiMMHelper;
 import org.preesm.workflow.elements.Workflow;
 import org.preesm.workflow.implement.AbstractTaskImplementation;
 
@@ -17,19 +19,30 @@ import org.preesm.workflow.implement.AbstractTaskImplementation;
  *
  */
 @PreesmTask(id = "pgan-scheduler", name = "PGAN Scheduler", inputs = { @Port(name = "PiMM", type = PiGraph.class) },
-    outputs = { @Port(name = "Schedule", type = Schedule.class) })
+    outputs = { @Port(name = "Schedules", type = Map.class) })
 public class PGANSchedulerTask extends AbstractTaskImplementation {
 
   @Override
   public Map<String, Object> execute(Map<String, Object> inputs, Map<String, String> parameters,
       IProgressMonitor monitor, String nodeName, Workflow workflow) {
-
+    // Retrieve input graph
     PiGraph inputGraph = (PiGraph) inputs.get("PiMM");
-
-    PGANScheduler scheduler = new PGANScheduler(inputGraph);
+    Map<AbstractActor, Schedule> schedulesMap = new HashMap<>();
+    // Resolve parameters
+    PiMMHelper.resolveAllParameters(inputGraph);
+    // Generate schedule for every cluster
+    for (AbstractActor actor : inputGraph.getAllActors()) {
+      if (actor instanceof PiGraph) {
+        PiGraph subgraph = (PiGraph) actor;
+        // if (subgraph.isCluster()) {
+        PGANScheduler scheduler = new PGANScheduler(subgraph);
+        schedulesMap.put(subgraph, scheduler.schedule());
+        // }
+      }
+    }
 
     Map<String, Object> outputs = new HashMap<>();
-    outputs.put("Schedule", scheduler.schedule());
+    outputs.put("Schedules", schedulesMap);
     return outputs;
   }
 
