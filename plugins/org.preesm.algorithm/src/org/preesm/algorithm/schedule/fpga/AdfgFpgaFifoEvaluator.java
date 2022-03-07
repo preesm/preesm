1dcf8f5aa3f2ba8ae700cac24b87dabbca0043ca
Alexandre Honorat <alexandre.honorat@insa-rennes.fr>
2022-03-07 19:09:11 +0100

    (FPGA) cont'd of ADFG buffer sizing (adds cycle ILP equation)

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/schedule/fpga/AdfgFpgaFifoEvaluator.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/schedule/fpga/AdfgFpgaFifoEvaluator.java
@@ -1,15 +1,19 @@
 package org.preesm.algorithm.schedule.fpga;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.eclipse.xtext.xbase.lib.Pair;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.cycle.PatonCycleBase;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultUndirectedGraph;
+import org.ojalgo.optimisation.Expression;
 import org.ojalgo.optimisation.ExpressionsBasedModel;
 import org.ojalgo.optimisation.Variable;
 import org.preesm.algorithm.mapper.ui.stats.IStatGenerator;
@@ -18,6 +22,7 @@ import org.preesm.algorithm.pisdf.autodelays.AbstractGraph.FifoAbstraction;
 import org.preesm.commons.exceptions.PreesmRuntimeException;
 import org.preesm.commons.logger.PreesmLogger;
 import org.preesm.commons.math.LongFraction;
+import org.preesm.commons.math.MathFunctionsHelper;
 import org.preesm.model.pisdf.AbstractActor;
 import org.preesm.model.pisdf.AbstractVertex;
 import org.preesm.model.pisdf.DataPort;
@@ -79,11 +84,9 @@ public class AdfgFpgaFifoEvaluator extends AbstractGenericFpgaFifoEvaluator {
     // ojAlgo dependency should be used to create the model because it has dedicated code to ILP,
     // or Choco (but not dedicated to ILP) at last resort.
 
-    // create intermediate graphs
+    // create intermediate FifoAbstraction graphs
     final DefaultDirectedGraph<AbstractActor, FifoAbstraction> ddg = AbstractGraph.createAbsGraph(flatGraph, brv);
     final DefaultUndirectedGraph<AbstractActor, FifoAbstraction> dug = AbstractGraph.undirectedGraph(ddg);
-    final Set<List<FifoAbstraction>> cycles = (new PatonCycleBase<AbstractActor, FifoAbstraction>(dug)).getCycleBasis()
-        .getCycles();
 
     // build model
     // create Maps to retrieve ID of variables (ID in order of addition in the model)
@@ -93,32 +96,204 @@ public class AdfgFpgaFifoEvaluator extends AbstractGenericFpgaFifoEvaluator {
     final Map<FifoAbstraction, Integer> fifoAbsToPhiVariableID = new LinkedHashMap<>();
     for (final FifoAbstraction fifoAbs : dug.edgeSet()) {
       final int index = fifoAbsToPhiVariableID.size();
-      final Variable varPhi = new Variable("phi_" + index);
-      varPhi.setInteger(true);
-      // TODO separate neg. from pos.?
       fifoAbsToPhiVariableID.put(fifoAbs, index);
-      model.addVariable(varPhi);
+      // we separate neg. from pos. because unsure that ojAlgo handles negative integers
+      final Variable varPhiPos = new Variable("phi_pos_" + index);
+      varPhiPos.setInteger(true);
+      varPhiPos.lower(0L);
+      model.addVariable(varPhiPos);
+      final Variable varPhiNeg = new Variable("phi_neg_" + index);
+      varPhiNeg.setInteger(true);
+      varPhiNeg.lower(0L);
+      model.addVariable(varPhiNeg);
     }
 
-    // add equation for cycles to the model
-    for (final List<FifoAbstraction> cycle : cycles) {
-      // create cycle equation
+    // create intermediate AffineRelation graph and cycle lists
+    final DefaultDirectedGraph<AbstractActor, AffineRelation> ddgAR = buildGraphAR(ddg, dug, fifoAbsToPhiVariableID);
+    final Set<
+        GraphPath<AbstractActor, FifoAbstraction>> cyclesGP = new PatonCycleBase<AbstractActor, FifoAbstraction>(dug)
+            .getCycleBasis().getCyclesAsGraphPaths();
+    final Set<List<AbstractActor>> cyclesAA = new LinkedHashSet<>();
+    cyclesGP.forEach(gp -> cyclesAA.add(gp.getVertexList()));
+
+    // add equations for cycles to the model
+    for (final List<AbstractActor> cycleAA : cyclesAA) {
+      generateCycleConstraint(ddgAR, cycleAA, model);
     }
 
     // Fifo to delta/size Variable ID (theta/delay is fixed for us, so not a variable)
     final Map<Fifo, Integer> fifoToSizeVariableID = new LinkedHashMap<>();
     // create size variables/equations
-    for (final FifoAbstraction fifoAbs : ddg.edgeSet()) {
-      for (final Fifo fifo : fifoAbs.fifos) {
+    for (final FifoAbstraction fa : ddg.edgeSet()) {
+      final AbstractActor src = ddg.getEdgeSource(fa);
+      final AbstractActor tgt = ddg.getEdgeTarget(fa);
+      final AffineRelation ar = ddgAR.getEdge(src, tgt);
+
+      for (final Fifo fifo : fa.fifos) {
         // create size variable and underflow and overflow expression
+        generateChannelConstraint(model, fifoToSizeVariableID, lambdaPerPort, fifo, ar);
       }
     }
 
     // objective function (minimize buffer sizes + phi)
 
     // TODO build a schedule using the normalized graph II and each actor offset (computed by the ILP)
+    // convert phi into cncrete time offset (thanks to average II)
+    // take the lowest value (might be negative) and adds it everywhere so that all offsets are positive
 
     throw new PreesmRuntimeException("This analysis is not yet completely implemented, stopping here.");
+  }
+
+  protected static class AffineRelation {
+    protected final long    nProd;
+    protected final long    dCons;
+    protected final int     phiIndex;
+    protected final boolean phiNegate;
+
+    protected AffineRelation(final long nProd, final long dCons, final int phiIndex, final boolean phiNegate) {
+      this.nProd = nProd;
+      this.dCons = dCons;
+      this.phiIndex = phiIndex;
+      this.phiNegate = phiNegate;
+    }
+
+  }
+
+  /**
+   * Builds a directed graph with affine relation information. Each edge is doubled (in a direction and in the opposite,
+   * even if only one direction is present in the original graph).
+   * 
+   * @param ddg
+   *          Abstract directed simple graph.
+   * @param dug
+   *          Abstract undirected simple graph.
+   * @param fifoAbsToPhiVariableID
+   *          Map from edges in the undirected graph to the phi variable index in the model.
+   * @return Directed simple graph of doubled affine relation (one in each direction).
+   */
+  protected static DefaultDirectedGraph<AbstractActor, AffineRelation> buildGraphAR(
+      final DefaultDirectedGraph<AbstractActor, FifoAbstraction> ddg,
+      final DefaultUndirectedGraph<AbstractActor, FifoAbstraction> dug,
+      final Map<FifoAbstraction, Integer> fifoAbsToPhiVariableID) {
+    DefaultDirectedGraph<AbstractActor, AffineRelation> ddgAR = new DefaultDirectedGraph<>(AffineRelation.class);
+    for (final AbstractActor aa : ddg.vertexSet()) {
+      ddgAR.addVertex(aa);
+    }
+    for (final FifoAbstraction fa : dug.edgeSet()) {
+      final AbstractActor src = ddg.getEdgeSource(fa);
+      final AbstractActor tgt = ddg.getEdgeTarget(fa);
+      final AffineRelation ar = new AffineRelation(fa.getProdRate(), fa.getConsRate(), fifoAbsToPhiVariableID.get(fa),
+          false);
+      ddgAR.addEdge(src, tgt, ar);
+      final AffineRelation arReverse = new AffineRelation(ar.dCons, ar.nProd, ar.phiIndex, true);
+      ddgAR.addEdge(tgt, src, arReverse);
+    }
+
+    return ddgAR;
+  }
+
+  /**
+   * Fill the model with cycle equations.
+   * <p>
+   * This method is adapted from the ADFG software. See Adnan Bouakaz thesis p. 70, proposition 2.8 .
+   * 
+   * @param ddgAR
+   *          Directed simple graph of Affine Relation;
+   * @param cycleAA
+   *          Cycle to consider.
+   * @param model
+   *          Model where to add the constraint.
+   */
+  protected static void generateCycleConstraint(final DefaultDirectedGraph<AbstractActor, AffineRelation> ddgAR,
+      final List<AbstractActor> cycleAA, final ExpressionsBasedModel model) {
+    final int cycleSize = cycleAA.size();
+    if (cycleSize < 2) {
+      return;
+    }
+
+    // the constraint expression must be always equal to 0
+    final Expression expression = model.addExpression("Cycle").level(0L);
+
+    final AffineRelation[] ars = new AffineRelation[cycleSize];
+    final long[] coefsPhi = new long[cycleSize];
+    for (int i = 0; i < coefsPhi.length; ++i) {
+      coefsPhi[i] = 1;
+    }
+    int nbPhi = 0;
+    long mulN = 1;
+    long mulD = 1;
+
+    // cycle is redundant (last == first)
+    final Iterator<AbstractActor> aaIterator = cycleAA.iterator();
+    AbstractActor dest = aaIterator.next();
+    while (aaIterator.hasNext()) {
+      final AbstractActor src = dest;
+      dest = aaIterator.next();
+      final AffineRelation ar = ddgAR.getEdge(src, dest);
+      ars[nbPhi] = ar;
+
+      for (int i = 0; i < nbPhi; ++i) {
+        coefsPhi[i] *= ar.nProd;
+      }
+      for (int i = nbPhi + 1; i < coefsPhi.length; ++i) {
+        coefsPhi[i] *= ar.dCons;
+      }
+      mulN *= ar.nProd;
+      mulD *= ar.dCons;
+      long g = MathFunctionsHelper.gcd(mulN, mulD);
+      mulN /= g;
+      mulD /= g;
+      g = coefsPhi[0];
+      for (int i = 0; i < coefsPhi.length; ++i) {
+        g = MathFunctionsHelper.gcd(g, coefsPhi[i]);
+      }
+      for (int i = 0; i < coefsPhi.length; ++i) {
+        coefsPhi[i] = coefsPhi[i] / g;
+      }
+      ++nbPhi;
+    }
+
+    if (mulN != mulD) {
+      throw new PreesmRuntimeException("Some cycles do not satisfy consistency Part 1.");
+    }
+    for (int i = 0; i < ars.length - 1; ++i) {
+      final long coefSign = ars[i].phiNegate ? -1L : 1L;
+      final int index_2 = ars[i].phiIndex * 2;
+      final Variable varPhiPos = model.getVariable(index_2);
+      expression.set(varPhiPos, coefsPhi[i] * coefSign);
+      final Variable varPhiNeg = model.getVariable(index_2 + 1);
+      expression.set(varPhiNeg, coefsPhi[i] * (-coefSign));
+    }
+
+  }
+
+  /**
+   * Fill the model with underflow and overflow equations. Also set the minimization objective.
+   * <p>
+   * See Adnan Bouakaz thesis p. 72-78.
+   * 
+   * @param model
+   *          Model to consider.
+   * @param fifoToSizeVariableID
+   *          Map of fifo to variable index in model (to be updated).
+   * @param lambdaPerPort
+   *          Map of port to lambda to consider.
+   * @param fifo
+   *          Fifo to consider.
+   * @param ar
+   *          Affine Relation to consider.
+   */
+  protected static void generateChannelConstraint(final ExpressionsBasedModel model,
+      final Map<Fifo, Integer> fifoToSizeVariableID, final Map<DataPort, LongFraction> lambdaPerPort, final Fifo fifo,
+      final AffineRelation ar) {
+    final Variable sizeVar = new Variable("size_");
+    sizeVar.setInteger(true);
+    sizeVar.lower(0L); // could be refined to max(prod, cons, delau)
+    model.addVariable(sizeVar);
+    // TODO
+    // compute delay if any
+    // compute coefficient
+    // write equations
   }
 
 }
