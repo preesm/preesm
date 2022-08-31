b78f596acb718de986daa49171d64c986e1c690b
Dardaillon Mickael <mickael.dardaillon@insa-rennes.fr>
2022-08-31 12:43:24 +0200

    [fpga] Add explicit pipeline pragma in packing

--- plugins/org.preesm.algorithm/resources/stdfiles/xilinxCodegen/packing.hpp
+++ plugins/org.preesm.algorithm/resources/stdfiles/xilinxCodegen/packing.hpp
@@ -6,6 +6,7 @@
 template<int IN_W, int OUT_W, typename in_t> void packTokens(
 		hls::stream<in_t> &input, hls::stream<ap_uint<OUT_W>> &output) {
 	static_assert(OUT_W%IN_W == 0, "Output width should be a multiple of the input width");
+#pragma HLS PIPELINE II=OUT_W/IN_W
 
 	ap_uint<OUT_W> buffer;
 	for (int i = 0; i < OUT_W / IN_W; i++) {
@@ -18,6 +19,8 @@ template<int IN_W, int OUT_W, typename in_t> void packTokens(
 template<int IN_W, int OUT_W, typename out_t> void unpackTokens(
 		hls::stream<ap_uint<IN_W>> &input, hls::stream<out_t> &output) {
 	static_assert(IN_W%OUT_W == 0, "Input width should be a multiple of the output width");
+#pragma HLS PIPELINE II=IN_W/OUT_W
+
 	ap_uint<IN_W> buffer = input.read();
 	for (int i = 0; i < IN_W / OUT_W; i++) {
 #pragma HLS UNROLL
