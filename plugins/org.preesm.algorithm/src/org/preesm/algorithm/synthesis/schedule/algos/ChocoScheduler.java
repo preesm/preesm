9bf7128446d0f4d265e6320d8f2d72cfa04f1b54
Julien Heulot <jheulot@insa-rennes.fr>
2022-12-21 17:09:18 +0100

    Revert "TO FIX: small dependencies pbs"
    This reverts commit c7d76fca07c42c338fa5b5fe56fab8bae22abba4.

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/algos/ChocoScheduler.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/synthesis/schedule/algos/ChocoScheduler.java
@@ -178,7 +178,7 @@ public class ChocoScheduler extends PeriodicScheduler {
     }
 
     if (verbose) {
-      // solver.setOut(System.err);
+      solver.setOut(System.err);
       solver.printStatistics();
     }
 
