30c33772fee030f5bcae44193b9452daecac3e7c
Dardaillon Mickael <mickael.dardaillon@insa-rennes.fr>
2023-01-11 19:04:40 +0100

    [fpga] Reimplement detection of steady state for cosim

--- plugins/org.preesm.algorithm/resources/templates/xilinxCodegen/template_script_iterative_cosim.py
+++ plugins/org.preesm.algorithm/resources/templates/xilinxCodegen/template_script_iterative_cosim.py
@@ -16,43 +16,55 @@ graph_ii = $PREESM_GRAPH_II
 #[[#]]# DSE constants and variables
 INI_BUF_DEPTH = 5
 INI_LAMBDA_SUM = 4
-COEF_II = 0.99
 COEF_LAMBDAS = 0.5
-nb_iterations_cosim = 2
+nb_iterations = 2
+total_nb_cosim = 0
+total_nb_iterations = 0
 
 #[[#]]# DSE options
 use_ressource_wise = True
 use_lambdas = True
 use_initial_tests = True
+detect_steady_state = True
 
 def run_cosim(buffer_sizes):
     write_buffer_sizes(buffer_sizes)
     subprocess.run(['vitis_hls', 'scripts/script_hls.tcl',  'cosim', top_kernel_name, top_kernel_name + '.cpp'])
-    cosim_ii_list = get_cosim_ii_list()
-    # Reduce number of required iterations based on II results
-    good_cosim_ii = [v for v in cosim_ii_list if is_expected_ii([v])]
-    if len(good_cosim_ii) > 0:
-        global nb_iterations_cosim
-        nb_iterations_cosim = cosim_ii_list.index(good_cosim_ii[0]) + 2
-    return cosim_ii_list
+    global total_nb_cosim
+    total_nb_cosim += 1
+    global nb_iterations
+    global total_nb_iterations
+    total_nb_iterations += nb_iterations
+    cosim_timings = get_cosim_timings()
+    # Reduce number of required iterations based on ET results
+    if is_expected_ii(cosim_timings) and detect_steady_state:
+        nb_iterations = cosim_timings[0].index(cosim_timings[0][-1]) + 2
+    return cosim_timings
 
-def get_cosim_ii_list():
+def get_cosim_timings():
     try:
         with open(top_kernel_name + '/solution1/sim/report/verilog/result.transaction.rpt') as file:
-            results = file.readlines()[1:-1]
-            results = [int(result.split()[3]) for result in results]
-            return results
+            results = file.readlines()
+            et_list = list([int(result.split()[2]) for result in results[1:]])
+            ii_list = list([int(result.split()[3]) for result in results[1:-1]])
+            return (et_list, ii_list)
     except FileNotFoundError:
-        return [-1]
+        return ([-1],[-1])
 
-def is_expected_ii(cosim_ii_list):
-    return max(cosim_ii_list) <= graph_ii and max(cosim_ii_list) > graph_ii * COEF_II
+def is_expected_ii(cosim_timings):
+    return max(cosim_timings[1]) <= graph_ii and is_steady_state(cosim_timings)
+
+def is_steady_state(cosim_timings):
+    if detect_steady_state:
+        return len([v for v in cosim_timings[0] if v == cosim_timings[0][-1] and v > 0]) > 1
+    else:
+        return True
 
 def write_buffer_sizes(buffer_sizes):
     with open('PreesmAutoDefinedSizes.h', 'a') as file:
         for i in range(len(names)):
             file.write('#[[#]]#define ' + names[i] + ' ' + str(int(buffer_sizes[i])) + '\n')
-        file.write('#[[#]]#define NB_ITERATIONS_COSIM ' + str(nb_iterations_cosim) + '\n')
+        file.write('#[[#]]#define NB_ITERATIONS_COSIM ' + str(nb_iterations) + '\n')
 
 def candidate_buffer_size(lower, upper, width):
     if use_ressource_wise:
@@ -89,66 +101,55 @@ def is_improved_ressource_wise(candidate, upper, width):
 
 def iterative_cosim():
     # Start by setting the number of iterations of cosim to reach steady state
-    cosim_ii_list = run_cosim(upper_bound)
-    nb_cosim = 1
-    if cosim_ii_list == [-1]:
+    cosim_timings = run_cosim(upper_bound)
+    if cosim_timings[1] == [-1]:
         raise ValueError('Graph deadlocked with original buffer sizes')
-    while not is_expected_ii(cosim_ii_list):
-        if max(cosim_ii_list) > graph_ii:
+    while not is_expected_ii(cosim_timings):
+        if max(cosim_timings[1]) > graph_ii:
             raise ValueError('Graph does not reach II with original buffer sizes')
-        global nb_iterations_cosim
-        nb_iterations_cosim = nb_iterations_cosim * 2
-        cosim_ii_list = run_cosim(upper_bound)
-        nb_cosim += 1
+        global nb_iterations
+        nb_iterations = nb_iterations * 2
+        cosim_timings = run_cosim(upper_bound)
     # Perform cosim using the different strategies
     if use_initial_tests:
-        nb_cosim += initial_tests_cosim()
+        initial_tests_cosim()
     if use_lambdas:
-        nb_cosim += lambda_iterative_cosim()
-    nb_cosim += sequential_iterative_cosim()
+        lambda_iterative_cosim()
+    sequential_iterative_cosim()
     write_buffer_sizes(upper_bound)
-    return nb_cosim
 
 def sequential_iterative_cosim():
-    nb_cosim = 0
     for i in range(len(names)):
         buffer_sizes = [x for x in upper_bound]
         buffer_sizes[i] = candidate_buffer_size(lower_bound[i], upper_bound[i], widths[i])
         while is_improved(buffer_sizes[i], upper_bound[i], widths[i]):
-            cosim_ii_list = run_cosim(buffer_sizes)
-            nb_cosim += 1
-            if is_expected_ii(cosim_ii_list):
+            cosim_timings = run_cosim(buffer_sizes)
+            if is_expected_ii(cosim_timings):
                 upper_bound[i] = buffer_sizes[i]
             else:
                 lower_bound[i] = buffer_sizes[i]
             buffer_sizes[i] = candidate_buffer_size(lower_bound[i], upper_bound[i], widths[i])
-    return nb_cosim
 
 def initial_tests_cosim():
-    nb_cosim = 0
     for i in range(len(names)):
         buffer_sizes = [x for x in upper_bound]
         buffer_sizes[i] = INI_BUF_DEPTH
         if is_improved(buffer_sizes[i], upper_bound[i], widths[i]):
-            cosim_ii_list = run_cosim(buffer_sizes)
-            nb_cosim += 1
-            if is_expected_ii(cosim_ii_list):
+            cosim_timings = run_cosim(buffer_sizes)
+            if is_expected_ii(cosim_timings):
                 upper_bound[i] = buffer_sizes[i]
                 lambdas[i] = INI_LAMBDA_SUM
             else:
                 lower_bound[i] = buffer_sizes[i]
-    return nb_cosim
 
 def lambda_iterative_cosim():
-    nb_cosim = 0
     while max(lambdas) > 0:
         candidates = [i for (i, c) in enumerate(lambdas) if c >  max(lambdas)* COEF_LAMBDAS]
         buffer_sizes = [x for x in upper_bound]
         for i in candidates:
             buffer_sizes[i] = candidate_buffer_size(lower_bound[i], upper_bound[i], widths[i])
-        cosim_ii_list = run_cosim(buffer_sizes)
-        nb_cosim += 1
-        if is_expected_ii(cosim_ii_list):
+        cosim_timings = run_cosim(buffer_sizes)
+        if is_expected_ii(cosim_timings):
             for i in candidates:
                 upper_bound[i] = buffer_sizes[i]
                 lambdas[i] = lambdas[i] / 2
@@ -158,20 +159,19 @@ def lambda_iterative_cosim():
             for i in candidates:
                 buffer_sizes = [x for x in upper_bound]
                 buffer_sizes[i] = candidate_buffer_size(lower_bound[i], upper_bound[i], widths[i])
-                cosim_ii_list = run_cosim(buffer_sizes)
-                nb_cosim += 1
-                if is_expected_ii(cosim_ii_list):
+                cosim_timings = run_cosim(buffer_sizes)
+                if is_expected_ii(cosim_timings):
                     upper_bound[i] = buffer_sizes[i]
                     lambdas[i] = lambdas[i] / 2
                 else:
                     lower_bound[i] = buffer_sizes[i]
                     lambdas[i] = 0
-    return nb_cosim
 
 if __name__=="__main__":
     start = time.time()
-    nb_cosim = iterative_cosim()
+    iterative_cosim()
     end = time.time()
     print('buffer sizes: ', upper_bound)
-    print('nb_cosim: ', nb_cosim)
+    print('total_nb_cosim: ', total_nb_cosim)
+    print('total_nb_iterations: ', total_nb_iterations)
     print('runtime: ', end - start)
