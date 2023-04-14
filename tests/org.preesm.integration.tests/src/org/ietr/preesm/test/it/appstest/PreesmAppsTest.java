33fa5e0491cee3e629e22dd4e3024799939cdba9
Miomandre Hugo <hugo.miomandre@insa-rennes.fr>
2023-04-14 21:37:38 +0200

    Dynamic tests on preesm apps now functional

--- tests/org.preesm.integration.tests/src/org/ietr/preesm/test/it/appstest/PreesmAppsTest.java
+++ tests/org.preesm.integration.tests/src/org/ietr/preesm/test/it/appstest/PreesmAppsTest.java
@@ -1,7 +1,5 @@
 package org.ietr.preesm.test.it.appstest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,22 +9,24 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.apache.commons.lang3.SystemUtils;
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.ietr.preesm.test.it.api.WorkflowRunner;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.preesm.commons.exceptions.PreesmResourceException;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -39,10 +39,10 @@ public class PreesmAppsTest {
 
   private final String CI_FILENAME = ".ci.yaml";
 
-  private final String PROJECT_KEY  = "project";
-  private final String PATH_KEY     = "path";
   private final String SCENARIO_KEY = "scenario";
   private final String WORKFLOW_KEY = "workflow";
+
+  private static final String PREESM_APP_REPO = "https://github.com/preesm/preesm-apps.git";
 
   private static File preesmAppsFolder;
 
@@ -56,7 +56,6 @@ public class PreesmAppsTest {
 
   @BeforeAll
   public static void setupTest() throws IOException, InvalidRemoteException, TransportException, GitAPIException {
-    System.out.println("Before tests begin.");
 
     // Create temp folder with specific access
     if (SystemUtils.IS_OS_UNIX) {
@@ -64,109 +63,60 @@ public class PreesmAppsTest {
           .asFileAttribute(PosixFilePermissions.fromString("rwx------"));
       preesmAppsFolder = Files.createTempDirectory("preesmAppsFolder", attr).toFile();
     } else {
+
+      boolean fail = true;
+
       preesmAppsFolder = Files.createTempDirectory("preesmAppsFolder").toFile();
-      preesmAppsFolder.setReadable(true, true);
-      preesmAppsFolder.setWritable(true, true);
-      preesmAppsFolder.setExecutable(true, true);
+      fail &= preesmAppsFolder.setReadable(true, true);
+      fail &= preesmAppsFolder.setWritable(true, true);
+      fail &= preesmAppsFolder.setExecutable(true, true);
+
+      Assertions.assertTrue(fail);
     }
 
-    // pulling preesm-apps repo with submodules
-    // Git.cloneRepository().setURI("https://github.com/preesm/preesm-apps.git").setDirectory(preesmAppsFolder)
-    // .setCloneSubmodules(true).call();
-    preesmAppsFolder = new File("/home/miomand/git/temp/preesm-apps");
+    preesmAppsFolder.deleteOnExit();
 
-    System.out.println("Before tests end.");
+    // pulling preesm-apps repo with submodules
+    Git.cloneRepository().setURI(PREESM_APP_REPO).setDirectory(preesmAppsFolder).setCloneSubmodules(true).call();
   }
 
-  // @TestFactory // 1
-  // Stream<DynamicTest> dynamicTestStream() { // 2
-  // return IntStream.of(0, 3, 6, 9)
-  // .mapToObj(v -> DynamicTest.dynamicTest(v + " is a multiple of 3", () -> assertEquals(0, v % 3)) // 3
-  // );
-  // }
+  @TestFactory
+  Stream<DynamicTest> preesmAppsTestFactory() throws IOException {
 
-  // @TestFactory
-  // Stream<DynamicTest> preesmAppsTestFactory() throws IOException, CoreException {
-  //
-  // final Stream<Path> ciPaths = Files.walk(Paths.get(preesmAppsFolder.toString()))
-  // .filter(f -> f.getFileName().toString().equals(CI_FILENAME));
-  //
-  // final Stream<Map<String, String>> ciCases = ciPaths.map(ciPath -> {
-  // try {
-  // final Yaml yaml = new Yaml();
-  // final InputStream inputStream = new FileInputStream(ciPath.toFile());
-  // return yaml.load(inputStream);
-  // } catch (final IOException e) {
-  // throw new PreesmResourceException(e);
-  // }
-  // });
-  //
-  // return ciCases.map(ciCase -> DynamicTest.dynamicTest(
-  // "Running " + ciCase.get(PROJECT_KEY) + " with " + ciCase.get(SCENARIO_KEY) + " and " + ciCase.get(WORKFLOW_KEY),
-  // () -> {
-  // try {
-  // final String projectName = ciCase.get(PROJECT_KEY);
-  // final String projectRoot = preesmAppsFolder.toString() + "/" + ciCase.get(PATH_KEY) + "/";
-  // final String scenarioFilePathStr = "/Scenarios/" + ciCase.get(SCENARIO_KEY);
-  // final String workflowFilePathStr = "/Workflows/" + ciCase.get(WORKFLOW_KEY);
-  //
-  // final boolean success = WorkflowRunner.runWorkFlow(projectRoot, projectName, workflowFilePathStr,
-  // scenarioFilePathStr);
-  // Assertions.assertTrue(success,
-  // "Workflow [" + workflowFilePathStr + "] with scenario [" + scenarioFilePathStr + "] caused failure");
-  // } catch (IOException | CoreException e) {
-  // throw new PreesmResourceException(e);
-  // }
-  // }));
-  // }
+    return Files.walk(Paths.get(preesmAppsFolder.toString()))
+        .filter(f -> f.getFileName().toString().equals(CI_FILENAME)).flatMap(ciPathLambda -> {
 
-  @Test
-  void mainTest() throws IOException, CoreException {
-    System.out.println("Main tests.");
-    assertTrue(Boolean.TRUE);
-
-    Files.walk(Paths.get(preesmAppsFolder.toString())).filter(f -> f.getFileName().toString().equals(CI_FILENAME))
-        .forEach(ciPathLambda -> {
           final LoaderOptions options = new LoaderOptions();
           final Yaml yaml = new Yaml(new Constructor(CIConfig.class, options));
-          // final Yaml yaml = new Yaml();
-          try {
 
+          try {
             final InputStream inputStream = new FileInputStream(ciPathLambda.toFile());
             final CIConfig appMap = yaml.load(inputStream);
-            // final List<Map<String, String>> config = (List<Map<String, String>>) yaml.load(inputStream);
-            // System.out.println(appMap);
 
-            // final String projectName = appMap.get(PROJECT_KEY);
+            if (appMap.config == null) {
+              appMap.config = Collections.emptyList();
+            }
+
             final String projectRoot = ciPathLambda.toString().replace(CI_FILENAME, "");
             final String projectName = getProjectName(projectRoot);
 
-            appMap.config.stream().filter(f -> !f.isEmpty()).forEach(testConfig -> {
+            return appMap.config.stream().filter(f -> f != null)
+                .map(ciCase -> DynamicTest.dynamicTest(
+                    "Running " + projectName + " with " + ciCase.get(SCENARIO_KEY) + " and " + ciCase.get(WORKFLOW_KEY),
+                    () -> {
+                      final String scenarioFilePathStr = "/Scenarios/" + ciCase.get(SCENARIO_KEY);
+                      final String workflowFilePathStr = "/Workflows/" + ciCase.get(WORKFLOW_KEY);
 
-              final String scenarioFilePathStr = "/Scenarios/" + testConfig.get(SCENARIO_KEY);
-              final String workflowFilePathStr = "/Workflows/" + testConfig.get(WORKFLOW_KEY);
+                      final boolean success = WorkflowRunner.runWorkFlow(projectRoot, projectName, workflowFilePathStr,
+                          scenarioFilePathStr);
+                      Assertions.assertTrue(success, "Workflow [" + workflowFilePathStr + "] with scenario ["
+                          + scenarioFilePathStr + "] caused failure");
+                    }));
 
-              try {
-                final boolean success = WorkflowRunner.runWorkFlow(projectRoot, projectName, workflowFilePathStr,
-                    scenarioFilePathStr);
-                Assertions.assertTrue(success, "Workflow [" + workflowFilePathStr + "] with scenario ["
-                    + scenarioFilePathStr + "] caused failure");
-
-              } catch (IOException | CoreException e) {
-                throw new PreesmResourceException(e);
-              }
-            });
           } catch (final IOException e) {
             throw new PreesmResourceException(e);
           }
         });
-  }
-
-  @AfterAll
-  public static void cleanupTest() throws IOException {
-    System.out.println("After tests begin.");
-    // FileUtils.deleteDirectory(preesmAppsFolder);
-    System.out.println("After tests end.");
   }
 
   private String getProjectName(String projectRoot) {
@@ -195,11 +145,9 @@ public class PreesmAppsTest {
 
     try {
       final SAXParser saxParser = factory.newSAXParser();
-
       final CustomHandler handler = new CustomHandler();
 
       saxParser.parse(projectRoot + ".project", handler);
-
       return handler.projectName;
 
     } catch (SAXException | ParserConfigurationException | IOException e) {
