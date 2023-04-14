f6b1567d6fb97b6dd281ea5d2f0218fd38e8d209
Miomandre Hugo <hugo.miomandre@insa-rennes.fr>
2023-04-15 00:20:13 +0200

    Renamed test packages to be properly picked by sonar

--- tests/org.preesm.algorithm.tests/src/org/preesm/algorithm/synthesis/test/ScheduleIteratorTest.java
+++ tests/org.preesm.algorithm.tests/src/org/preesm/algorithm/synthesis/test/ScheduleIteratorTest.java
@@ -34,7 +34,7 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-package org.preesm.algorithm.synthesis;
+package org.preesm.algorithm.synthesis.test;
 
 import static org.junit.Assert.assertEquals;
 
