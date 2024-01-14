7b2e984dcb3c78a5adede9b3ef540e4645229923
Ophelie-Renaud <ophelie.renaud@insa-rennes.fr>
2024-01-14 17:46:59 +0100

    adding pareto analysis page6

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/node/simulator/AnalysisPage4.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/node/simulator/AnalysisPage4.java
@@ -30,10 +30,12 @@ import org.preesm.commons.files.PreesmIOHelper;
  * @author orenaud
  */
 public class AnalysisPage4 {
-  private final String                                        path;
-  private static final String                                 NET_NAME                 = "multicriteria.csv";
-  private final List<NetworkInfo>                             networkInfoList          = new ArrayList<>();
-  private final List<NetworkInfo>                             networkInfoNormalList    = new ArrayList<>();
+
+  private final String                   path;
+  private static final String            NET_NAME              = "multicriteria.csv";
+  private static final List<NetworkInfo> networkInfoList       = new ArrayList<>();
+  private final List<NetworkInfo>        networkInfoNormalList = new ArrayList<>();
+
   Map<Integer, Map<Integer, Map<Integer, List<NetworkInfo>>>> nodeNetworkInfoNormalMap = new LinkedHashMap<>();
   int                                                         nodeKey                  = 0;
   int                                                         coreKey                  = 0;
@@ -238,6 +240,10 @@ public class AnalysisPage4 {
    */
   public List<NetworkInfo> getNetworkInfoNormalList() {
     return networkInfoNormalList;
+  }
+
+  public static List<NetworkInfo> getnetworkInfoList() {
+    return networkInfoList;
   }
 
   /**
