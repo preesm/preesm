a3a97cafb6652b47723241bab009f6249d1cd1e7
Ophelie-Renaud <ophelie.renaud@insa-rennes.fr>
2024-03-01 08:05:16 +0100

    adding GPU into scheduling

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scape/ClusteringScapeTask.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/scape/ClusteringScapeTask.java
@@ -18,7 +18,10 @@ import org.preesm.model.pisdf.AbstractActor;
 import org.preesm.model.pisdf.Actor;
 import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.scenario.Scenario;
+import org.preesm.model.slam.CPU;
 import org.preesm.model.slam.ComponentInstance;
+import org.preesm.model.slam.GPU;
+import org.preesm.model.slam.check.SlamDesignPEtypeChecker;
 import org.preesm.workflow.elements.Workflow;
 import org.preesm.workflow.implement.AbstractTaskImplementation;
 
@@ -127,16 +130,28 @@ public class ClusteringScapeTask extends AbstractTaskImplementation {
     final Map<String, Object> output = new HashMap<>();
     // return topGraph
     output.put("PiMM", tempGraph);
+    // update scenario, clear the intra cluster actors
     for (final Entry<ComponentInstance, EList<AbstractActor>> gp : scenario.getConstraints().getGroupConstraints()) {
-      final int size = gp.getValue().size();
-      for (int i = 1; !gp.getValue().isEmpty(); i++) {
-        final int k = size - i;
-        gp.getValue().remove(k);
-      }
-      for (final AbstractActor actor : tempGraph.getAllActors()) {
-
-        gp.getValue().add(actor);
-      }
+      gp.getValue().clear();
+    }
+    // add all actor on all resources if full CPU
+    if (SlamDesignPEtypeChecker.isOnlyCPU(scenario.getDesign())) {
+      scenario.getConstraints().getGroupConstraints().forEach(groupConstraint -> {
+        tempGraph.getAllActors().forEach(actor -> {
+          groupConstraint.getValue().add(actor);
+        });
+      });
+      // temporary force srv/urc cluster to be map on GPU other on CPU if dual design
+      // TODO: implementing smarter allocation
+    } else if (SlamDesignPEtypeChecker.isDualCPUGPU(scenario.getDesign())) {
+      scenario.getConstraints().getGroupConstraints().forEach(gp -> {
+        tempGraph.getAllActors().stream()
+            .filter(actor -> (gp.getKey().getComponent() instanceof GPU
+                && (actor.getName().contains("srv") || actor.getName().contains("urc")))
+                || (gp.getKey().getComponent() instanceof CPU
+                    && !(actor.getName().contains("srv") || actor.getName().contains("urc"))))
+            .forEach(gp.getValue()::add);
+      });
     }
 
     // return scenario updated
