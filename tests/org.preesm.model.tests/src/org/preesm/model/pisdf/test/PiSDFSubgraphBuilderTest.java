6460392d4a6ae7e4cfba3fd2f47ad3960dff2190
Miomandre Hugo <hugo.miomandre@insa-rennes.fr>
2024-03-12 14:25:54 +0100

    PiSDFSubgraphBuilderTest: small refacto

--- tests/org.preesm.model.tests/src/org/preesm/model/pisdf/test/PiSDFSubgraphBuilderTest.java
+++ tests/org.preesm.model.tests/src/org/preesm/model/pisdf/test/PiSDFSubgraphBuilderTest.java
@@ -62,7 +62,7 @@ import org.preesm.model.pisdf.util.PiSDFSubgraphBuilder;
 
 /**
  * This class is used to test the PiSDFSubgraphBuilder class.
- * 
+ *
  * @author dgageot
  *
  */
@@ -75,8 +75,8 @@ public class PiSDFSubgraphBuilderTest {
   private AbstractActor actorC;
   private AbstractActor actorD;
   private Parameter     param;
-  private final String  topGraphName = new String("topgraph");
-  private final String  subGraphName = new String("B_C");
+  private final String  topGraphName = "topgraph";
+  private final String  subGraphName = "B_C";
 
   /**
    * Set-up the test environnement
@@ -91,9 +91,9 @@ public class PiSDFSubgraphBuilderTest {
     this.actorD = (AbstractActor) topGraph.lookupAllVertex("D");
     this.param = topGraph.lookupParameterGivenGraph("useless", topGraphName);
     // Regroup the two reference in a list
-    List<AbstractActor> subGraphActors = Arrays.asList(actorB, actorC);
+    final List<AbstractActor> subGraphActors = Arrays.asList(actorB, actorC);
     // Build a PiSDFSubgraphBuilder
-    PiSDFSubgraphBuilder subgraphBuilder = new PiSDFSubgraphBuilder(topGraph, subGraphActors, subGraphName);
+    final PiSDFSubgraphBuilder subgraphBuilder = new PiSDFSubgraphBuilder(topGraph, subGraphActors, subGraphName);
     // Process the transformation
     subgraphBuilder.build();
     // Keep a reference to the builded subgraph
@@ -129,7 +129,7 @@ public class PiSDFSubgraphBuilderTest {
   @Test
   public void testRepetitionCountsOfSubGraph() {
     // Compute the repetition vector
-    Map<AbstractVertex, Long> repetitionVector = PiBRV.compute(topGraph, BRVMethod.LCM);
+    final Map<AbstractVertex, Long> repetitionVector = PiBRV.compute(topGraph, BRVMethod.LCM);
     // Check if the subgraph is repeated 4 times
     Assert.assertEquals(4, repetitionVector.get(subGraph).longValue());
     // Check if the actor B is repeated 2 times
@@ -148,7 +148,7 @@ public class PiSDFSubgraphBuilderTest {
   @Test
   public void testFifoContainedInSubGraph() {
     // Check if the fifo that link B to C is contained
-    Fifo fifoBC = actorB.getDataOutputPorts().get(0).getFifo();
+    final Fifo fifoBC = actorB.getDataOutputPorts().get(0).getFifo();
     Assert.assertEquals(subGraph, fifoBC.getContainingPiGraph());
     // Check that the delay and its actor is contained in the subgraph
     Assert.assertEquals(subGraph, fifoBC.getDelay().getContainingPiGraph());
@@ -162,13 +162,13 @@ public class PiSDFSubgraphBuilderTest {
   public void testFifoOutsideOfSubGraph() {
     // Check that outside delays are outside
     // 1. On fifo from actor A
-    Fifo fifoAB = actorA.getDataOutputPorts().get(0).getFifo();
+    final Fifo fifoAB = actorA.getDataOutputPorts().get(0).getFifo();
     Assert.assertEquals(topGraph, fifoAB.getContainingPiGraph());
     // Check that the delay and its actor is contained in the top graph
     Assert.assertEquals(topGraph, fifoAB.getDelay().getContainingPiGraph());
     Assert.assertEquals(topGraph, fifoAB.getDelay().getActor().getContainingPiGraph());
     // 2. On fifo to actor D
-    Fifo fifoCD = actorD.getDataInputPorts().get(0).getFifo();
+    final Fifo fifoCD = actorD.getDataInputPorts().get(0).getFifo();
     Assert.assertEquals(topGraph, fifoCD.getContainingPiGraph());
     // Check that the delay and its actor is contained in the top graph
     Assert.assertEquals(topGraph, fifoCD.getDelay().getContainingPiGraph());
@@ -179,7 +179,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testDataInputPortsOfSubGraph() {
     // Check the number of data input ports
     Assert.assertEquals(1, subGraph.getDataInputPorts().size());
-    DataInputPort dipSubGraph = subGraph.getDataInputPorts().get(0);
+    final DataInputPort dipSubGraph = subGraph.getDataInputPorts().get(0);
     // Check port name
     Assert.assertTrue(dipSubGraph.getName().contains("in_0"));
     // Check rate
@@ -192,7 +192,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testDataOutputPortsOfSubGraph() {
     // Check the number of data input ports
     Assert.assertEquals(1, subGraph.getDataOutputPorts().size());
-    DataOutputPort dopSubGraph = subGraph.getDataOutputPorts().get(0);
+    final DataOutputPort dopSubGraph = subGraph.getDataOutputPorts().get(0);
     // Check port name
     Assert.assertTrue(dopSubGraph.getName().contains("out_0"));
     // Check rate
@@ -205,7 +205,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testConfigInputPortsOfSubGraph() {
     // Check the number of data input ports
     Assert.assertEquals(1, subGraph.getConfigInputPorts().size());
-    ConfigInputPort cipSubGraph = subGraph.getConfigInputPorts().get(0);
+    final ConfigInputPort cipSubGraph = subGraph.getConfigInputPorts().get(0);
     // Check port name
     Assert.assertTrue(cipSubGraph.getName().contains("cfg_0"));
     // Check if setter is param object
@@ -216,7 +216,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testDataInputInterfaceOfSubGraph() {
     // Check the number of data input interfaces
     Assert.assertEquals(1, subGraph.getDataInputInterfaces().size());
-    DataInputInterface diiSubGraph = subGraph.getDataInputInterfaces().get(0);
+    final DataInputInterface diiSubGraph = subGraph.getDataInputInterfaces().get(0);
     // Check interface name
     Assert.assertTrue(diiSubGraph.getName().contains("in_0"));
     // Check if graph port
@@ -233,7 +233,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testDataOutputInterfaceOfSubGraph() {
     // Check the number of data output interfaces
     Assert.assertEquals(1, subGraph.getDataOutputInterfaces().size());
-    DataOutputInterface doiSubGraph = subGraph.getDataOutputInterfaces().get(0);
+    final DataOutputInterface doiSubGraph = subGraph.getDataOutputInterfaces().get(0);
     // Check interface name
     Assert.assertTrue(doiSubGraph.getName().contains("out_0"));
     // Check if graph port
@@ -250,7 +250,7 @@ public class PiSDFSubgraphBuilderTest {
   public void testConfigInputInterfaceOfSubGraph() {
     // Check the number of data output interfaces
     Assert.assertEquals(1, subGraph.getConfigInputInterfaces().size());
-    ConfigInputInterface cfgSubGraph = subGraph.getConfigInputInterfaces().get(0);
+    final ConfigInputInterface cfgSubGraph = subGraph.getConfigInputInterfaces().get(0);
     // Check interface name
     Assert.assertTrue(cfgSubGraph.getName().contains("cfg_0"));
     // Check if graph port
@@ -262,34 +262,34 @@ public class PiSDFSubgraphBuilderTest {
 
   private PiGraph createChainedActorsPiGraph() {
     // Create the top graph
-    PiGraph topGraph = PiMMUserFactory.instance.createPiGraph();
-    topGraph.setName(topGraphName);
-    topGraph.setUrl(topGraphName);
+    final PiGraph chainedActorGraph = PiMMUserFactory.instance.createPiGraph();
+    chainedActorGraph.setName(topGraphName);
+    chainedActorGraph.setUrl(topGraphName);
     // Create 4 actors
-    AbstractActor _actorA = PiMMUserFactory.instance.createActor("A");
-    AbstractActor _actorB = PiMMUserFactory.instance.createActor("B");
-    AbstractActor _actorC = PiMMUserFactory.instance.createActor("C");
-    AbstractActor _actorD = PiMMUserFactory.instance.createActor("D");
+    final AbstractActor chainA = PiMMUserFactory.instance.createActor("A");
+    final AbstractActor chainB = PiMMUserFactory.instance.createActor("B");
+    final AbstractActor chainC = PiMMUserFactory.instance.createActor("C");
+    final AbstractActor chainD = PiMMUserFactory.instance.createActor("D");
     // Create a list for the 4 actors to easily add them to the top graph
-    List<AbstractActor> actorsList = Arrays.asList(_actorA, _actorB, _actorC, _actorD);
+    final List<AbstractActor> actorsList = Arrays.asList(chainA, chainB, chainC, chainD);
     // Add actors to the top graph
-    for (AbstractActor actor : actorsList) {
-      topGraph.addActor(actor);
+    for (final AbstractActor actor : actorsList) {
+      chainedActorGraph.addActor(actor);
     }
     // Create data output and input ports
-    DataOutputPort outputA = PiMMUserFactory.instance.createDataOutputPort("out");
-    DataOutputPort outputB = PiMMUserFactory.instance.createDataOutputPort("out");
-    DataOutputPort outputC = PiMMUserFactory.instance.createDataOutputPort("out");
-    DataInputPort inputB = PiMMUserFactory.instance.createDataInputPort("in");
-    DataInputPort inputC = PiMMUserFactory.instance.createDataInputPort("in");
-    DataInputPort inputD = PiMMUserFactory.instance.createDataInputPort("in");
+    final DataOutputPort outputA = PiMMUserFactory.instance.createDataOutputPort("out");
+    final DataOutputPort outputB = PiMMUserFactory.instance.createDataOutputPort("out");
+    final DataOutputPort outputC = PiMMUserFactory.instance.createDataOutputPort("out");
+    final DataInputPort inputB = PiMMUserFactory.instance.createDataInputPort("in");
+    final DataInputPort inputC = PiMMUserFactory.instance.createDataInputPort("in");
+    final DataInputPort inputD = PiMMUserFactory.instance.createDataInputPort("in");
     // Attach them to actors
-    _actorA.getDataOutputPorts().add(outputA);
-    _actorB.getDataOutputPorts().add(outputB);
-    _actorC.getDataOutputPorts().add(outputC);
-    _actorB.getDataInputPorts().add(inputB);
-    _actorC.getDataInputPorts().add(inputC);
-    _actorD.getDataInputPorts().add(inputD);
+    chainA.getDataOutputPorts().add(outputA);
+    chainB.getDataOutputPorts().add(outputB);
+    chainC.getDataOutputPorts().add(outputC);
+    chainB.getDataInputPorts().add(inputB);
+    chainC.getDataInputPorts().add(inputC);
+    chainD.getDataInputPorts().add(inputD);
     // Create fifos and form a chain such as A -> B -> C -> D
     // Setup data output and input ports rates
     // Repetition vector should be equal to [1, 8, 4, 2]'
@@ -300,38 +300,38 @@ public class PiSDFSubgraphBuilderTest {
     outputC.setExpression(4);
     inputD.setExpression(8);
     // Set delay to fifo AC
-    Delay delayAC = PiMMUserFactory.instance.createDelay();
+    final Delay delayAC = PiMMUserFactory.instance.createDelay();
     delayAC.setExpression(16);
-    Fifo fifoAB = PiMMUserFactory.instance.createFifo(outputA, inputB, "void");
+    final Fifo fifoAB = PiMMUserFactory.instance.createFifo(outputA, inputB, "void");
     fifoAB.assignDelay(delayAC);
-    topGraph.addDelay(delayAC);
+    chainedActorGraph.addDelay(delayAC);
     // Set delay to fifo BC
-    Delay delayBC = PiMMUserFactory.instance.createDelay();
+    final Delay delayBC = PiMMUserFactory.instance.createDelay();
     delayBC.setExpression(2);
-    Fifo fifoBC = PiMMUserFactory.instance.createFifo(outputB, inputC, "void");
+    final Fifo fifoBC = PiMMUserFactory.instance.createFifo(outputB, inputC, "void");
     fifoBC.assignDelay(delayBC);
-    topGraph.addDelay(delayBC);
+    chainedActorGraph.addDelay(delayBC);
     // Set delay to fifo CD
-    Delay delayCD = PiMMUserFactory.instance.createDelay();
+    final Delay delayCD = PiMMUserFactory.instance.createDelay();
     delayCD.setExpression(4);
-    Fifo fifoCD = PiMMUserFactory.instance.createFifo(outputC, inputD, "void");
+    final Fifo fifoCD = PiMMUserFactory.instance.createFifo(outputC, inputD, "void");
     fifoCD.assignDelay(delayCD);
-    topGraph.addDelay(delayCD);
+    chainedActorGraph.addDelay(delayCD);
     // Create a list for the 3 fifos to easily add them to the top graph
-    List<Fifo> fifosList = Arrays.asList(fifoAB, fifoBC, fifoCD);
+    final List<Fifo> fifosList = Arrays.asList(fifoAB, fifoBC, fifoCD);
     // Add fifos to the top graph
-    for (Fifo fifo : fifosList) {
-      topGraph.addFifo(fifo);
+    for (final Fifo fifo : fifosList) {
+      chainedActorGraph.addFifo(fifo);
     }
     // Add a parameter to actor B
-    Parameter parameter = PiMMUserFactory.instance.createParameter("useless", 2);
-    ConfigInputPort configInputB = PiMMUserFactory.instance.createConfigInputPort();
-    _actorB.getConfigInputPorts().add(configInputB);
-    Dependency dependency = PiMMUserFactory.instance.createDependency(parameter, configInputB);
-    topGraph.addParameter(parameter);
-    topGraph.addDependency(dependency);
+    final Parameter parameter = PiMMUserFactory.instance.createParameter("useless", 2);
+    final ConfigInputPort configInputB = PiMMUserFactory.instance.createConfigInputPort();
+    chainB.getConfigInputPorts().add(configInputB);
+    final Dependency dependency = PiMMUserFactory.instance.createDependency(parameter, configInputB);
+    chainedActorGraph.addParameter(parameter);
+    chainedActorGraph.addDependency(dependency);
     // Return top graph
-    return topGraph;
+    return chainedActorGraph;
   }
 
 }
