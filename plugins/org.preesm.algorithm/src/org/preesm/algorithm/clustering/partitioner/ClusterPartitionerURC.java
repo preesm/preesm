0b0edd6e399975004c9ae01e09bb915ce0a60028
Ewen Michel <ewenquentin.michel@gmail.com>
2024-07-23 12:20:40 +1000

    Fixed GPU timing

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/partitioner/ClusterPartitionerURC.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/partitioner/ClusterPartitionerURC.java
@@ -35,18 +35,25 @@
  */
 package org.preesm.algorithm.clustering.partitioner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.preesm.algorithm.clustering.ClusteringHelper;
+import org.preesm.commons.graph.Vertex;
 import org.preesm.model.pisdf.AbstractActor;
 import org.preesm.model.pisdf.AbstractVertex;
+import org.preesm.model.pisdf.Actor;
+import org.preesm.model.pisdf.BroadcastActor;
 import org.preesm.model.pisdf.DataInputInterface;
 import org.preesm.model.pisdf.DataInputPort;
 import org.preesm.model.pisdf.DataOutputInterface;
 import org.preesm.model.pisdf.DataOutputPort;
+import org.preesm.model.pisdf.DataPort;
 import org.preesm.model.pisdf.Delay;
 import org.preesm.model.pisdf.Fifo;
+import org.preesm.model.pisdf.InterfaceActor;
 import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.pisdf.brv.BRVMethod;
 import org.preesm.model.pisdf.brv.PiBRV;
@@ -54,7 +61,11 @@ import org.preesm.model.pisdf.factory.PiMMUserFactory;
 import org.preesm.model.pisdf.util.PiSDFSubgraphBuilder;
 import org.preesm.model.pisdf.util.URCSeeker;
 import org.preesm.model.scenario.Scenario;
+import org.preesm.model.slam.CPU;
 import org.preesm.model.slam.ComponentInstance;
+import org.preesm.model.slam.GPU;
+import org.preesm.model.slam.TimingType;
+import org.preesm.model.slam.check.SlamDesignPEtypeChecker;
 
 /**
  * This class provide an algorithm to cluster a PiSDF graph and balance actor firings of clustered actor between coarse
@@ -216,12 +227,266 @@ public class ClusterPartitionerURC {
     return this.graph;
   }
 
+  private void mergeFIFO(PiGraph subGraph) {
+    final Map<BroadcastActor, List<InterfaceActor>> sourceMap = redundantFIFOMap(subGraph);
+
+    // check matching opportunities
+    for (final Entry<BroadcastActor, List<InterfaceActor>> map : sourceMap.entrySet()) {
+      // merge redundant buffer when they are more than 2
+      if (map.getValue().size() > 1) {
+        final DataPort sourcePort = map.getValue().get(0).getDataPort();
+        final DataPort targetPort0 = map.getValue().get(0).getDataPort().getFifo().getTargetPort();
+        // add a new broadcast inside cluster to retrieve the scaling
+        final BroadcastActor brd = PiMMUserFactory.instance.createBroadcastActor();
+        brd.setName("Broadcast_Merge_" + map.getKey().getName());
+        brd.setContainingGraph(subGraph);
+        // connect interface to the new broadcast
+        final DataInputPort dataInputPort = PiMMUserFactory.instance.createDataInputPort();
+        dataInputPort.setName("in");
+        final Long dt = map.getValue().get(0).getDataPort().getExpression().evaluate();
+        dataInputPort.setExpression(dt);
+        brd.getDataInputPorts().add(dataInputPort);
+        final Fifo fin = PiMMUserFactory.instance.createFifo();
+        fin.setType(sourcePort.getFifo().getType());
+        fin.setSourcePort((DataOutputPort) sourcePort);
+        fin.setTargetPort(dataInputPort);
+        fin.setContainingGraph(subGraph);
+        // connect broadcast to former targets
+        for (final InterfaceActor iActor : map.getValue()) {
+          final DataOutputPort dataOutputPort;
+          if (iActor.equals(map.getValue().get(0))) {
+            dataOutputPort = PiMMUserFactory.instance.copy(iActor.getDataOutputPorts().get(0));
+            final Fifo fout = PiMMUserFactory.instance.createFifo();
+            fout.setType(fin.getType());
+            fout.setSourcePort(dataOutputPort);
+            fout.setTargetPort((DataInputPort) targetPort0);
+            fout.setContainingGraph(subGraph);
+          } else {
+            dataOutputPort = iActor.getDataOutputPorts().get(0);
+          }
+          brd.getDataOutputPorts().add(dataOutputPort);
+
+          // remove FIFO connecting the former redundant interface
+          if (!iActor.equals(map.getValue().get(0))) {
+            final Fifo oldFifo = iActor.getGraphPort().getFifo();
+            final DataOutputPort oldPort = iActor.getGraphPort().getFifo().getSourcePort();
+            ((AbstractActor) iActor.getGraphPort().getFifo().getSource()).getDataOutputPorts().remove(oldPort);
+            iActor.getGraphPort().getContainingActor().getContainingPiGraph().removeFifo(oldFifo);
+          }
+        }
+        // remove former redundant interface
+        subGraph.getDataInputInterfaces().stream().filter(x -> x.getDataOutputPorts().isEmpty())
+            .forEach(subGraph::removeActor);
+      }
+
+    }
+
+    subGraph.getAllDataPorts().forEach(port -> port.setName(port.getName().substring(port.getName().indexOf('_') + 1)));
+    subGraph.getDataInterfaces()
+        .forEach(port -> port.setName(port.getName().substring(port.getName().indexOf('_') + 1)));
+  }
+
+  private Map<BroadcastActor, List<InterfaceActor>> redundantFIFOMap(PiGraph subGraph) {
+    final Map<BroadcastActor, List<InterfaceActor>> sourceMap = new HashMap<>();
+    // Identify the mergeable buffer
+    for (final InterfaceActor iActor : subGraph.getDataInterfaces()) {
+      if (iActor.getGraphPort().getFifo().getSource() instanceof final BroadcastActor broadcast) {
+        // Check if the map already contains the broadcast actor
+        if (sourceMap.containsKey(broadcast)) {
+          // Add the InterfaceActor to the existing list
+          sourceMap.get(broadcast).add(iActor);
+        } else {
+          // Create a new list and add the InterfaceActor
+          final List<InterfaceActor> list = new ArrayList<>();
+          list.add(iActor);
+          // Put the new list in the map
+          sourceMap.put(broadcast, list);
+        }
+      }
+    }
+    return sourceMap;
+  }
+
+  public static void reduceMemExMatches(PiGraph subGraph, Long scale, Map<AbstractVertex, Long> rv) {
+    for (final InterfaceActor iActor : subGraph.getDataInterfaces()) {
+      if (iActor.getGraphPort().getFifo().getSource() instanceof final BroadcastActor broadcast) {
+        final Long brdInput = broadcast.getDataInputPorts().get(0).getExpression().evaluate();
+        final long interfaceRate = iActor.getGraphPort().getExpression().evaluate();
+        final DataInputPort targetPort = iActor.getDataPort().getFifo().getTargetPort();
+        // set output broadcast expression
+        iActor.getGraphPort().getFifo().getSourcePort().setExpression(brdInput * scale);
+        // scale cluster in put on broadcast new value
+        iActor.getGraphPort().setExpression(brdInput * rv.get(broadcast));
+        iActor.getDataPort().setExpression(brdInput * rv.get(broadcast));
+
+        // add a new broadcast inside cluster to retrieve the scaling
+        final BroadcastActor brd = PiMMUserFactory.instance.createBroadcastActor();
+        brd.setName("Broadcast_" + broadcast.getName());
+        brd.setContainingGraph(subGraph);
+        // connect interface to the new broadcast
+        final DataInputPort dataInputPort = PiMMUserFactory.instance.createDataInputPort();
+        dataInputPort.setName("in");
+        final Long dt = brdInput * rv.get(broadcast);
+        dataInputPort.setExpression(dt);
+        brd.getDataInputPorts().add(dataInputPort);
+        final Fifo fin = PiMMUserFactory.instance.createFifo();
+        fin.setType(iActor.getDataPort().getFifo().getType());
+        fin.setSourcePort(iActor.getDataPort().getFifo().getSourcePort());
+        fin.setTargetPort(dataInputPort);
+        fin.setContainingGraph(subGraph);
+        // connect broadcast to target
+        final DataOutputPort dataOutputPort = PiMMUserFactory.instance.createDataOutputPort();
+        dataOutputPort.setName("out");
+        final Long rt = interfaceRate;
+        dataOutputPort.setExpression(rt);
+        brd.getDataOutputPorts().add(dataOutputPort);
+        final Fifo fout = PiMMUserFactory.instance.createFifo();
+        fout.setType(iActor.getDataPort().getFifo().getType());
+        fout.setSourcePort(dataOutputPort);
+        fout.setTargetPort(targetPort);
+        fout.setContainingGraph(subGraph);
+
+      }
+
+    }
+
+  }
+
+  public static Object[] mapping(List<AbstractActor> urc, Scenario scenario, int numberOfPEs,
+      Map<AbstractVertex, Long> brv) {
+    final Component cpu = scenario.getDesign().getOperatorComponentInstances().stream()
+        .map(ComponentInstance::getComponent).filter(component -> component instanceof CPU).findFirst().orElseThrow();
+    final Long timingCPU = timingCPU(urc, scenario, brv, cpu);
+    final Long timingGPU = timingGPU(urc, scenario);
+
+    // EWEN MODIFS
+    boolean isGPUPossible = false;
+    for (final AbstractActor act : urc) {
+      for (final ComponentInstance comp : scenario.getPossibleMappings(act)) {
+        if (comp.getInstanceName().equals("GPU")) {
+          isGPUPossible = true;
+        }
+      }
+    }
+
+    if (timingCPU > timingGPU && isGPUPossible) {
+      for (final AbstractActor act : urc) {
+        for (final ComponentInstance comp : scenario.getPossibleMappings(act)) {
+          if (comp.getInstanceName().equals("GPU")) {
+            act.setOnGPU(true);
+          }
+        }
+      }
+    }
+    // END EWEN MODIFS
+
+    if (SlamDesignPEtypeChecker.isOnlyCPU(scenario.getDesign()) || timingCPU < timingGPU || !isGPUPossible) {
+
+      return new Object[] { (long) numberOfPEs, Boolean.FALSE };
+    }
+
+    final Long gpuCount = scenario.getDesign().getOperatorComponentInstances().stream()
+        .filter(opId -> opId.getComponent() instanceof GPU).count();
+    return new Object[] { gpuCount, Boolean.TRUE };
+  }
+
+  public static Long timingCPU(List<AbstractActor> urc, Scenario scenario, Map<AbstractVertex, Long> brv,
+      Component cpu) {
+
+    Long sum = 0L;
+    for (final AbstractActor actor : urc) {
+      if (actor instanceof Actor) {
+        final AbstractActor aaa = scenario.getTimings().getActorTimings().keySet().stream()
+            .filter(aa -> actor.getName().equals(aa.getName())).findFirst().orElse(null);
+
+        sum += scenario.getTimings().evaluateTimingOrDefault(aaa, cpu, TimingType.EXECUTION_TIME) * brv.get(actor);
+      }
+    }
+    return sum;
+  }
+
+  public static Long timingGPU(List<AbstractActor> urc, Scenario scenario) {
+    if (SlamDesignPEtypeChecker.isDualCPUGPU(scenario.getDesign())) {
+      final GPU gpu = (GPU) scenario.getDesign().getOperatorComponentInstances().stream()
+          .map(ComponentInstance::getComponent).filter(component -> component instanceof GPU).findFirst().orElseThrow();
+
+      final Long offloading = offLoadingCost(gpu, urc);
+      final Long timing = urc.stream().filter(actor -> actor instanceof Actor).mapToLong(actor -> {
+        final AbstractActor aaa = scenario.getTimings().getActorTimings().keySet().stream()
+            .filter(aa -> actor.getName().equals(aa.getName())).findFirst().orElse(null);
+
+        return scenario.getTimings().evaluateTimingOrDefault(aaa, gpu, TimingType.EXECUTION_TIME);
+      }).sum();
+      return offloading + timing;
+
+    }
+    return Long.MAX_VALUE;
+  }
+
+  private static Long offLoadingCost(GPU gpu, List<AbstractActor> urc) {
+    // If GPU parameters are different from 0, use their value, otherwise default to 1
+    final Long dedicatedMemSpeed = (long) gpu.getDedicatedMemSpeed() != 0 ? (long) gpu.getDedicatedMemSpeed() : 1;
+    final Long unifiedMemSpeed = (long) gpu.getUnifiedMemSpeed() != 0 ? (long) gpu.getUnifiedMemSpeed() : 1;
+    final String memoryToUse = gpu.getMemoryToUse();
+
+    Long sum = 0L;
+    // build the list of PiGraph future DataPort
+    final List<DataPort> dataPortList = new LinkedList<>();
+
+    for (final AbstractActor actor : urc) {
+      for (final DataPort port : actor.getAllDataPorts()) {
+        // filter add port if target XOR source is in the list
+        if (urc.contains(port.getFifo().getSource()) ^ urc.contains(port.getFifo().getTarget())) {
+          dataPortList.add(port);
+        }
+      }
+    }
+
+    for (final DataPort dataPort : dataPortList) {
+      final Long gpuInputSize = dataPort.getPortRateExpression().evaluate();
+      double time;
+
+      if (memoryToUse.equalsIgnoreCase("unified")) {
+        time = ((double) gpuInputSize / (double) unifiedMemSpeed);
+      } else {
+        time = ((double) gpuInputSize / (double) dedicatedMemSpeed);
+      }
+
+      sum += (long) time;
+    }
+
+    for (final AbstractActor act : urc) {
+      for (final DataOutputPort outPort : act.getDataOutputPorts()) {
+        final Vertex target = outPort.getFifo().getTarget();
+
+        final boolean onGpuAndNextOnCpu = !((AbstractActor) target).isOnGPU() && act.isOnGPU();
+        final boolean onCpuAndNextOnGpu = ((AbstractActor) target).isOnGPU() && !act.isOnGPU();
+
+        if (onGpuAndNextOnCpu || onCpuAndNextOnGpu) {
+          System.out.println(act.getName());
+          final Long gpuInputSize = outPort.getPortRateExpression().evaluate();
+          double time;
+
+          if (memoryToUse.equalsIgnoreCase("unified")) {
+            time = ((double) gpuInputSize / (double) unifiedMemSpeed);
+          } else {
+            time = ((double) gpuInputSize / (double) dedicatedMemSpeed);
+          }
+
+          sum += (long) time;
+        }
+      }
+    }
+
+    return sum;
+  }
+
   /**
    * Used to compute the scaling factor :)
    *
    * @param subGraph
    *          graph
-   * 
+   *
    */
   private Long computeScalingFactor(PiGraph subGraph) {
     final Map<AbstractVertex, Long> rv = PiBRV.compute(subGraph, BRVMethod.LCM);
