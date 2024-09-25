70b9b5d9c1d13070be36d95a609dac88e4384b17
Hugo Miomandre <hugo.miomandre@insa-rennes.fr>
2024-09-25 13:46:13 +0200

    Activate PFastScheduler integration test

--- tests/org.preesm.integration.tests/src/org/ietr/preesm/test/it/appstest/FastSchedulerTest.java
+++ tests/org.preesm.integration.tests/src/org/ietr/preesm/test/it/appstest/FastSchedulerTest.java
@@ -18,14 +18,14 @@ public class FastSchedulerTest {
     Assert.assertTrue("[FAILED] Workflow [" + workflow + "] with scenario [" + scenario + "] failed.", success);
   }
 
-  // @Test
-  // public void testPFastScheduler() throws IOException, CoreException {
-  // final String projectName = "org.ietr.preesm.stereo";
-  // final String scenario = "/Scenarios/" + "8coresC6678.scenario";
-  // final String workflow = "/Workflows/" + "CodegenPFast.workflow";
-  //
-  // final boolean success = WorkflowRunner.runWorkFlow(null, projectName, workflow, scenario);
-  // Assert.assertTrue("[FAILED] Workflow [" + workflow + "] with scenario [" + scenario + "] failed.", success);
-  // }
+  @Test
+  public void testPFastScheduler() throws IOException, CoreException {
+    final String projectName = "org.ietr.preesm.stereo";
+    final String scenario = "/Scenarios/" + "8coresC6678.scenario";
+    final String workflow = "/Workflows/" + "CodegenPFast.workflow";
+
+    final boolean success = WorkflowRunner.runWorkFlow(null, projectName, workflow, scenario);
+    Assert.assertTrue("[FAILED] Workflow [" + workflow + "] with scenario [" + scenario + "] failed.", success);
+  }
 
 }
