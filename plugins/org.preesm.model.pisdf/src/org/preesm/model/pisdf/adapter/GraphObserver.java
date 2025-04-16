c22165dec13c043a708228089e4b15c01fc38b8c
Hugo Miomandre <hugo.miomandre@insa-rennes.fr>
2025-04-16 14:25:34 +0200

    The PiSDF XCore now handles by itself the proper ordering of edges and vertices, and the addition/removal of graph ports associated with interfaces

--- plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/adapter/GraphObserver.java
+++ plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/adapter/GraphObserver.java
@@ -39,29 +39,14 @@
  */
 package org.preesm.model.pisdf.adapter;
 
-import java.util.List;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.preesm.commons.graph.Edge;
-import org.preesm.model.pisdf.AbstractActor;
-import org.preesm.model.pisdf.AbstractVertex;
-import org.preesm.model.pisdf.ConfigInputInterface;
-import org.preesm.model.pisdf.ConfigInputPort;
-import org.preesm.model.pisdf.ConfigOutputPort;
-import org.preesm.model.pisdf.DataInputPort;
-import org.preesm.model.pisdf.DataOutputPort;
-import org.preesm.model.pisdf.DataPort;
 import org.preesm.model.pisdf.Delay;
-import org.preesm.model.pisdf.DelayActor;
-import org.preesm.model.pisdf.Dependency;
 import org.preesm.model.pisdf.Fifo;
-import org.preesm.model.pisdf.InterfaceActor;
-import org.preesm.model.pisdf.Parameter;
 import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.pisdf.PiMMPackage;
-import org.preesm.model.pisdf.Port;
-import org.preesm.model.pisdf.factory.PiMMUserFactory;
 
 /**
  * The purpose of this {@link Adapter} is to observe the {@link Vertex} list and {@link Edge} of a {@link PiGraph} to
@@ -103,181 +88,11 @@ public class GraphObserver extends AdapterImpl {
     // Nothing to do here
   }
 
-  /**
-   * Method called when an {@link AbstractVertex} is possibly added to the Observed {@link PiGraph}. <br>
-   * <br>
-   * This Method create the {@link Port} port corresponding to the added {@link InterfaceActor} or {@link Parameter} and
-   * add it to the {@link PiGraph#getInputPorts()}, the {@link PiGraph#getOutputPorts()}, or the
-   * {@link PiGraph#getConfigInputPorts()} list of the {@link PiGraph}. It also handles the vertex storage indexes
-   *
-   * @param vertex
-   *          The {@link AbstractVertex} added to the {@link PiGraph}
-   * @param graph
-   *          The {@link PiGraph}
-   */
-  protected void addVertex(final AbstractVertex vertex, final PiGraph graph) {
-
-    if ((vertex instanceof AbstractActor) && !(vertex instanceof DelayActor)) {
-      if (vertex instanceof final InterfaceActor iActorVertex) {
-        // If the added vertex is an Interface of the graph
-        addInterfaceActor(iActorVertex, graph);
-      }
-      graph.updateActorIndex();
-    } else if (vertex instanceof final Parameter paramVertex) {
-      if (paramVertex.isConfigurationInterface()) {
-        // If the added vertex is an Parameter and an Interface of the graph
-        addInputParamInterfaceActor((ConfigInputInterface) vertex, graph);
-      }
-      graph.updateParameterIndex();
-    } else if ((vertex instanceof Delay)) {
-      graph.updateDelayIndex();
-    }
-  }
-
-  /**
-   * Method called when an {@link Edge} is possibly added to the Observed {@link PiGraph}. <br>
-   * <br>
-   * It handles the {@link Edge} storage indexes
-   *
-   * @param vertex
-   *          The {@link Edge} added to the {@link PiGraph}
-   * @param graph
-   *          The {@link PiGraph}
-   */
-  protected void addEdge(final Edge edge, final PiGraph graph) {
-
-    if (edge instanceof final Fifo fifo) {
-      if (fifo.isHasADelay()) {
-        graph.updateFifoWithDelayIndex();
-      } else {
-        graph.updateFifoWithoutDelayIndex();
-      }
-    } else if (edge instanceof Dependency) {
-      // Nothing to do
-    }
-  }
-
-  /**
-   * Create the {@link Port} corresponding to an {@link InterfaceActor} added to the {@link PiGraph}.
-   *
-   * @param iActor
-   *          the {@link InterfaceActor} added to the {@link PiGraph}
-   * @param graph
-   *          the observed {@link PiGraph}
-   */
-  protected void addInterfaceActor(final InterfaceActor iActor, final PiGraph graph) {
-
-    // In case of UNDO action, the graph port associated with the interface actor as already been readded
-    if (iActor.getGraphPort() != null && graph.getAllPorts().contains(iActor.getGraphPort())) {
-      return;
-    }
-
-    // Create the Associated port and store it in the appropriate List
-    DataPort port;
-    switch (iActor.getKind()) {
-      case DATA_INPUT:
-        port = PiMMUserFactory.instance.createDataInputPort();
-        graph.getDataInputPorts().add((DataInputPort) port);
-        break;
-      case DATA_OUTPUT:
-        port = PiMMUserFactory.instance.createDataOutputPort();
-        graph.getDataOutputPorts().add((DataOutputPort) port);
-        break;
-      case CFG_OUTPUT:
-        port = PiMMUserFactory.instance.createConfigOutputPort();
-        graph.getConfigOutputPorts().add((ConfigOutputPort) port);
-        break;
-      default:
-        return;
-    }
-
-    // Set the interface properties
-    port.setName(iActor.getName());
-    iActor.setGraphPort(port);
-  }
-
-  /**
-   * Create the {@link Port} corresponding to an interface {@link Parameter} added to the {@link PiGraph}.
-   *
-   * @param param
-   *          the {@link Parameter} added to the {@link PiGraph}
-   * @param graph
-   *          the observed {@link PiGraph}
-   */
-  protected void addInputParamInterfaceActor(final ConfigInputInterface param, final PiGraph graph) {
-    final ConfigInputPort port = PiMMUserFactory.instance.createConfigInputPort();
-    port.setName(param.getName());
-    graph.getConfigInputPorts().add(port);
-
-    // Set the parameter Property
-    param.setGraphPort(port);
-  }
-
   @Override
   public void notifyChanged(final Notification notification) {
     super.notifyChanged(notification);
 
-    // Check if the vertices or Parameters are concerned by this
-    // notification
-    if ((notification.getNotifier() instanceof final PiGraph graph)
-        && (notification.getFeatureID(null) == PiMMPackage.PI_GRAPH__VERTICES)) {
-
-      switch (notification.getEventType()) {
-        case Notification.ADD:
-          // It is safe to cast because we already checked that the
-          // notification was caused by an addition to the graph vertices.
-          final AbstractVertex vertextoAdd = (AbstractVertex) notification.getNewValue();
-          addVertex(vertextoAdd, graph);
-          break;
-
-        case Notification.ADD_MANY:
-          final List<?> listToAdd = (List<?>) notification.getNewValue();
-          listToAdd.forEach(o -> addVertex((AbstractVertex) o, graph));
-          break;
-
-        case Notification.REMOVE:
-          final AbstractVertex vertexToRemove = (AbstractVertex) notification.getOldValue();
-          removeVertex(vertexToRemove, graph);
-          break;
-
-        case Notification.REMOVE_MANY:
-          final List<?> listToRemove = (List<?>) notification.getOldValue();
-          listToRemove.forEach(o -> removeVertex((AbstractVertex) o, graph));
-          break;
-
-        default:
-          // nothing
-      }
-    } else if ((notification.getNotifier() instanceof final PiGraph graph)
-        && (notification.getFeatureID(null) == PiMMPackage.PI_GRAPH__EDGES)) {
-
-      switch (notification.getEventType()) {
-        case Notification.ADD:
-          // It is safe to cast because we already checked that the
-          // notification was caused by an addition to the graph edge.
-          final Edge edgetoAdd = (Edge) notification.getNewValue();
-          addEdge(edgetoAdd, graph);
-          break;
-
-        case Notification.ADD_MANY:
-          final List<?> listToAdd = (List<?>) notification.getNewValue();
-          listToAdd.forEach(o -> addEdge((Edge) o, graph));
-          break;
-
-        case Notification.REMOVE:
-          final Edge edgeToRemove = (Edge) notification.getOldValue();
-          removeEdge(edgeToRemove, graph);
-          break;
-
-        case Notification.REMOVE_MANY:
-          final List<?> listToRemove = (List<?>) notification.getOldValue();
-          listToRemove.forEach(o -> removeEdge((Edge) o, graph));
-          break;
-
-        default:
-          // nothing
-      }
-    } else if ((notification.getNotifier() instanceof final Fifo fifo)
+    if ((notification.getNotifier() instanceof final Fifo fifo)
         && (notification.getFeatureID(null) == PiMMPackage.FIFO__DELAY)) {
 
       final PiGraph graph = fifo.getContainingPiGraph();
@@ -304,85 +119,5 @@ public class GraphObserver extends AdapterImpl {
     }
 
     // TODO Add support when a Parameter changes from a config interface to a non config param
-  }
-
-  /**
-   * Method called when an {@link AbstractVertex} is possibly removed to the Observed {@link PiGraph}. <br>
-   * <br>
-   * This Method remove the {@link Port} port corresponding to the removed {@link InterfaceActor} or {@link Parameter}
-   * and from the {@link PiGraph#getInputPorts()}, the {@link PiGraph#getOutputPorts()}, or the
-   * {@link PiGraph#getConfigInputPorts()} list of the {@link PiGraph}. Also handles storage indexes.
-   *
-   * @param vertex
-   *          The {@link AbstractVertex} removed from the {@link PiGraph}
-   * @param graph
-   *          The {@link PiGraph}
-   */
-  protected void removeVertex(final AbstractVertex vertex, final PiGraph graph) {
-    if ((vertex instanceof final Parameter paramVertex)) {
-      if (paramVertex.isConfigurationInterface()) {
-        // If the added vertex is an Parameter and an Interface of the graph
-        removeInputParamInterfaceActor((ConfigInputInterface) vertex, graph);
-      }
-      graph.updateParameterIndex();
-    } else if ((vertex instanceof AbstractActor) && !(vertex instanceof DelayActor)) {
-      if (vertex instanceof final InterfaceActor iActorVertex) {
-        // Targets DataInputInterface, DataOutputInterface AND ConfigOutputInterface
-        removeInterfaceActor(iActorVertex, graph);
-      }
-      graph.updateActorIndex();
-    } else if (vertex instanceof Delay) {
-      graph.updateDelayIndex();
-    }
-  }
-
-  /**
-   * Method called when an {@link Edge} is possibly removed to the Observed {@link PiGraph}. <br>
-   * <br>
-   * Handles storage indexes.
-   *
-   * @param edge
-   *          The {@link Edge} removed from the {@link PiGraph}
-   * @param graph
-   *          The {@link PiGraph}
-   */
-  protected void removeEdge(final Edge edge, final PiGraph graph) {
-    if (edge instanceof final Fifo fifo) {
-      // If the added vertex is an Interface of the graph
-      if (fifo.isHasADelay()) {
-        graph.updateFifoWithDelayIndex();
-      } else {
-        graph.updateFifoWithoutDelayIndex();
-      }
-    } else if (edge instanceof Dependency) {
-      // Nothing to do
-    }
-  }
-
-  /**
-   * Remove the {@link Port} corresponding to an {@link InterfaceActor} removed to the {@link PiGraph}.
-   *
-   * @param iActor
-   *          the {@link InterfaceActor} removed from the {@link PiGraph}
-   * @param graph
-   *          the observed {@link PiGraph}
-   */
-  protected void removeInterfaceActor(final InterfaceActor iActor, final PiGraph graph) {
-    // We remove from both list, but only one will actually remove something.
-    graph.getDataInputPorts().remove(iActor.getGraphPort());
-    graph.getDataOutputPorts().remove(iActor.getGraphPort());
-    graph.getConfigOutputPorts().remove(iActor.getGraphPort());
-  }
-
-  /**
-   * Remove the {@link Port} corresponding to an Interface {@link Parameter} removed from the {@link PiGraph}.
-   *
-   * @param param
-   *          the Interface {@link Parameter} removed from the {@link PiGraph}
-   * @param graph
-   *          the observed {@link PiGraph}
-   */
-  protected void removeInputParamInterfaceActor(final ConfigInputInterface param, final PiGraph graph) {
-    graph.getConfigInputPorts().remove(param.getGraphPort());
   }
 }
--- plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/factory/PiMMUserFactory.java
+++ plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/factory/PiMMUserFactory.java
@@ -95,24 +95,27 @@ public final class PiMMUserFactory extends PiMMFactoryImpl implements PreesmUser
 
     if (copy instanceof final PiGraph piGraph) {
 
-      // Check if the PiGraph has an observer
-      if (piGraph.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
-        copy.eAdapters().add(GraphObserver.getInstance());
-      }
+      // // Check if the PiGraph has an observer
+      // if (piGraph.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
+      // copy.eAdapters().add(GraphObserver.getInstance());
+      // }
 
-      // Check for all subgraph in this PiGraph and its subgraph
-      for (final PiGraph graph : piGraph.getAllChildrenGraphs()) {
-        if (graph.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
-          graph.eAdapters().add(GraphObserver.getInstance());
-        }
-      }
+      // // Check for all subgraph in this PiGraph and its subgraph
+      // for (final PiGraph graph : piGraph.getAllChildrenGraphs()) {
+      // if (graph.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
+      // graph.eAdapters().add(GraphObserver.getInstance());
+      // }
+      // }
 
       // Check for all fifos in this PiGraph and its subgraph
-      for (final Fifo fifo : piGraph.getAllFifos()) {
-        if (fifo.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
-          fifo.eAdapters().add(GraphObserver.getInstance());
-        }
-      }
+      // for (final Fifo fifo : piGraph.getAllFifos()) {
+      // if (fifo.eAdapters().stream().noneMatch(GraphObserver.class::isInstance)) {
+      // fifo.eAdapters().add(GraphObserver.getInstance());
+      // }
+      // }
+      piGraph.getAllFifos().parallelStream()
+          .filter(fifo -> fifo.eAdapters().stream().noneMatch(GraphObserver.class::isInstance))
+          .forEach(fifo -> fifo.eAdapters().add(GraphObserver.getInstance()));
     }
 
     return copy;
@@ -361,7 +364,7 @@ public final class PiMMUserFactory extends PiMMFactoryImpl implements PreesmUser
     final PiGraph res = super.createPiGraph();
     final Expression exp = createExpression();
     res.setExpression(exp);
-    res.eAdapters().add(GraphObserver.getInstance());
+    // res.eAdapters().add(GraphObserver.getInstance());
     return res;
   }
 
