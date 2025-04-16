b7f107fa5d6c693a403c97b5bfd114bc28196c3f
Jacques Morin <ja.morin@free.fr>
2025-04-16 14:07:23 +0200

    if actor is a cluster it is not mergeable

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/MinimalMergingHeuristic.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/clustering/MinimalMergingHeuristic.java
@@ -3,6 +3,7 @@ package org.preesm.algorithm.clustering;
 import java.util.List;
 import java.util.Map;
 import org.preesm.model.pisdf.AbstractActor;
+import org.preesm.model.pisdf.PiGraph;
 import org.preesm.model.scenario.Scenario;
 import org.preesm.model.slam.ComponentInstance;
 import org.preesm.workflow.implement.AbstractWorkflowNodeImplementation;
@@ -11,9 +12,12 @@ public class MinimalMergingHeuristic extends MergingHeuristic {
 
   /***
    * Successor actors are eligible for merging if all their predecessors are the same arch refArchi. Predecessors are
-   * eligible if they are the same arch refArchi.
+   * eligible if they are the same arch refArchi. For now clusters are immediately ruled out.
    */
   public boolean assess(AbstractActor seed, AbstractActor actor, Map<String, Object> params) {
+    if (actor instanceof PiGraph) {
+      return false;
+    }
 
     final int position = (int) params.get("position");
     final Scenario scenario = (Scenario) params.get(AbstractWorkflowNodeImplementation.KEY_SCENARIO);
