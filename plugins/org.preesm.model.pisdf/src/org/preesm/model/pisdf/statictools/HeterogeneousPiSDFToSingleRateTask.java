53cc1c4d641c5e6976eee51743ed97d3f6da5f49
JakeMomo <ja.morin@free.fr>
2026-02-09 13:45:48 +0100

    created heterogeneous single-rate conversion task

---
+++ plugins/org.preesm.model.pisdf/src/org/preesm/model/pisdf/statictools/HeterogeneousPiSDFToSingleRateTask.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright or © or Copr. IETR/INSA - Rennes (2018 - 2024) :
+ *
+ * Antoine Morvan [antoine.morvan@insa-rennes.fr] (2018 - 2019)
+ * Florian Arrestier [florian.arrestier@insa-rennes.fr] (2018)
+ * Hugo Miomandre [hugo.miomandre@insa-rennes.fr] (2024)
+ *
+ * This software is a computer program whose purpose is to help prototyping
+ * parallel applications using dataflow formalism.
+ *
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+/**
+ *
+ */
+package org.preesm.model.pisdf.statictools;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.preesm.commons.doc.annotations.Parameter;
+import org.preesm.commons.doc.annotations.Port;
+import org.preesm.commons.doc.annotations.PreesmTask;
+import org.preesm.commons.doc.annotations.Value;
+import org.preesm.commons.exceptions.PreesmRuntimeException;
+import org.preesm.commons.logger.PreesmLogger;
+import org.preesm.model.pisdf.Arch;
+import org.preesm.model.pisdf.PiGraph;
+import org.preesm.model.pisdf.brv.BRVMethod;
+import org.preesm.workflow.elements.Workflow;
+import org.preesm.workflow.implement.AbstractTaskImplementation;
+import org.preesm.workflow.implement.AbstractWorkflowNodeImplementation;
+
+/**
+ * @author farresti
+ *
+ */
+@PreesmTask(id = "heterogeneous-pisdf-srdag", name = "Heterogeneous PiSDF Single-Rate Transformation",
+    category = "Graph Transformation",
+
+    inputs = { @Port(name = AbstractWorkflowNodeImplementation.KEY_PI_GRAPH, type = PiGraph.class) },
+    outputs = { @Port(name = AbstractWorkflowNodeImplementation.KEY_PI_GRAPH, type = PiGraph.class) },
+
+    parameters = { @Parameter(name = HeterogeneousPiSDFToSingleRateTask.CONSISTENCY_METHOD,
+        values = { @Value(name = "LCM", effect = ""), @Value(name = "Topology", effect = "") }) })
+public class HeterogeneousPiSDFToSingleRateTask extends AbstractTaskImplementation {
+
+  public static final String CONSISTENCY_METHOD = "Consistency_Method";
+
+  final Logger logger = PreesmLogger.getLogger();
+
+  @Override
+  public Map<String, Object> execute(final Map<String, Object> inputs, final Map<String, String> parameters,
+      final IProgressMonitor monitor, final String nodeName, final Workflow workflow) {
+    final PiGraph graph = (PiGraph) inputs.get(KEY_PI_GRAPH);
+    logger.info(() -> "Computing Repetition Vector for graph [" + graph.getName() + "]");
+
+    final String consistencyMethod = parameters.get(CONSISTENCY_METHOD);
+    final BRVMethod method = BRVMethod.getByName(consistencyMethod);
+    if (method == null) {
+      throw new PreesmRuntimeException("Unsupported method for checking consistency [" + consistencyMethod + "]");
+    }
+
+    // list of pigraphs that must not be flattened
+    final List<String> flattenExclusionList = graph.getAllChildrenGraphs().stream().filter(PiGraph::isClusterValue)
+        .map(c -> c.getName()).toList();
+
+    // List of pigraphs that must not be converted to srdag
+    final List<String> srdagExclusionList = graph.getAllChildrenGraphs().stream()
+        .filter(g -> (g.isCluster() && g.getTargetArch().equals(Arch.FPGA))).map(c -> c.getName()).toList();
+
+    // Flatten the graph
+    // apparemment pas nécessaire ???
+    // final PiGraph flatGraph = PiSDFHeterogeneousFlattener.flatten(graph, flattenExclusionList);
+
+    // then convert to single-rate
+    final PiGraph result = PiSDFToSingleRate.computeWithExclusionLists(graph, method, flattenExclusionList,
+        srdagExclusionList);
+
+    final Map<String, Object> output = new LinkedHashMap<>();
+    output.put(KEY_PI_GRAPH, result);
+    return output;
+  }
+
+  @Override
+  public Map<String, String> getDefaultParameters() {
+    final LinkedHashMap<String, String> res = new LinkedHashMap<>();
+    res.put(CONSISTENCY_METHOD, BRVMethod.LCM.getLiteral());
+    return res;
+  }
+
+  @Override
+  public String monitorMessage() {
+    return "Transforming PiGraph to Single-Rate Directed Acyclic PiGraph.";
+  }
+
+}
