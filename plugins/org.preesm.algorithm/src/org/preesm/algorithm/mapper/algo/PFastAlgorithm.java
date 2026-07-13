612611594fee5b14829709f5fa400d6383e37268
Hugo Miomandre <hugo.miomandre@insa-rennes.fr>
2026-07-13 13:31:53 +0200

    Fixed PFastAlgorithm ?

--- plugins/org.preesm.algorithm/src/org/preesm/algorithm/mapper/algo/PFastAlgorithm.java
+++ plugins/org.preesm.algorithm/src/org/preesm/algorithm/mapper/algo/PFastAlgorithm.java
@@ -332,7 +332,7 @@ public class PFastAlgorithm {
         final String name = String.format("thread%d", i);
 
         // step 9/11
-        final PFastCallable thread = new PFastCallable(name, dag, archi, subIter.next(), true, abcParams, fastParams,
+        final PFastCallable thread = new PFastCallable(name, dag, archi, subIter.next(), false, abcParams, fastParams,
             scenario);
 
         final FutureTask<MapperDAG> task = new FutureTask<>(thread);
